5b728bfe2e7cf6b1e3da79391eaee5893cc66e06
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-12-20 10:45:18 +0100

    X10.44

--- codigo_dic_23/objetos.py
+++ codigo_dic_23/objetos.py
@@ -13,6 +13,15 @@ class Persona:
         self.edad = edad
         self.altura = altura
 
+    def cumple(self):
+        self.edad += 1
+
+    def hablarCon(self, otro=None):
+        if otro:
+            print(self.nombre,'habla con ',otro.nombre)
+        else:
+            print(self.nombre,'habla solo')
+
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
 
@@ -20,13 +29,15 @@ class Persona:
         pass
         # print("Se está eliminando a: ", self.nombre)
 
-
 if __name__ == "__main__":
     p1 = Persona("Sandra", 30, 1.7)
+    p2 = Persona("Jose",44,1.76)
     print(p1.nombre)
     p1.nombre = "Eva"
-    # del(p1) # Fuerza la llamada al destructor: __del__
-    print("continua el programa")
+    # del(p1) # Fuerza la llamada al destructor: __del__    
     print(p1)
-    print(str(p1))
-    print(p1.__str__())
+    #print(str(p1))
+    #print(p1.__str__())
+    p1.cumple() # objeto.metodo()
+    p2.hablarCon()
+    p2.hablarCon(p1) 
