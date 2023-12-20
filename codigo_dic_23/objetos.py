848c0f2bef880be8d7182c1d3081e3d7351117dd
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-12-20 11:50:02 +0100

    X11.49

--- codigo_dic_23/objetos.py
+++ codigo_dic_23/objetos.py
@@ -44,12 +44,20 @@ class Guia(Persona):
         self.idiomas = idiomas
 
     def __str__(self):
-        return super().__str__() + " " + self.ambito + \
-            " " + ",".join(self.idiomas)
-    
+        return super().__str__() + " " + self.ambito + " " + ",".join(self.idiomas)
+
     def hablarCon(self, otro=None):
-        # Comprobar si tienen un idioma en común 
-        return super().hablarCon(otro)
+        # Comprobar si tienen un idioma en común
+        if not otro:
+            return super().hablarCon(otro)
+        else:
+            c1 = set(self.idiomas)
+            c2 = set(otro.idiomas)
+            comun = c1 & c2
+            if len(comun) == 0:
+                raise Exception("No tienen un idioma en común")
+            else:
+                print(self.nombre, "y", otro.nombre, "pueden hablar en", comun)
 
 
 def testPersona():
@@ -74,7 +82,7 @@ def testPersona():
 
 def testGuia():
     g1 = Guia("Ana", 34, 1.77, "I", ["inglés", "alemán"])
-    g2 = Guia("Sonia", 32, 1.87, "N", ["inglés", "francés"])
+    g2 = Guia("Sonia", 32, 1.87, "N", ["inglés", "francés", "alemán"])
     g3 = Guia("Victor", 28, 1.85, "L", ["inglés"])
     print(g1)
     g1.cumple()
@@ -83,6 +91,7 @@ def testGuia():
     L = [g1, g2, g3]
     L.sort()
     print(L)
+    g1.hablarCon(g2)
 
 
 if __name__ == "__main__":
