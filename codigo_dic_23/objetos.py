1cee0620372e35b98120441e6ee1e147ad30cd09
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-12-20 10:56:45 +0100

    X10.56

--- codigo_dic_23/objetos.py
+++ codigo_dic_23/objetos.py
@@ -18,26 +18,39 @@ class Persona:
 
     def hablarCon(self, otro=None):
         if otro:
-            print(self.nombre,'habla con ',otro.nombre)
+            print(self.nombre, "habla con ", otro.nombre)
         else:
-            print(self.nombre,'habla solo')
+            print(self.nombre, "habla solo")
+
+    def __repr__(self):
+        return str(self)
 
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
+
+    def __lt__(self, other):
+        return self.edad < other.edad
 
     def __del__(self):
         pass
         # print("Se está eliminando a: ", self.nombre)
 
+
 if __name__ == "__main__":
     p1 = Persona("Sandra", 30, 1.7)
-    p2 = Persona("Jose",44,1.76)
+    p2 = Persona("Jose", 44, 1.76)
     print(p1.nombre)
     p1.nombre = "Eva"
-    # del(p1) # Fuerza la llamada al destructor: __del__    
+    # del(p1) # Fuerza la llamada al destructor: __del__
     print(p1)
-    #print(str(p1))
-    #print(p1.__str__())
-    p1.cumple() # objeto.metodo()
+    # print(str(p1))
+    # print(p1.__str__())
+    p1.cumple()  # objeto.metodo()
     p2.hablarCon()
-    p2.hablarCon(p1) 
+    p2.hablarCon(p1)
+    L = [p1, p2, Persona("Miguel", 23, 1.8)]
+    print(L)
+    L.sort(key=lambda p: p.nombre)
+    print(L)
+    L.sort()
+    print(L)
