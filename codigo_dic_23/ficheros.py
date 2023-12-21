5fc2e460fec88807f4097924a48eba8ddf2802f8
aldebarran22 <antonioespinherranz@hotmail.com>
2023-12-21 10:10:31 +0100

    J10.10

--- codigo_dic_23/ficheros.py
+++ codigo_dic_23/ficheros.py
@@ -3,19 +3,36 @@ Ficheros. Entrada/Salida
 """
 import sys
 
+
 def testStdout(fichero=sys.stdout):
     productos = [
         {"id": 1, "producto": "portatil", "precio": 1200.567},
         {"id": 2, "producto": "HP", "precio": 2250.6},
         {"id": 3, "producto": "Impresora", "precio": 566.0},
     ]
-    print("%-5s\t%-15s\t%8s" % tuple(productos[0].keys()),file=fichero)
+    print("%-5s\t%-15s\t%8s" % tuple(productos[0].keys()), file=fichero)
     for d in productos:
         # print("%05d\t%-15s\t%8.2f" % (d["id"], d["producto"],  d["precio"]))
-        print("%05d\t%-15s\t%8.2f" % tuple(d.values()),file=fichero)
+        print("%05d\t%-15s\t%8.2f" % tuple(d.values()), file=fichero)
+
+
+def leerFichero(path):
+    f = None
+    try:
+        f = open(path, "r")
+        for linea in f:
+            print(linea)
+    except Exception as e:
+        print(e)
+    finally:
+        if f:
+            f.close()
 
 
 if __name__ == "__main__":
-    f = open("productos.txt","w")
+    leerFichero("../ficheros/Empleados.txt")
+    """
+    f = open("productos.txt", "w")
     testStdout(f)
     f.close()
+    """
