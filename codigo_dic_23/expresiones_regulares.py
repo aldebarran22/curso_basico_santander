cca75167ad523847bc1c2c11de0abbaf065f7ad8
aldebarran22 <antonioespinherranz@hotmail.com>
2023-12-21 13:02:01 +0100

    J13.01

--- codigo_dic_23/expresiones_regulares.py
+++ codigo_dic_23/expresiones_regulares.py
@@ -12,3 +12,11 @@ def testPatron(patron, L):
     print()
 
 
+if __name__ == "__main__":
+    testPatron(r".ython", ["python", "Pyton", "Java", "cython", "Jython"])
+    testPatron(r"...\.", ["1234.", "abc", "abc.", "hola.", "123."])
+
+    # Validar un DNI:
+    testPatron("[0-9]{8}[A-Z]$", \
+               ["12345678a","12345678", "123456789S", "S", "1234A", \
+                "12345678A", "12345678AB"])
