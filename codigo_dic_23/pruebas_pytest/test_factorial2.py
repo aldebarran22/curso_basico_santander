06b5ffb045fdb3b5bebb0b7907985d922a4f64cc
aldebarran22 <antonioespinherranz@hotmail.com>
2023-12-22 15:00:25 +0100

    todo

--- codigo_dic_23/pruebas_pytest/test_factorial2.py
+++ codigo_dic_23/pruebas_pytest/test_factorial2.py
@@ -4,6 +4,14 @@ Prueba del Factorial con Recursividad
 
 
 def fact(n):
+    """
+    Calcula el factorial de forma recursiva
+
+    :param n: El factorial a calcular
+    :type n: int
+    :returns: int -- El factorial de n
+    :raises: RecursionError
+    """
     return 1 if n == 1 else n * fact(n - 1)
 
 
