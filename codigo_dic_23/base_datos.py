d0933ffd245857645cb12fd57263ba9bceb83187
aldebarran22 <antonioespinherranz@hotmail.com>
2023-12-22 09:43:32 +0100

    v9.43

--- codigo_dic_23/base_datos.py
+++ codigo_dic_23/base_datos.py
@@ -5,26 +5,30 @@ import sqlite3 as dbapi
 from os.path import isfile
 
 
-def test(path, tabla):
+def test(path, tabla, pathFile):
     conexion = None
     cur = None
+    fich = None
     try:
         if isfile(path):
             conexion = dbapi.connect(path)
             print("Conexión ok!")
+            fich = open(pathFile, "w")
             cur = conexion.cursor()
             sql = f"select * from {tabla}"
             cur.execute(sql)
             cabs = ";".join([t[0] for t in cur.description])
-            print(cabs)
+            print(cabs, file=fich)
             for t in cur.fetchall():
                 linea = ";".join([str(i) for i in t])
-                print(linea)
+                print(linea, file=fich)
         else:
             raise FileNotFoundError(f"El fichero {path} no existe")
     except Exception as e:
         print(e)
     finally:
+        if fich:
+            fich.close()
         if cur:
             cur.close()
         if conexion:
@@ -32,4 +36,4 @@ def test(path, tabla):
 
 
 if __name__ == "__main__":
-    test("../bd/empresa3.db", "categorias")
+    test("../bd/empresa3.db", "categorias", "../ficheros/categorias.csv")
