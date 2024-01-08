82ea2bdf0fc4e6c29f982306770f4d1c4652e451
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-08 10:57:05 +0100

    10.56

---
+++ codigo_ene_8_12/fstring.py
@@ -0,0 +1,12 @@
+"""
+Pruebas con las f-string de Python
+"""
+
+numero = 100
+print("el numero es: ",numero)
+print(f"el numero es: {numero}")
+
+directorio = "documentos"
+fichero ="libro1.xlsx"
+path = f"C:/clientes/{directorio}/{fichero}"
+print(path)
