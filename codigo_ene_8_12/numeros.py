2e7ebc3a991731df750a67c8ad12aa59e804686e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-08 10:36:21 +0100

    10.36

--- codigo_ene_8_12/numeros.py
+++ codigo_ene_8_12/numeros.py
@@ -8,5 +8,9 @@ numero2 = 34.88
 print(numero, " el tipo es:", type(numero))
 print(numero2, " el tipo es:", type(numero2))
 resul = numero2 + numero
-print(resul, " el tipo es:", type(resul))
+print("suma:", resul, " el tipo es:", type(resul))
+resta = numero2 - numero
+print("resta:", resta, " el tipo es:", type(resta))
+
+
 
---
+++ codigo_ene_8_12/teclado.py
@@ -0,0 +1,9 @@
+"""
+Leer de teclado dos números y sumarlos
+"""
+
+# Leer un dato de teclado y lo convierte a entero
+numero1 = int(input("Dame numero1:"))
+numero2 = int(input("Dame numero2:"))
+resul = numero1 + numero2
+print("Suma:", resul)
