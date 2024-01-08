16ef1200e45847ff8c01570bd4b5599eb04b5eca
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-08 10:27:32 +0100

    10.27

---
+++ codigo_ene_8_12/numeros.py
@@ -0,0 +1,12 @@
+"""
+Operaciones con números: int, float, complex
+"""
+
+numero = 10
+numero2 = 34.88
+
+print(numero, " el tipo es:", type(numero))
+print(numero2, " el tipo es:", type(numero2))
+resul = numero2 + numero
+print(resul, " el tipo es:", type(resul))
+
--- codigo_ene_8_12/prueba.py
+++ codigo_ene_8_12/prueba.py
@@ -1,2 +1,8 @@
-print('ok')
-print('hola')
+"""
+Primer ejemplo para imprimir un mensaje
+"""
+
+
+print("ok")  # Imprime y salta de línea
+print("hola")
+
