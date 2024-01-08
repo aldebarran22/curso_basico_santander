95cd24c1a6598c92eab007f77a7c33592be69600
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-08 14:44:36 +0100

    14.44

--- codigo_ene_8_12/listas.py
+++ codigo_ene_8_12/listas.py
@@ -10,24 +10,39 @@ print(L2)
 
 # Acceso a los elementos:
 print("El primero:", L[0], L[-5])
-print('El último: ', L[4], L[-1])
+print("El último: ", L[4], L[-1])
 
 L[3] = 7
 # L[-2] = 7
 print(L)
 
 # Funciones aplicadas a las listas:
-print('suma:', sum(L))
-print('min: ', min(L))
-print('max: ', max(L))
-print('recuento: ', len(L))
-print('promedio:', sum(L)/len(L))
+print("suma:", sum(L))
+print("min: ", min(L))
+print("max: ", max(L))
+print("recuento: ", len(L))
+print("promedio:", sum(L) / len(L))
 
 # Recorrido:
 for i in L:
-    print(i, end=' ')
-print() # Imprime un salto de linea: \n
+    print(i, end=" ")
+print()  # Imprime un salto de linea: \n
 
 # Recorrido imprimiendo el indice y el valor:
 for ind, valor in enumerate(L):
     print(ind, valor)
+
+# Crear listas con list:
+L3 = list("hola que tal")
+print(L3)
+
+L4 = list(range(11))
+print(L4)
+
+# slicing: L[ini:fin-1:salto=1]
+L = [1, 2, 3, 4, 5, 6, 7]
+print("los 3 primeros:", L[0:3])
+print("los 3 primeros:", L[:3])  # desde el principio
+print("los tres últimos:", L[-3:])
+print("Toda la lista de dos en dos:", L[::2])
+print('Quitar el primero y el último:', L[1:-1])
