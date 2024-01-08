5f2453b42c23e849c9ae629fd0b232a65f8dfc39
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-08 10:49:13 +0100

    10.49

--- codigo_ene_8_12/teclado.py
+++ codigo_ene_8_12/teclado.py
@@ -3,11 +3,11 @@ Leer de teclado dos números y sumarlos
 """
 
 # Leer un dato de teclado y lo convierte a entero
-#numero1 = int(input("Dame numero1:"))
-#numero2 = int(input("Dame numero2:"))
+# numero1 = int(input("Dame numero1:"))
+# numero2 = int(input("Dame numero2:"))
 
 # Convertir a número real:
-numero1 = float(input('Dame numero1:'))
-numero2 = float(input('Dame numero2:'))
+numero1 = float(input("Dame numero1:"))
+numero2 = int(input("Dame numero2:"))
 resul = numero1 + numero2
-print("Suma:", resul)
+print("Suma:", round(resul, 2))
