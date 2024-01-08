63297b78ad0ffee79d7742ac7eb5a5e3fe172549
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-08 11:50:16 +0100

    11.50

---
+++ codigo_ene_8_12/comparar.py
@@ -0,0 +1,17 @@
+"""
+Ejemplo de condiciones
+"""
+numero = 10
+if numero:
+    print("true")
+
+    if numero > 5:
+        print("mayor que 5")
+else:
+    print("false")
+
+cadena = ""
+if cadena:
+    print("true")
+else:
+    print("false")
--- codigo_ene_8_12/fstring.py
+++ codigo_ene_8_12/fstring.py
@@ -3,10 +3,10 @@ Pruebas con las f-string de Python
 """
 
 numero = 100
-print("el numero es: ",numero)
+print("el numero es: ", numero)
 print(f"el numero es: {numero}")
 
 directorio = "documentos"
-fichero ="libro1.xlsx"
-path = f"C:/clientes/{directorio}/{fichero}"
+fichero = "libro1"
+path = f"C:/clientes/{directorio}/{fichero}.xlsx"
 print(path)
