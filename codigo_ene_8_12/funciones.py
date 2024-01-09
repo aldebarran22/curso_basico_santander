2db72573b00aed1fa4df06f4daeacb0144b031eb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-09 11:50:24 +0100

    m11.50

--- codigo_ene_8_12/funciones.py
+++ codigo_ene_8_12/funciones.py
@@ -2,25 +2,30 @@
 Ejemplo de funciones en Python
 """
 
-def sumar(a,b):
-    return a+b
 
-def sumar3(a,b,c):
-    return a+b+c
+def sumar(a, b):
+    return a + b
+
+
+def sumar3(a, b, c):
+    return a + b + c
+
 
 def imprimirPosiciones(t, numero):
     ini = 0
-    for i in range(t.count(numero)):
-        pos = t.index(numero, ini)
-        print(pos)
-        ini = pos + 1
+    if numero in t:
+        for i in range(t.count(numero)):
+            pos = t.index(numero, ini)
+            print(f"{numero} está en la posición: {pos}")
+            ini = pos + 1
+    else:
+        print(f"{numero} no se encuentra en la tupla")
 
-print(sumar(3,5))
-s = sumar(3,5)
+
+print(sumar(3, 5))
+s = sumar(3, 5)
 print(s)
-print(sumar("55","44"))
+print(sumar("55", "44"))
 
-imprimirPosiciones((1,2,3,4,5), 5)
-
-
-
+imprimirPosiciones((1, 5, 5, 2, 3, 4, 5), 5)
+imprimirPosiciones((1, 2, 3, 4, 5), 6)
