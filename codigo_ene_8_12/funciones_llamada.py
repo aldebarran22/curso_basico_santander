db4a2d344af82da6c60b713207fdc6df23194f02
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-09 12:40:42 +0100

    m12.40

--- codigo_ene_8_12/funciones_llamada.py
+++ codigo_ene_8_12/funciones_llamada.py
@@ -22,3 +22,12 @@ if __name__ == "__main__":
     # nominal
     print(sumar(b=2, a=1))
     f1(s=30)
+
+    # tupla o lista:
+    t = (12, 30, 45)
+    f1(*t)
+    f1(t[0], t[1], t[2])
+
+    # con un diccionario:
+    d = {"h":12, "m":30, "s":45}
+    f1(**d)
