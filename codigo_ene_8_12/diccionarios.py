a4e5e69cf542ae67b314eeb93a1ce7661b20d79e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-09 09:33:04 +0100

    m9.32

--- codigo_ene_8_12/diccionarios.py
+++ codigo_ene_8_12/diccionarios.py
@@ -9,3 +9,24 @@ print(d["nombre"])
 d["dni"] = "56000112G"  # Añade una nueva clave
 d["edad"] = 44  # Modificar una clave
 print(d)
+
+# Recorrer un diccionario:
+for clave, valor in d.items():
+    print(clave, valor)
+
+# Comprobar si existe una clave en el diccionario:
+if "direccion" in d:
+    print(d["direccion"])
+else:
+    print("no existe la dirección")
+
+L = [100, 34, 45, 34, 200, 200, 67, 90, 45, 34, 200]
+# Construir un histograma: 100:1, 34:3, 45:2 ...
+d2 = dict()
+for i in L:
+    if i in d2:
+        # El número ya existe, contamos uno mas.
+        d2[i] += 1
+    else:
+        # La primera vez que vemos el número
+        d2[i] = 1
