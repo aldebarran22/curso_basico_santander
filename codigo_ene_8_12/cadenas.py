10205ceb5c85223883d667dffe484a744d8085d0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-09 11:02:16 +0100

    m.11.02

--- codigo_ene_8_12/cadenas.py
+++ codigo_ene_8_12/cadenas.py
@@ -11,3 +11,17 @@ print(r"hola\tadios")
 # Operadores a nivel de cadena:
 print("hola" + nombre)  # Concatenar
 print("hola" * 5)  # Concatenar n-veces
+
+# Partir una cadena en campos:
+linea = "1;Davolio;Representante de ventas"
+cab = "id;nombre;cargo"
+L = linea.split(";")
+Lcab = cab.split(";")
+print(L)
+print(Lcab)
+# Imprimir el nombre: 
+print(L[1])
+d = dict(zip(Lcab, L))
+print(d)
+# Imprimir el nombre: 
+print(d['nombre'])
