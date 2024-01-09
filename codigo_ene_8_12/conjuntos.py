ab7f2cf9f2e79ed648ef3496428ef98d5a3fc6eb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-09 09:56:23 +0100

    m9.56

--- codigo_ene_8_12/conjuntos.py
+++ codigo_ene_8_12/conjuntos.py
@@ -8,8 +8,12 @@ print(c, type(c))
 comida = {"Raquel", "José", "Miguel", "Sandra", "Jorge"}
 cena = {"Eva", "Miguel", "Sandra", "Nuria", "Juan"}
 
-print("Quien va a comer y a cenar: ", comida & cena)
-print("Quien va solo a comer:", comida - cena)
+print("Quien va a comer y a cenar: ", comida & cena) #intersección
+print("Quien va solo a comer:", comida - cena) # diferencia
 print("Quien va solo a cenar:", cena - comida)
-print("Quienes han participado en los eventos:", comida | cena)
-print("Quienes se han apuntado a un solo evento:", comida ^ cena)
+print("Quienes han participado en los eventos:", comida | cena) # unión
+print("Quienes se han apuntado a un solo evento:", comida ^ cena) # dif.simétrica
+
+todos = list(comida) + list(cena)
+print(todos)
+print('En total son:', len(todos))
