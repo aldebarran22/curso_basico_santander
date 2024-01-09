29fc62accd7b12a508fad53222bf4e172b30d41f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-09 14:58:15 +0100

    m14.58

--- codigo_ene_8_12/ordenaciones.py
+++ codigo_ene_8_12/ordenaciones.py
@@ -13,3 +13,13 @@ L2.sort() # ASC
 print(L2)
 L2.sort(reverse=True) # DESC
 print(L2)
+
+t = (-6, 0, -11, 123, 90, 56)
+t2 = tuple(sorted(t))
+print(t2)
+
+L = [('Sara', 34), ('Miguel',56), ('Andrés',23), ('Ana',33), ('Jorge',6)]
+L.sort() # Por defecto ordena por el primer campo de la tupla
+print(L)
+L.sort(key=lambda t : t[1])
+print(L)
