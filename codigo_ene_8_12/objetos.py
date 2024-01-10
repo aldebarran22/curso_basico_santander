234783133b769611b087df3a7464ba69fad82d90
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-10 11:36:08 +0100

    x10.36

--- codigo_ene_8_12/objetos.py
+++ codigo_ene_8_12/objetos.py
@@ -16,11 +16,18 @@ class Persona:
         self.edad = edad
         self.altura = altura
 
+    def cumple(self):
+        self.edad+=1
+
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
 
 if __name__ == '__main__':
     p1 = Persona("Pedro",33, 1.8)
     print(p1)
-    print(str(p1))
-    print(p1.__str__())
+    p1.cumple()
+    print(p1)
+    #print(str(p1))
+    #print(p1.__str__())
+    L = [p1, Persona("Ana",45,1.77), Persona("Sara",23,1.88)]
+    print(L)
