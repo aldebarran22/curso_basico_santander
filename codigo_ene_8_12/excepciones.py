9532858835dab8c65127034253279e8dc490f6ac
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-10 14:06:04 +0100

    X14.05

--- codigo_ene_8_12/excepciones.py
+++ codigo_ene_8_12/excepciones.py
@@ -4,7 +4,15 @@ Capturar y lanzar excepciones en Python
 
 
 def prueba1():
-    pass
+    # Capturar un única excepción y tratarla
+    try:
+        L = [1, 2, 3, 4, 5]
+        print("Pos 100:", L[100])
+        print("Longitud de L:", len(L))
+    except IndexError as e:
+        print(e)
+
+    print('fin')
 
 
 if __name__ == "__main__":
