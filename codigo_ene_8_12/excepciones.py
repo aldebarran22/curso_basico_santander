9ec6d1be1fff87a557009ab086f92db3f1ce56dc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-10 14:24:29 +0100

    X14.24

--- codigo_ene_8_12/excepciones.py
+++ codigo_ene_8_12/excepciones.py
@@ -27,6 +27,25 @@ def prueba2():
         print("No existe la clave del dict: ", e)
 
 
+def prueba3():
+    # Capturar dos excepciones distintas y otras posibles excepciones
+    try:
+        L = [1, 2, 3, 4, 5]
+        d = {"a": 1, "b": 2}
+        print(L[2] / 0)
+        print(d["x"])  # KeyError
+        print("Pos 100:", L[100])  # IndexError
+        print("Longitud de L:", len(L))
+
+    except IndexError as e:
+        print(e)
+    except KeyError as e:
+        print("No existe la clave del dict: ", e)
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+
 if __name__ == "__main__":
     # prueba1()
-    prueba2()
+    # prueba2()
+    prueba3()
