8948675136b677c0a3c7f1710eaec9ef8d70b847
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-10 11:53:39 +0100

    X11.53

--- codigo_ene_8_12/objetos.py
+++ codigo_ene_8_12/objetos.py
@@ -17,7 +17,10 @@ class Persona:
         self.altura = altura
 
     def cumple(self):
-        self.edad+=1
+        self.edad += 1
+
+    def __lt__(self, other):
+        return self.edad < other.edad
 
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
@@ -26,14 +29,21 @@ class Persona:
         # Vuelve a llamar al método __str__()
         return str(self)
 
-if __name__ == '__main__':
-    p1 = Persona("Pedro",33, 1.8)
+
+if __name__ == "__main__":
+    p1 = Persona("Pedro", 33, 1.8)
+    p2 = Persona("Juan", 30, 1.82)
+    if p1 < p2:  # if p1.__lt__(p2)
+        print(p1, "menor que", p2)
+    else:
+        print(p2, "menor que", p1)
     print(p1)
     p1.cumple()
     print(p1)
-    #print(str(p1))
-    #print(p1.__str__())
-    L = [p1, Persona("Ana",45,1.77), Persona("Sara",23,1.88)]
+    # print(str(p1))
+    # print(p1.__str__())
+    L = [p1, Persona("Ana", 45, 1.77), Persona("Sara", 23, 1.88)]
     print(L)
-    L.sort(key=lambda obj : obj.edad)
+    L.sort(key=lambda obj: obj.altura, reverse=True)
+    L.sort()
     print(L)
