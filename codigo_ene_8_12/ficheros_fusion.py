264f0b3aa452197f51fc44fa627cbbc018d0b0b4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-11 10:45:54 +0100

    J10.45

---
+++ codigo_ene_8_12/ficheros_fusion.py
@@ -0,0 +1,27 @@
+"""
+A partir de dos ficheros de empleados, fusionar
+los datos en otro fichero nuevo y dejarlos ordenados
+por id. No añadir repetidos.
+"""
+
+
+def cargaFichero(path):
+    """
+    Leer el fichero entero, separar en linea
+    y cargarlo a un conjunto.
+    """
+    fich = None
+    try:
+        fich = open(path, "r")
+        txt = fich.read()
+        print(txt)
+    except Exception as e:
+        print(e)
+    finally:
+        if fich:
+            fich.close()
+
+
+if __name__ == "__main__":
+    c1 = cargaFichero("Empleados2.txt")
+    c2 = cargaFichero("Empleados3.txt")
