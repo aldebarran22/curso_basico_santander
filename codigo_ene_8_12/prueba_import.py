d552168e323d2cbf17857046fa26fbb026a61595
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-11 12:38:31 +0100

    J12.38

--- codigo_ene_8_12/prueba_import.py
+++ codigo_ene_8_12/prueba_import.py
@@ -2,6 +2,9 @@
 Importar un módulo que está fuera de la carpeta
 de code y tampoco está en las librería de Python
 """
+import sys
+
+sys.path.append(r"D:\temp")
 from funcion_abc import operar
 
 print(operar(1, 4))
