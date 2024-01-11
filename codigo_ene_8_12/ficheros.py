0489c31182099d3773d8002f277b739a4ea52a14
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-11 10:16:35 +0100

    J10.16

--- codigo_ene_8_12/ficheros.py
+++ codigo_ene_8_12/ficheros.py
@@ -1,6 +1,8 @@
 """
 Leer y escribir ficheros CSV con Python
 """
+
+
 def leerFichero(path):
     """
     Imprimir el contenido en consola
@@ -10,12 +12,16 @@ def leerFichero(path):
         # Abrir el fichero en modo lectura
         fich = open(path, "r")
         for linea in fich:
+            # Limpia el carácter fin de línea (EOL)
+            linea = linea.strip()
             print(linea)
-            
+
     except Exception as e:
         print(e)
     finally:
-        if fich != None: fich.close()
+        if fich != None:
+            fich.close()
 
-if __name__ == '__main__':
-    leerFichero('../ficheros/Pedidos.txt')
+
+if __name__ == "__main__":
+    leerFichero("../ficheros/Pedidos.txt")
