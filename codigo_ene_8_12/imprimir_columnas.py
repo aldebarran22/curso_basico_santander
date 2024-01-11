2122f0fd8e5cc9e41bd66ebf17fdbb2fe5c6c080
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-11 09:54:38 +0100

    J9.54

--- codigo_ene_8_12/imprimir_columnas.py
+++ codigo_ene_8_12/imprimir_columnas.py
@@ -1,7 +1,9 @@
 """
 Mostrar información en columnas
 """
-
-L = [('HP',1245.967), ('Teclado',45.5),('Ratón',15.0)]
+# Abrir un fichero para grabar datos
+fich = open("productos.txt", "w")
+L = [("HP", 1245.967), ("Teclado", 45.5), ("Ratón", 15.0)]
 for producto, precio in L:
-    print("%-12s\t%8.2f" % (producto, precio))
+    print("%-12s\t%8.2f" % (producto, precio), file=fich)
+fich.close()
