7f5ae01a65265b9cea273059ea8996db97166a01
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-11 11:59:44 +0100

    J12.00

--- codigo_ene_8_12/fichero_iterador.py
+++ codigo_ene_8_12/fichero_iterador.py
@@ -1,7 +1,7 @@
 # Leer un fichero con un iterador:
 
 print("Imprimir el contenido de un archivo")
-with open("consola.py") as fp:
+with open("Empleados_todo.txt") as fp:
 	for linea in iter(fp.readline,''):
 		print(linea)
 
