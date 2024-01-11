d1be887289730d9a1bd966ddc170a8851c8cb7bd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-11 10:25:48 +0100

    J10.25

--- codigo_ene_8_12/ficheros.py
+++ codigo_ene_8_12/ficheros.py
@@ -23,5 +23,28 @@ def leerFichero(path):
             fich.close()
 
 
+def filtroPaises(path, *paises):
+    """
+    Imprimir el contenido en consola
+    """
+    fich = None
+    try:
+        # Abrir el fichero en modo lectura
+        fich = open(path, "r")
+        for linea in fich:
+            # Limpia el carácter fin de línea (EOL)
+            linea = linea.strip()
+            columnas = linea.split(";")
+            if columnas[-1] in paises:
+                print(linea)
+
+    except Exception as e:
+        print(e)
+    finally:
+        if fich != None:
+            fich.close()
+
+
 if __name__ == "__main__":
-    leerFichero("../ficheros/Pedidos.txt")
+    # leerFichero("../ficheros/Pedidos.txt")
+    filtroPaises("../ficheros/Pedidos.txt", "Alemania", "Francia")
