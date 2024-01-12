7bb7584be3410d8e1e8f1fe04ff884e048acbb9a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-12 12:16:06 +0100

    V12.16

---
+++ codigo_ene_8_12/cliente.py
@@ -0,0 +1,17 @@
+"""
+Cliente TCP en Python
+"""
+import socket
+
+s = socket.socket()
+s.connect(("localhost", 9999))
+while True:
+    mensaje = input("> ")
+    s.send(mensaje.encode("utf-8"))
+    recibido = s.recv(1024)
+    recibido = recibido.decode("utf-8")
+    if mensaje == "quit":
+        break
+    print("Recibido:", recibido)
+print("adios")
+s.close()
