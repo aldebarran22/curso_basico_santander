655a5982be927ca929d74d317e6ed2de90736e46
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-12 13:02:43 +0100

    V13.02

--- codigo_ene_8_12/hilos1.py
+++ codigo_ene_8_12/hilos1.py
@@ -17,12 +17,12 @@ class Mensajes(Thread):
 
     def run(self):
         for i in range(self.n):
-            print(self.getName(), " mensaje: ", (i + 1))
+            print(self.name, " mensaje: ", (i + 1))
             sleep(randint(1, 2))
-        print("Termina:", self.getName())
+        print("Termina:", self.name)
 
 
-if __name__ == "__main__":
+def test2Threads():
     m1 = Mensajes(12, "mensajero1")
     m2 = Mensajes(9, "mensajero2")
 
@@ -31,3 +31,13 @@ if __name__ == "__main__":
 
     m1.join()
     m2.join()
+    print("main termina!")
+
+
+def testListThreads():
+    pass
+
+
+if __name__ == "__main__":
+    # test2Threads()
+    testListThreads()
