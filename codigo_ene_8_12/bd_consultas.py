dade9dee26da0997780448e84e31023d4f4789b1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-12 10:34:54 +0100

    V10.34

--- codigo_ene_8_12/bd_consultas.py
+++ codigo_ene_8_12/bd_consultas.py
@@ -5,9 +5,10 @@ Crear conexiones y cursores. Ejecutar una consulta.
 
 import sqlite3 as dbapi
 from os.path import isfile
+import sys
 
 
-def consulta(tabla, path, sep=";"):
+def consulta(tabla, path, sep=";", file=sys.stdout):
     """
     Lista el contenido de la tabla que recibe
     """
@@ -24,11 +25,11 @@ def consulta(tabla, path, sep=";"):
             sql = f"select * from {tabla}"
             cur.execute(sql)
             cabs = sep.join([t[0] for t in cur.description])
-            print(cabs)
+            print(cabs, file=file)
 
             for t in cur.fetchall():
                 fila = sep.join([str(i) for i in t])
-                print(fila)
+                print(fila, file=file)
 
     except Exception as e:
         print(e)
@@ -58,6 +59,22 @@ def testConexion(path):
             con.close()
 
 
+def exportar(*tablas, sep=";"):
+    for tabla in tablas:
+        fich = None
+        try:
+            path = f"../ficheros/{tabla}.csv"
+            fich = open(path, "w")
+            consulta(tabla, path, sep, fich)
+
+        except Exception as e:
+            print(e)
+        finally:
+            if fich:
+                fich.close()
+
+
 if __name__ == "__main__":
     # testConexion("../bd/empresa3.db")
-    consulta("categorias", "../bd/empresa3.db")
+    # consulta("categorias", "../bd/empresa3.db")
+    exportar("clientes", "pedidos", "productos", sep=";")
