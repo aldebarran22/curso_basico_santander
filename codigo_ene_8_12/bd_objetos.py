2f9cc2d51314b924faf1c3d82124f5e6d86a419a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-12 11:00:56 +0100

    V11.00

--- codigo_ene_8_12/bd_objetos.py
+++ codigo_ene_8_12/bd_objetos.py
@@ -1,3 +1,7 @@
+import sqlite3 as dbapi
+from os.path import isfile
+
+
 class Empleado:
     def __init__(self, id=0, nombre="", cargo=""):
         self.__id = id
@@ -33,3 +37,42 @@ class Empleado:
 
     def __repr__(self):
         return str(self)
+
+
+class EmpleadoBD:
+    def __init__(self, path):
+        if not isfile(path):
+            raise FileNotFoundError(f"No existe el fichero {path}")
+        else:
+            self.con = dbapi.connect(path)
+
+    def __del__(self):
+        if hasattr(self, "con"):
+            self.con.close()
+
+
+if __name__ == "__main__":
+    try:
+        bd = EmpleadoBD("../bd/empresa3.db")
+        L = bd.select()
+        print(f"Tenemos {len(L)} empleados")
+
+        e1 = bd.read(1)
+        print(e1)
+
+        e1.setCargo("Gerente")
+        if bd.update(e1) == 1:
+            print("Registro actualizado")
+
+        """
+        nuevo = Empleado(0, "Sandra", "Gerente")
+        if bd.create(nuevo) == 1:
+            print("Registro creado")
+            print(nuevo)
+        """
+
+        if bd.delete(150) == 1:
+            print("Registro borrado")
+
+    except Exception as e:
+        print(e)
