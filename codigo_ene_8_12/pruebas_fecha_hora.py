ed181eb0651ee04ab671283bc6d743a08fe10f2f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-12 14:31:11 +0100

    V14.31

--- codigo_ene_8_12/pruebas_fecha_hora.py
+++ codigo_ene_8_12/pruebas_fecha_hora.py
@@ -13,11 +13,18 @@ class PruebasTime(unittest.TestCase):
         Comprobar si al convertir en texto un Time hace correctamente
         el relleno de ceros
         """
-        t1 = Time(2,3,1)
+        t1 = Time(2, 3, 1)
         self.assertEqual(str(t1), "02:03:01", msg="No rellena correctamente los ceros")
 
+        t2 = Time(12)
+        self.assertEqual(str(t2), "12:00:00", msg="No rellena correctamente los ceros")
+
     def testAdd(self):
-        pass
+        t1 = Time(2, 3, 1)
+        t2 = Time(12)
+        r = t1 + t2
+        r2 = Time(14, 3, 1)
+        self.assertEqual(str(r), str(r2), msg="No suma correctamente")
 
     def testException(self):
         pass
