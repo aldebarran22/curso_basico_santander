1dcfa5f80036605473fb3e1516c6642d23e8d0a0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-12 14:51:42 +0100

    V14.51

--- codigo_ene_8_12/pruebas_bd_consultas.py
+++ codigo_ene_8_12/pruebas_bd_consultas.py
@@ -4,14 +4,24 @@ exportar del módulo: bd_consultas.py
 """
 from bd_consultas import exportar
 import unittest
+from os import remove
 
 
 class PruebasExportar(unittest.TestCase):
-    def test1(self):
-        print("todo ok")
 
-    def test2(self):
-        raise Exception("error")
+    def setUp(self):
+        self.t = ("clientes", "pedidos")
+        exportar("empresa3.db", *self.t)
+
+    def testFicheros(self):
+        pass
+
+    def tearDown(self):
+        # Borrar los ficheros:
+        for f in self.t:
+            path = "../csv/{f}.csv"
+            remove(path)
+
 
 
 if __name__ == "__main__":
