b92b4f85c5dffa4f2d588a276eec3754b435a59d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-22 12:27:53 +0100

    12.27

--- codigo_ene_22_26/ecuacion.py
+++ codigo_ene_22_26/ecuacion.py
@@ -7,4 +7,16 @@ x1 y x2
 import math
 
 # inicializar variables:
-a, b, c = 1, 5, 4  # a = 1 b = 5 c = 4
+#a, b, c = 1, 5, 4  # a = 1 b = 5 c = 4
+a,b,c=1,2,3
+
+raiz = b**2 - 4*a*c
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2*a)
+    x2 = (-b - math.sqrt(raiz)) / (2*a)
+
+    print('x1:', x1, sep='')
+    print(f'x2:{x2}')
+else:
+    print('No se puede calcular')
+print('fuera')
