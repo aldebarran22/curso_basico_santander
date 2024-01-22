c77e7f5abe695166417561979616a3e8dcb8a9be
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-22 11:52:38 +0100

    11.52

--- codigo_ene_22_26/tipos_basicos.py
+++ codigo_ene_22_26/tipos_basicos.py
@@ -12,3 +12,16 @@ print(resul, type(resul))
 # El resultado será complex
 resul2 = 10 * 2.5 + (8 + 7j)
 print(resul2, type(resul2))
+
+# Convertir de texto a número:
+numero = "1234.99"
+numero2 = '456.67'
+resul = numero + numero2
+print("resul:",resul)
+num1 = float(numero)
+num2 = float(numero2)
+resul = num1 + num2
+print("resul:",resul)
+
+
+
