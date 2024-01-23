a8caea3529d60f763f60efa920919d46763eb5f3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-23 14:34:07 +0100

    14.34

--- codigo_ene_22_26/prueba_ecuacion.py
+++ codigo_ene_22_26/prueba_ecuacion.py
@@ -1,12 +1,29 @@
 """
 Importar el módulo funciones_ecuacion y utilizar
 la función ecuación.
-import modulo
-import modulo as alias
-from modulo import funcion
+1) import modulo
+2) import modulo as alias
+3) from modulo import funcion
 """
 
+"""
+# opción 1
 import funciones_ecuacion
 
 resul = funciones_ecuacion.ecuacion(1, 5, 4)
+print(resul)
+"""
+
+"""
+# opción 2
+import funciones_ecuacion as fe
+
+resul = fe.ecuacion(1, 5, 4)
+print(resul)
+"""
+
+# opción 3
+from funciones_ecuacion import ecuacion
+
+resul = ecuacion(1, 5, 4)
 print(resul)
