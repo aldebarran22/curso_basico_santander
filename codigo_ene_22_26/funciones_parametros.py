a71019d3b397b507db54026eb7e2312d1ceef47b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-23 13:35:56 +0100

    13.35

--- codigo_ene_22_26/funciones_parametros.py
+++ codigo_ene_22_26/funciones_parametros.py
@@ -7,12 +7,20 @@ Tipos de parámetros en una función.
 
 Tipos de llamada  a una función(con respecto a los parámetros):
 - Posicional
+- Nominal: con los nombres de los parámetros
 """
 
 def funcion(a,b):
     print('obligatorios: ',a,b)
     print()
 
+def funcion2(a=10, b=20):
+    print('opcionales: ',a,b)
+    print()
+
 if __name__ == '__main__':
-    funcion(1,2) # forma posicional
+    funcion(1,2)        # forma posicional
+    funcion(b=2, a=1)   # forma nominal
+
+    funcion2(b=200)
 
