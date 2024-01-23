47d95be4276df69bd4d781625e79f437b36206b0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-23 12:09:49 +0100

    12.09

--- codigo_ene_22_26/control_flujo.py
+++ codigo_ene_22_26/control_flujo.py
@@ -42,6 +42,13 @@ while True:
         if numero % 10 == 0:
             break
         else:
-            print('Solo se aceptan múltiplos de 10')
+            print("Solo se aceptan múltiplos de 10")
     else:
-        print('Solo aceptan números')
+        print("Solo aceptan números")
+
+    if intentos == 3:
+        print("Ha consumido los 3 intentos")
+        break
+    
+if numero % 10 == 0:
+    print("El importe es: ", numero)
