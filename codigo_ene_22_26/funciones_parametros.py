9816422c226de8eb61f08a3878e112fa5595f676
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-23 14:13:09 +0100

    14.13

--- codigo_ene_22_26/funciones_parametros.py
+++ codigo_ene_22_26/funciones_parametros.py
@@ -10,32 +10,48 @@ Tipos de llamada  a una función(con respecto a los parámetros):
 - Nominal: con los nombres de los parámetros
 """
 
-def funcion(a,b):
-    print('obligatorios: ',a,b)
+
+def funcion(a, b):
+    print("obligatorios: ", a, b)
     print()
+
 
 def funcion2(a=10, b=20):
-    print('opcionales: ',a,b)
+    print("opcionales: ", a, b)
     print()
 
-def funcion3(a,b,d,c=0):
+
+def funcion3(a, b, d, c=0):
     pass
 
+
 def funcion4(*args):
-    print("args: ",args)
+    print("args: ", args)
     print()
+
 
 def funcion5(obligatorio, opcional=10, *args, **kwargs):
-    print('obligatorio:', obligatorio)
-    print('opcional:', opcional)
-    print('args: ', args)
-    print('kwargs: ', kwargs)
+    print("obligatorio:", obligatorio)
+    print("opcional:", opcional)
+    print("args: ", args)
+    print("kwargs: ", kwargs)
     print()
 
-if __name__ == '__main__':
-    funcion(1,2)        # forma posicional
-    funcion(b=2, a=1)   # forma nominal
 
-    funcion2(b=200)     # forma nominal.
-    funcion4(1,2,3,4,5) # Se recibe como una tupla
+def plot(**kwargs):
+    # Valores por defecto
+    d = {"color": "red", "grosor": 1.0}
+    d.update(kwargs)
+    print("pinta gráfica de color: ", d["color"], "y grosor:", d["grosor"])
 
+
+if __name__ == "__main__":
+    plot(color="green")
+    exit()
+    funcion5(0, 1, 2, 3, 4, 5, x=100, y=200)
+
+    funcion(1, 2)  # forma posicional
+    funcion(b=2, a=1)  # forma nominal
+
+    funcion2(b=200)  # forma nominal.
+    funcion4(1, 2, 3, 4, 5)  # Se recibe como una tupla
