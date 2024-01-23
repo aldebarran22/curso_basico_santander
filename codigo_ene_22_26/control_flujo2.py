227ad132f3cb7144739c864eb9a4d4d08cb9ef4c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-23 12:20:01 +0100

    12.19

--- codigo_ene_22_26/control_flujo2.py
+++ codigo_ene_22_26/control_flujo2.py
@@ -1,11 +1,14 @@
 # Validación de números con 3 intentos posibles
 intentos = 0
+numero = 0
+correcto = False
 while True:
     intentos += 1
     snumero = input("Teclear importe:>")
     if snumero.isnumeric():
         numero = int(snumero)
         if numero % 10 == 0:
+            correcto = True
             break
         else:
             print("Solo se aceptan múltiplos de 10")
@@ -16,5 +19,5 @@ while True:
         print("Ha consumido los 3 intentos")
         break
 
-if numero % 10 == 0:
+if correcto:
     print("El importe es: ", numero)
