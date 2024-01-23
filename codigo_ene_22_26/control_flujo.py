6a740e64eccc9f52eb8c02c6085698c337fe5bf7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-23 12:07:57 +0100

    12.07

--- codigo_ene_22_26/control_flujo.py
+++ codigo_ene_22_26/control_flujo.py
@@ -19,8 +19,29 @@ while i <= 10:
     i += 1
 
 # Validación de teclado: sólo acepta múltiplos de 10
+"""
 numero = int(input("Teclear importe:>"))
-while numero % 10 != 0:
+intento = 1
+while numero % 10 != 0 and intento < 3:
     print("Sólo se aceptan múltiplos de 10")
     numero = int(input("Teclear importe:>"))
-print("El importe es: ", numero)
+    intento += 1
+if numero % 10 == 0:
+    print("El importe es: ", numero)
+else:
+    print("ha consumido los 3 intentos")
+"""
+
+# Validación de números con 3 intentos posibles
+intentos = 0
+while True:
+    intentos += 1
+    snumero = input("Teclear importe:>")
+    if snumero.isnumeric():
+        numero = int(snumero)
+        if numero % 10 == 0:
+            break
+        else:
+            print('Solo se aceptan múltiplos de 10')
+    else:
+        print('Solo aceptan números')
