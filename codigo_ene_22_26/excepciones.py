b7ff785d7ada2c21f0ad7a768c40dc88399073e3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-24 14:35:04 +0100

    14.34

--- codigo_ene_22_26/excepciones.py
+++ codigo_ene_22_26/excepciones.py
@@ -2,8 +2,28 @@
 Control de excepciones en Python
 """
 
-def test1():
-    pass
 
-if __name__ == '__main__':
-    test1()
+def test1():
+    # Capturar una excepción concreta
+    try:
+        L = [1, 3, 4, 5, 6]
+        print(L[15])
+        print("última línea")
+    except IndexError as e:
+        print(e)
+    #print("fin")
+        
+def test2():
+    # Capturar una excepción concreta pero falla otra excepción distinta
+    try:
+        L = [1, 3, 4, 5, 6]
+        print(100/0)
+        print(L[15])
+        print("última línea")
+    except IndexError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    #test1()
+    test2()
