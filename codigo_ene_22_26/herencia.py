a46fd0f58f0b4c270d79037b94a985a40605aa73
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-24 13:38:45 +0100

    13.38

--- codigo_ene_22_26/herencia.py
+++ codigo_ene_22_26/herencia.py
@@ -33,6 +33,14 @@ class Persona:
         # Vuelve a llamar al método __str__()
         return str(self)
 
+
+class Extranjero(Persona):
+    def __init__(self, nombre="", edad=0, altura=0.0, idiomas=[]):
+        Persona.__init__(self, nombre, edad, altura)
+        # super(nombre, edad, altura)
+        self.idiomas = idiomas
+
+
 def testPersona():
     p1 = Persona("Ana")
     p2 = Persona("Tomas")
@@ -40,5 +48,14 @@ def testPersona():
     p2.hablarCon()
     p2.hablarCon(p1)
 
-if __name__ == '__main__':
-    testPersona()
+def testExtranjero():
+    ex1 = Extranjero("Pedro", 24, 1.77, ['inglés','francés'])
+    ex2 = Extranjero("Sara", 22, 1.67, ['inglés','francés','alemán'])
+    print(ex1)
+    ex1.cumple()
+    print(ex1)
+    ex1.hablarCon(ex2)
+
+if __name__ == "__main__":
+    #testPersona()
+    testExtranjero()
