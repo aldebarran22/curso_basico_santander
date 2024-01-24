e0f0f2c4d3474070e3055b0562ffa36ce8791d35
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-24 14:10:59 +0100

    14.10

--- codigo_ene_22_26/fecha_hora.py
+++ codigo_ene_22_26/fecha_hora.py
@@ -21,3 +21,16 @@ class Date:
 
     def __str__(self):
         return "%02d/%02d/%04d" % (self.d, self.m, self.y)
+
+class DateTime(Time, Date):
+
+    def __init__(self, d=1, m=1, y=1970, hh=0, mm=0, ss=0):
+        Date.__init__(self, d,m,y)
+        Time.__init__(self, hh,mm,ss)
+
+    def __str__(self):
+        return Date.__str__(self)+" "+Time.__str__(self)
+    
+if __name__ == '__main__':
+    dt = DateTime()
+    print(dt)
