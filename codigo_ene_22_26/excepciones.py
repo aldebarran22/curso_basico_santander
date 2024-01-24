7cb8258425c973554dfcb3771e35f21976ee7343
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-24 14:55:18 +0100

    14.55

--- codigo_ene_22_26/excepciones.py
+++ codigo_ene_22_26/excepciones.py
@@ -11,27 +11,30 @@ def test1():
         print("última línea")
     except IndexError as e:
         print(e)
-    #print("fin")
-        
+    # print("fin")
+
+
 def test2():
     # Capturar una excepción concreta pero falla otra excepción distinta
     try:
         L = [1, 3, 4, 5, 6]
-        print(100/0)
+        print(100 / 0)
         print(L[15])
         print("última línea")
     except IndexError as e:
         print(e)
 
+
 def test3():
     # Solución al caso test2
     try:
         L = [1, 3, 4, 5, 6]
-        print(100/10)
+        print(100 / 10)
         print(L[15])
         print("última línea")
     except Exception as e:
         print(e.__class__.__name__, e)
+
 
 def test4():
     #  Una captura y otra más general
@@ -39,22 +42,19 @@ def test4():
         num = 100
         nombre = "hola"
         d = {"a": 1, "b": 2, "c": 3}
-        print(d["a"])
-        print(num+num+nombre)
+        print(d["d"])
+        print(num + num + nombre)
         print("última línea")
 
-    except KeyError as e:
-        print('Falla la clave del dict: ', e)
+    except (IndexError, KeyError) as e:
+        print("Falla la clave : ", e)
 
     except Exception as e:
         print(e.__class__.__name__, e)
 
-    
-
-    
 
 if __name__ == "__main__":
-    #test1()
-    #test2()
-    #test3()
+    # test1()
+    # test2()
+    # test3()
     test4()
