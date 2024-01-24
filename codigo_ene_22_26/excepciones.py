57a0619112c3594b6e0021094409fd765eba4911
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-24 14:44:59 +0100

    14.44

--- codigo_ene_22_26/excepciones.py
+++ codigo_ene_22_26/excepciones.py
@@ -23,7 +23,38 @@ def test2():
     except IndexError as e:
         print(e)
 
+def test3():
+    # Solución al caso test2
+    try:
+        L = [1, 3, 4, 5, 6]
+        print(100/10)
+        print(L[15])
+        print("última línea")
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+def test4():
+    #  Una captura y otra más general
+    try:
+        num = 100
+        nombre = "hola"
+        d = {"a": 1, "b": 2, "c": 3}
+        print(d["a"])
+        print(num+num+nombre)
+        print("última línea")
+
+    except KeyError as e:
+        print('Falla la clave del dict: ', e)
+
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+    
+
+    
 
 if __name__ == "__main__":
     #test1()
-    test2()
+    #test2()
+    #test3()
+    test4()
