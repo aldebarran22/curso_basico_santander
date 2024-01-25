1b9e8aeec11b2a917f2a043a554d3010c87545b3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-25 13:47:56 +0100

    13.47

---
+++ codigo_ene_22_26/expresiones_reg.py
@@ -0,0 +1,17 @@
+"""
+Expresiones regulares en Python
+"""
+import re
+
+
+def comprobar(patron, L):
+    print("Patron:", patron)
+    for i in L:
+        print(i, re.match(patron, i))
+    print("-" * 15)
+
+
+# Validar DNI
+patron = ""
+L = ["12345678A", "12", "123B", "12345678AB", "12345678a"]
+comprobar(patron, L)
