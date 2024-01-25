2fc8f039e89fe578ba1fdbd9038b39b2d3eeaf98
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-25 14:11:41 +0100

    14.11

--- codigo_ene_22_26/expresiones_reg.py
+++ codigo_ene_22_26/expresiones_reg.py
@@ -22,6 +22,17 @@ comprobar(patron, L)
 
 
 # Validar DNI
-patron = ""
+patron = "^[0-9]{1,8}[A-Z]$"
 L = ["12345678A", "12", "123B", "12345678AB", "12345678a"]
+comprobar(patron, L)
+
+"""
+EXPRESIONES REGULARES:
+1. Comprobar la validez del siguiente código:
+Palabra COD o S/N, un guión bajo, 3 vocales mayúsculas, guión bajo y 6 números que no pueden empezar por 0.
+COD_AEE_800959
+S/N_UOO_958474
+"""
+patron = ""
+L = ["COD_AEE_800959", "S/N_UOO_958474"]
 comprobar(patron, L)
