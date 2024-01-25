3e5c00db98a46c3ef1908ca1728af3a893ed61fe
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-25 10:38:53 +0100

    10.38

--- codigo_ene_22_26/ficheros.py
+++ codigo_ene_22_26/ficheros.py
@@ -7,7 +7,26 @@ def leerFichero(path):
     try:
         fich = open(path, "r")
         for linea in fich.readlines():
+            linea = linea.rstrip()
             print(linea)
+
+    except Exception as e:
+        print(e)
+    finally:
+        if fich: fich.close()
+
+def sumaImporte(path, pais, sep=';'):
+    fich = None
+    importe = 0
+    try:
+        fich = open(path, "r")
+        for linea in fich.readlines():
+            linea = linea.rstrip()
+            L = linea.split(sep)
+            if L[-1] == pais:
+                importe += float(L[-2])
+                
+        return importe
 
     except Exception as e:
         print(e)
