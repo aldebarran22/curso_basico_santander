464a7faa7328fc5c13c1611b8e8d6313491f8310
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-25 12:31:10 +0100

    12.31

--- codigo_ene_22_26/app_tiempo/modulos/fechahora.py
+++ codigo_ene_22_26/app_tiempo/modulos/fechahora.py
@@ -1,3 +1,11 @@
+try:
+    from modulos.fecha import Date
+    from modulos.hora import Time
+except:
+    from fecha import Date
+    from hora import Time
+
+
 class DateTime(Time, Date):
     def __init__(self, d=1, m=1, y=1970, hh=0, mm=0, ss=0):
         Date.__init__(self, d, m, y)
