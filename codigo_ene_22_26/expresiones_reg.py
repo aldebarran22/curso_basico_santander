a8592bdc248bc4344516365a26281896d33eefb9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-25 14:05:06 +0100

    14.05

--- codigo_ene_22_26/expresiones_reg.py
+++ codigo_ene_22_26/expresiones_reg.py
@@ -7,13 +7,17 @@ import re
 def comprobar(patron, L):
     print("Patron:", patron)
     for i in L:
-        print(i, True if re.search(patron, i) else False)
+        print(
+            i,
+            True if re.match(patron, i) else False,
+            True if re.search(patron, i) else False,
+        )
     print("-" * 15)
 
 
 # Validar palabras de 3 letras que terminan en .
 patron = r"^...\.$"
-L = ["abc.","123.","adv", "123", "abc.5", "1234."]
+L = ["abc.", "123.", "adv", "123", "abc.5", "1234.", "*-0."]
 comprobar(patron, L)
 
 
