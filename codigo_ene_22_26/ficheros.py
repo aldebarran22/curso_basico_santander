71a9dc11a6f0c442ecdd0e72c3926a4cec1a7e26
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-25 10:31:18 +0100

    10.31

--- codigo_ene_22_26/ficheros.py
+++ codigo_ene_22_26/ficheros.py
@@ -6,6 +6,8 @@ def leerFichero(path):
     fich = None
     try:
         fich = open(path, "r")
+        for linea in fich.readlines():
+            print(linea)
 
     except Exception as e:
         print(e)
@@ -13,5 +15,5 @@ def leerFichero(path):
         if fich: fich.close()
 
 if __name__ == "__main__":
-    leerFichero('no.txt')
+    leerFichero('../ficheros/Pedidos.txt')
 
