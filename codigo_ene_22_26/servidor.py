9ff847bbbe7288b988012e4878a30c5c4934aeaa
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-26 11:35:25 +0100

    11.35

---
+++ codigo_ene_22_26/servidor.py
@@ -0,0 +1,23 @@
+"""
+Servidor TCP en Python
+"""
+import socket
+
+s = socket.socket()
+print("Socket creado!")
+s.bind(("localhost", 9999))
+print("Bind ok!")
+s.listen(1)
+print("Servidor a la espera de clientes ...")
+sc, addr = s.accept()
+print("Cliente conectado: ", addr)
+while True:
+    recibido = sc.recv(1024)
+    recibido = recibido.decode("utf-8")
+    if recibido == "quit":
+        break
+    print("Recibido:", recibido)
+    sc.send(recibido.encode("utf-8"))
+print("fin comunicación")
+sc.close()
+s.close()
