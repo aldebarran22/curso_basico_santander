095b0e2d6d180d3834e8486fbd929229ddc9b221
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-26 09:36:59 +0100

    9.36

--- codigo_ene_22_26/base_datos.py
+++ codigo_ene_22_26/base_datos.py
@@ -15,8 +15,11 @@ def listarTabla(path, tabla):
             cur = con.cursor()
             sql = f"select * from {tabla}"
             cur.execute(sql)
+            cabs = ";".join([t[0] for t in cur.description])
+            print(cabs)
             for t in cur.fetchall():
-                print(t)
+                linea = ";".join([str(i) for i in t])
+                print(linea)
 
         else:
             raise FileNotFoundError(f"El fichero {path} no existe...")
