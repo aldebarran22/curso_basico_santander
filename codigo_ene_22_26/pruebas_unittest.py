55e9af7d90699433695e02c0560749278ab5d96e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-26 14:20:48 +0100

    14.20

--- codigo_ene_22_26/pruebas_unittest.py
+++ codigo_ene_22_26/pruebas_unittest.py
@@ -14,14 +14,18 @@ class PruebasFicheros(unittest.TestCase):
         por cada país
         """
         path = "../ficheros_curso/Pedidos.txt"
-        path2 = "../ficheros"
+        path2 = "../ficheros_curso"
         L = ["Alemania", "Francia", "Italia"]
         L.sort()
         ficheros.exportarPaises(path, *L)
         Lficheros = os.listdir(path2)
         Lficheros.sort()
         L2 = [f.partition(".")[0] for f in Lficheros]
-        self.assertEqual(L, L2, msg="No coinciden los ficheros con los países")
+        for f in L:
+            if f not in L2:
+                self.fail(f"No se encuentra un fichero para {f}")
+
+        # self.assertEqual(L, L2, msg="No coinciden los ficheros con los países")
 
 
 if __name__ == "__main__":
