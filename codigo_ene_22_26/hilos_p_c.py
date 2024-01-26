5ba05578a2d6e848a5fd511c6a008ec9e81b729f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-26 12:43:43 +0100

    12.43

---
+++ codigo_ene_22_26/hilos_p_c.py
@@ -0,0 +1,69 @@
+"""
+Implementación del productor-consumidor en Python
+Solución M productores-N consumidores
+"""
+from threading import Thread, Lock, Semaphore
+from random import randint
+from time import sleep
+
+num_muestras = 20
+tam_buffer = 10
+
+num_productores = 2
+num_consumidores = 1
+
+
+class Productor(Thread):
+    def __init__(self, buffer, num_muestras, nombre):
+        Thread.__init__(self, name=nombre)
+        self.buffer = buffer
+        self.num_muestras = num_muestras
+
+    def run(self):
+        pass
+
+
+class Consumidor(Thread):
+    def __init__(self, buffer):
+        Thread.__init__(self)
+        self.buffer = buffer
+
+    def run(self):
+        pass
+
+
+class TBuffer:
+    def __init__(self):
+        self.ind_c = 0
+        self.ind_p = 0
+        self.buffer = [-1] * tam_buffer
+        self.mutex = Lock()
+        self.sem_huecos = Semaphore(tam_buffer)
+        self.sem_items = Semaphore(0)
+
+
+if __name__ == "__main__":
+    if num_muestras % num_productores != 0 or num_muestras % num_consumidores != 0:
+        print("El número tiene que estar equilibrado entre el n. de prod/con")
+        exit()
+
+    # Calcular el numero de muestra por productor y consumidor:
+    num_muestras_prod = num_muestras // num_productores
+    num_muestras_con = num_muestras // num_consumidores
+
+    buf = TBuffer()
+
+    con = Consumidor(buf)
+    con.start()
+
+    productores = []
+    for i in range(num_productores):
+        nombre = "P" + str(i + 1)
+        pro = Productor(buf, num_muestras_prod, nombre)
+        pro.start()
+        productores.append(pro)
+
+    con.join()
+
+    for pro in productores:
+        pro.join()
