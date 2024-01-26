e477b49f9a7937f178a113dbc56268d8aa313a16
aldebarran22 <antonioespinherranz@hotmail.com>
2024-01-26 14:14:13 +0100

    14.14

--- codigo_ene_22_26/pruebas_unittest.py
+++ codigo_ene_22_26/pruebas_unittest.py
@@ -6,22 +6,23 @@ import unittest
 import ficheros
 import os
 
-class PruebasFicheros(unittest.TestCase):
 
+class PruebasFicheros(unittest.TestCase):
     def testVariosPaises(self):
         """
         Exportar varios paises y comprobar si se genera un fichero
         por cada país
         """
-        path = "../ficheros/Pedidos.txt"
-        L = ["Alemania","Francia","Italia"]
+        path = "../ficheros_curso/Pedidos.txt"
+        path2 = "../ficheros"
+        L = ["Alemania", "Francia", "Italia"]
+        L.sort()
         ficheros.exportarPaises(path, *L)
-        Lficheros = os.listdir(path)
-        L2 = [f.partition('.')[0] for f in Lficheros]
+        Lficheros = os.listdir(path2)
+        Lficheros.sort()
+        L2 = [f.partition(".")[0] for f in Lficheros]
         self.assertEqual(L, L2, msg="No coinciden los ficheros con los países")
 
 
-  
-    
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
