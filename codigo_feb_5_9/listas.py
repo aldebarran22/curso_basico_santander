2b5f7bce723a538acb119504ff259e12f8b555c9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 12:36:13 +0100

    12.36

--- codigo_feb_5_9/listas.py
+++ codigo_feb_5_9/listas.py
@@ -4,5 +4,10 @@ Definición, acceso a los elementos
 índices, funciones ...
 """
 
-L = [3,4,5,6,7]
+L = [3, 4, 5, 6, 7]
 print(L, type(L))
+print("suma: ", sum(L))
+print("recuento:", len(L))
+print("minimo:", min(L))
+print("maximo:", max(L))
+print("promedio:", sum(L) / len(L))
