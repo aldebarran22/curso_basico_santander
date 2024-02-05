b5745e1c89527618ef02142308f3bcbbdbe0c799
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 13:53:13 +0100

    13.53

---
+++ codigo_feb_5_9/prc_listas.py
@@ -0,0 +1,12 @@
+"""
+Partimos de dos listas con números y queremos obtener la intersección
+en otra tercera lista.
+L1 = [1,2,3,4,5,6,7,4,5,6]
+L2 = [6,7,5,4,11,15]
+R = [4,5,6,7] no puede tener repetidos
+"""
+
+L1 = [1, 2, 3, 4, 5, 6, 7, 4, 5, 6]
+L2 = [6, 7, 5, 4, 11, 15]
+# Crea una lista vacía
+R = []  # R = list()
