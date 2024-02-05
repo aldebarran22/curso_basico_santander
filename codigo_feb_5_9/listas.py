b48157645caaaf04545415e6e9852fc6280f7c89
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 13:21:01 +0100

    13.20

--- codigo_feb_5_9/listas.py
+++ codigo_feb_5_9/listas.py
@@ -68,4 +68,16 @@ for nombre in L:
     if prefijo in nombre:
         print(nombre[n:])    
 
+# Modificar elementos de una lista:
+L = [3,4,5,6,7]
+print(L)
+L[0] = 30   # Machaca el valor de la posición 0
+L[-1] *= 2  # Duplica el último valor
+print(L)
+# range
+
+nombre = "ana"
+# Intento modificar la primera letra:
+nombre[0] = 'A'
+
 
