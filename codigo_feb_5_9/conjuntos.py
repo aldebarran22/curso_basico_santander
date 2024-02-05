17592fb6f0ce5d6ed171ccdd69290c91a0545342
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 14:59:00 +0100

    14.58

---
+++ codigo_feb_5_9/conjuntos.py
@@ -0,0 +1,14 @@
+"""
+Operaciones entre conjuntos: Operadores
+intersección: &
+unión: |
+diferencia: -
+dif. simétrica: ^
+"""
+comida = ['Miguel','Tomás','Raquel','Eva','Raúl']
+cena = ['Tomás','Eva','Sandra','Ana','Juan']
+
+print('Quien va a comer y a cenar:')
+print('Quienes van solo a comer:')
+print('Quienes van solo a un evento:')
+print('Quienes han participado en los eventos:')
