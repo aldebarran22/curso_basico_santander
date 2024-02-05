9fcb9ebc5d21ec0c6364ec7f8fe83093a58429a0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 11:49:53 +0100

    11.49

--- codigo_feb_5_9/prc_ecuacion.py
+++ codigo_feb_5_9/prc_ecuacion.py
@@ -2,12 +2,17 @@
 Resolver una ecuación de 2º grado en Python
 ax^2 + bx + c = 0
 """
+
 import math
 
 a = 1
-b = 5
-c = 4
+b = 2
+c = 3
 
-x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
-print(x1, x2)
+raiz = b**2 - 4 * a * c
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2 * a)
+    x2 = (-b - math.sqrt(raiz)) / (2 * a)
+    print(x1, x2)
+else:
+    print('No hay solución')
