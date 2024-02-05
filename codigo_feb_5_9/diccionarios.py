caaaecaec5ac694a61f7b2725c27487570e610c3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 14:36:09 +0100

    14.36

--- codigo_feb_5_9/diccionarios.py
+++ codigo_feb_5_9/diccionarios.py
@@ -4,10 +4,11 @@ Definir dicc
 Acceso a los elementos
 Bucles
 """
-usuario = {"nombre":"Jorge", "edad":34, "altura":1.89}
-print('El nombre:', usuario["nombre"])
+
+usuario = {"nombre": "Jorge", "edad": 34, "altura": 1.89}
+print("El nombre:", usuario["nombre"])
 # Incrementar la edad:
-usuario["edad"]+=1
+usuario["edad"] += 1
 print(usuario, type(usuario))
 usuario["peso"] = 78
 print(usuario)
@@ -20,7 +21,25 @@ print(Lcabs)
 print(Ldatos)
 d = dict(zip(Lcabs, Ldatos))
 print(d)
-print(d['nombre'])
-print('Todas las claves:')
+print(d["nombre"])
+print("Todas las claves:")
 for col, valor in d.items():
     print(col, valor)
+
+L1 = "ana"  # Ojo la clave 'a' se machaca con el valor 3.
+L2 = [1, 2, 3]
+d = dict(zip(L1, L2))
+d2 = dict(zip(L2, L1))
+print(d)
+print(d2)
+
+# Histograma: 1:4, 2:4, 4:2
+L = [1,2,2,2,1,1,1,2,4,5,5,6,7,6,4]
+k = set(L) # Quitar repetidos.
+print(k)
+histo = dict()
+for i in k:
+    histo[i] = L.count(i)
+print(histo)
+
+
