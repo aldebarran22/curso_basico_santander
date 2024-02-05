55f15aafeb06db0d6fa274ca1e8af7ad1a3a92c1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 14:09:44 +0100

    14.09

---
+++ codigo_feb_5_9/diccionarios.py
@@ -0,0 +1,11 @@
+"""
+Diccionarios en Python.
+Definir dicc
+Acceso a los elementos
+Bucles
+"""
+usuario = {"nombre":"Jorge", "edad":34, "altura":1.89}
+print('El nombre:', usuario["nombre"])
+# Incrementar la edad:
+usuario["edad"]+=1
+print(usuario, type(usuario))
