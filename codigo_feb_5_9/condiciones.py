3a0d9a7a417de0231c0ed14927ac2a37d3aa5ad7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 12:07:14 +0100

    12.07

--- codigo_feb_5_9/condiciones.py
+++ codigo_feb_5_9/condiciones.py
@@ -4,7 +4,7 @@ Operadores relacionales y lógicos en Python
 
 ini = 10
 fin = 50
-numero = 64
+numero = 25
 
 if numero >= ini and numero <= fin:
     print(numero, "dentro del intervalo", ini, fin)
@@ -15,3 +15,8 @@ if ini <= numero <= fin:
     print(numero, "dentro del intervalo", ini, fin)
 else:
     print("No está en el intervalo")
+
+if numero < ini or numero > fin:
+    print("No está en el intervalo")
+else:
+    print(numero, "dentro del intervalo", ini, fin)
