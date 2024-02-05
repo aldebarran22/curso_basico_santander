3020cd8523de9a9fad5bb79d4d5e27f52acdacc3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-05 12:46:53 +0100

    12.46

--- codigo_feb_5_9/listas.py
+++ codigo_feb_5_9/listas.py
@@ -6,8 +6,23 @@ Definición, acceso a los elementos
 
 L = [3, 4, 5, 6, 7]
 print(L, type(L))
-print("suma: ", sum(L))
+print("suma: ", sum(L))  # funcion(variable)
 print("recuento:", len(L))
 print("minimo:", min(L))
 print("maximo:", max(L))
 print("promedio:", sum(L) / len(L))
+
+nombre = "Ana"
+print(len(nombre))
+
+# Acceso a los elementos de la lista:
+print(L)
+print("El primero de la lista:", L[0])
+print("El primero de la lista:", L[-5])
+print("El último de la lista:", L[-1])
+print("El último de la lista:", L[4])
+
+path = "C:/mis documentos/excel/Libro1.xlsx"
+L2 = path.split("/")
+print(L2)
+print("Fichero:", L2[-1])
