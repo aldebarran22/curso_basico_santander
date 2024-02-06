c56a2d241fbcb5d63a3c028a258768fb6b4c3315
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 09:28:20 +0100

    9.28

--- codigo_feb_5_9/tuplas.py
+++ codigo_feb_5_9/tuplas.py
@@ -4,15 +4,22 @@ Tuplas en Python:
 - Intercambiar variables
 - Paso de parámetros a una función sin indicar el número
 - En los resultados de una consulta a la base de datos
+- Setencias parametrizadas:
+- select * from clientes where pais=? ('España',)
 """
 
 # Expansión de tuplas
-a,b,c = 1,2,3
+a, b, c = 1, 2, 3
 
-t = 1,2,3
-t2 = (4,5,6)
-print(t,t2,type(t),type(t2))
+t = 1, 2, 3
+t2 = (4, 5, 6)
+print(t, t2, type(t), type(t2))
 
+# Crear una tupla de un solo elemento:
 t3 = (1,)
 print(t3, type(t3))
 
+a = 10
+b = 20
+a,b = b,a
+print('a',a, 'b',b)
