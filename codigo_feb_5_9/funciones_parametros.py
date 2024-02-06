23b4f1bc7347de5b6c87ebc1243309900e0634ec
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 13:13:49 +0100

    13.13

---
+++ codigo_feb_5_9/funciones_parametros.py
@@ -0,0 +1,10 @@
+"""
+Definir una función con distintos tipos de parámetros
+"""
+
+def funcion(ob1, ob2, op1=1, op2=2):
+    print('obligatorios:', ob1, ob2)
+    print('opcionales: ', op1, op2)
+    print()
+
+funcion()
