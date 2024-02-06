4612490814e0eec4be25f432facdc5d151240023
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 10:56:42 +0100

    10.56

--- codigo_feb_5_9/control_flujo.py
+++ codigo_feb_5_9/control_flujo.py
@@ -10,9 +10,20 @@ if compacto
 #    pass
     
 
+# Opción 1:
 while True:
     numero = int(input('Dame un multiplo de 10:'))
     if numero % 10 != 0:
         print('Sólo múltiplos de 10')
     else:
         print('ok, múltiplo de 10: ', numero)
+        break # Rompe el bucle
+
+# Opción 2:
+numero = int(input('Dame un multiplo de 10:'))
+print('opcion 2:')
+while numero % 10 != 0:
+    print('Sólo múltiplos de 10')
+    numero = int(input('Dame un multiplo de 10:'))
+print('ok, múltiplo de 10: ', numero)
+
