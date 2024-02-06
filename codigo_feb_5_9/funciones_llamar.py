47cf2ace443d41e08548e35aa770a9a909bc8337
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 12:16:17 +0100

    12.16

--- codigo_feb_5_9/funciones_llamar.py
+++ codigo_feb_5_9/funciones_llamar.py
@@ -13,6 +13,6 @@ def sumar(a, b):
 
 print(sumar(1, 3))  # Posicional
 print(sumar(b=3, a=1))  # Nominal
-t = (1,3)
-print(sumar(t[0], t[1])) # incómodo!!!
-print(sumar(*t)) # Con el * desempaqueta la tupla
+t = (1, 3)
+print(sumar(t[0], t[1]))  # incómodo!!!
+print(sumar(*t))  # Con el * desempaqueta la tupla
