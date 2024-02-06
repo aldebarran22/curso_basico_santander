2bb33793db46dadc0efcf45e6e44df0bddf2a198
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 09:22:30 +0100

    9.22

---
+++ codigo_feb_5_9/tuplas.py
@@ -0,0 +1,18 @@
+"""
+Tuplas en Python: 
+- Inicializar múltiples variables
+- Intercambiar variables
+- Paso de parámetros a una función sin indicar el número
+- En los resultados de una consulta a la base de datos
+"""
+
+# Expansión de tuplas
+a,b,c = 1,2,3
+
+t = 1,2,3
+t2 = (4,5,6)
+print(t,t2,type(t),type(t2))
+
+t3 = (1,)
+print(t3, type(t3))
+
