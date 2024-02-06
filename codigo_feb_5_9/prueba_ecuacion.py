b4d38f18fb5f596f774b98132a95d2f47000d571
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 13:08:35 +0100

    13.08

---
+++ codigo_feb_5_9/prueba_ecuacion.py
@@ -0,0 +1,12 @@
+"""
+Importar la función ecuGrado2 del módulo ecuación
+Y calcular una ecuación
+"""
+
+# import ecuacion
+# print(ecuacion.ecuGrado2(1,5,4))
+
+# from modulo import funcion
+from ecuacion import ecuGrado2
+
+print(ecuGrado2(1,5,4))
