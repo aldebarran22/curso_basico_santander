0ec816d97f6a1880f59fcf0ec0e6c22b0de7edbc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 13:59:08 +0100

    13.59

--- codigo_feb_5_9/funciones_param_ref.py
+++ codigo_feb_5_9/funciones_param_ref.py
@@ -8,7 +8,8 @@ Parámetros mutables SI se modifican (por referencia)
 
 def incrementar(numero):
     numero += 1
-    print("en incrementar, numero: ", numero, "id:",id(numero))
+    print("en incrementar, numero: ", numero, "id:", id(numero))
+
 
 def añadir(L):
     L.append(123)
@@ -17,10 +18,12 @@ def añadir(L):
 
 if __name__ == "__main__":
     num = 100
-    print('en main: id: ', id(num))
+    print("en main: id: ", id(num))
     incrementar(num)  # manda una copia!
     print("num:", num)
 
-    lista = [1,2,3,4]
+    lista = [1, 2, 3, 4]
     añadir(lista)
+    # Si no queremos modificar la lista se puede enviar una copia:
+    # añadir(lista.copy())
     print(lista, id(lista))
