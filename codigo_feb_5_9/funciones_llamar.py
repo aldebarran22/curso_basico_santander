b10687818c9ed0de42fe9d28f9b700e1cc63de7e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 12:10:46 +0100

    12.10

---
+++ codigo_feb_5_9/funciones_llamar.py
@@ -0,0 +1,14 @@
+"""
+Formas de llamar a una función en Python
+1) Posicional
+2) Nominal
+3) Con una tupla / lista
+4) Con un diccionario
+"""
+
+
+def sumar(a, b):
+    return a + b
+
+
+print(sumar(1, 3))  # Posicional
