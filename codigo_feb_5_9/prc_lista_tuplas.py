cba2be4791f0252c5c43de7adeb707870ef75672
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 09:57:25 +0100

    9.57

---
+++ codigo_feb_5_9/prc_lista_tuplas.py
@@ -0,0 +1,12 @@
+"""
+Filtrar los ficheros de una carpeta a partir
+de la extensión.
+Buscar al menos dos extensiones distintas.
+txt, csv
+"""
+
+import os
+
+L = os.listdir()
+for i in L:
+    print(i)
