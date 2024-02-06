eee3fda44fe3065fc80addf3c9568c89fdf68f26
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-06 13:18:25 +0100

    13.18

--- codigo_feb_5_9/funciones_parametros.py
+++ codigo_feb_5_9/funciones_parametros.py
@@ -2,9 +2,13 @@
 Definir una función con distintos tipos de parámetros
 """
 
-def funcion(ob1, ob2, op1=1, op2=2):
-    print('obligatorios:', ob1, ob2)
-    print('opcionales: ', op1, op2)
+def funcion(ob1, ob2, op1=1, op2=2, *args, **kwargs):
+    print("obligatorios:", ob1, ob2)
+    print("opcionales: ", op1, op2)
+    print("args:", args)
+    print("kwargs:", kwargs)
     print()
 
-funcion()
+funcion(10, 20)
+funcion(10, 20, op2=200)  # Me salto el op1
+funcion(1,2,3,4,5,6,7,8,x=-1, y=-2)
