a30a917827ff7502a599886a40bfd459a724d23b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 14:25:33 +0100

    14.25

--- codigo_feb_5_9/excepciones.py
+++ codigo_feb_5_9/excepciones.py
@@ -9,32 +9,52 @@ def test1():
     try:
         d = {"a": 1, "b": 2}
         L = [1, 2, 3, 4]
-        print(d["d"]) # lanza keyError
+        print(d["d"])  # lanza keyError
         print(L[10])  # IndexError
     except KeyError as e:
         print(e.__class__.__name__, e)
+
 
 def test2():
     # Capturar una excepción pero salta otra excepción distinta:
     try:
         d = {"a": 1, "b": 2}
         L = [1, 2, 3, 4]
-        print(d["a"]) 
+        print(d["a"])
         print(L[10])  # IndexError
     except KeyError as e:
         print(e.__class__.__name__, e)
+
 
 def test3():
     # Capturamos a nivel de exception:
     try:
         d = {"a": 1, "b": 2}
         L = [1, 2, 3, 4]
-        print(d["a"]) 
+        print(d["a"])
         print(L[10])  # IndexError
     except Exception as e:
         print(e.__class__.__name__, e)
 
+
+def test4():
+    # Capturamos una exception y luego capturamos posibles excepciones
+    # de otro tipo
+    try:
+        d = {"a": 1, "b": 2}
+        L = [1, 2, 3, 4]
+        print(d["a"])
+        print(L[10])  # IndexError
+
+    except KeyError as e:
+        print("error en el dict: ", e)
+
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+
 if __name__ == "__main__":
     # test1()
     # test2()
-    test3()
+    # test3()
+    test4()
