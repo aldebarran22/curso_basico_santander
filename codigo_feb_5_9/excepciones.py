52d601b5d3c36df21756ff78d259d695a6b47788
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 14:14:53 +0100

    14.14

--- codigo_feb_5_9/excepciones.py
+++ codigo_feb_5_9/excepciones.py
@@ -5,7 +5,14 @@ try, except, finally, raise
 
 
 def test1():
-    pass
+    # Capturar una excepción concreta:
+    try:
+        d = {"a": 1, "b": 2}
+        L = [1, 2, 3, 4]
+        print(d["d"])
+        print(L[10])
+    except KeyError as e:
+        print(e.__class__.__name__, e)
 
 
 if __name__ == "__main__":
