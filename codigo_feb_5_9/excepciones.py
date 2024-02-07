520690bba56886bb63833dbc045540b93df8f2c0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 14:17:29 +0100

    14.17

--- codigo_feb_5_9/excepciones.py
+++ codigo_feb_5_9/excepciones.py
@@ -9,11 +9,22 @@ def test1():
     try:
         d = {"a": 1, "b": 2}
         L = [1, 2, 3, 4]
-        print(d["d"])
-        print(L[10])
+        print(d["d"]) # lanza keyError
+        print(L[10])  # IndexError
+    except KeyError as e:
+        print(e.__class__.__name__, e)
+
+def test2():
+    # Capturar una excepción pero salta otra excepción distinta:
+    try:
+        d = {"a": 1, "b": 2}
+        L = [1, 2, 3, 4]
+        print(d["a"]) 
+        print(L[10])  # IndexError
     except KeyError as e:
         print(e.__class__.__name__, e)
 
 
 if __name__ == "__main__":
-    test1()
+    # test1()
+    test2()
