0eeab8605dc2536bf8675e9d87532410ac188b49
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 13:12:36 +0100

    13.12

--- codigo_feb_5_9/fecha_hora.py
+++ codigo_feb_5_9/fecha_hora.py
@@ -1,36 +1,38 @@
-
-
 class Time:
 
-    def __init__(self,hh=0,mm=0,ss=0):
+    def __init__(self, hh=0, mm=0, ss=0):
         self.hh = hh
         self.mm = mm
         self.ss = ss
 
-   
-   
-    def __str__(self):        
-        return '%02d:%02d:%02d' % (self.hh,self.mm,self.ss)
+    def __str__(self):
+        return "%02d:%02d:%02d" % (self.hh, self.mm, self.ss)
 
-
-   
 
 class Date:
 
-    def __init__(self, dd,mm,yy):
+    def __init__(self, dd, mm, yy):
         self.dd = dd
         self.mm = mm
         self.yy = yy
 
     def __str__(self):
-        return "%02d/%02d/%04d" % (self.dd,self.mm,self.yy)
+        return "%02d/%02d/%04d" % (self.dd, self.mm, self.yy)
 
     def esBisiesto(self):
-        anyo = self.yy    
-        if  (anyo % 4 == 0 and anyo % 100 != 0) or (anyo%100 == 0 and anyo % 400 == 0):
-            return True    
+        anyo = self.yy
+        if (anyo % 4 == 0 and anyo % 100 != 0) or (anyo % 100 == 0 and anyo % 400 == 0):
+            return True
         else:
             return False
 
- 
 
+class DateTime(Date, Time):
+
+    def __init__(self, d=1, m=1, y=1970, hh=0, mm=0, ss=0):
+        pass
+
+
+if __name__ == "__main__":
+    dt = DateTime(7, 2, 2024, 13, 5, 34)  # 07/02/2024 13:05:34
+    dt2 = DateTime()
