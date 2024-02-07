bdf0f4cfea94305d482ca03891ffae7e1839c864
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-07 12:01:21 +0100

    12.01

--- codigo_feb_5_9/objetos.py
+++ codigo_feb_5_9/objetos.py
@@ -2,6 +2,7 @@
 POO en Python
 """
 
+
 class Persona:
     """
     Implementar la clase Persona
@@ -14,11 +15,24 @@ class Persona:
         self.altura = altura
 
     def __str__(self):
-        return self.nombre+" "+str(self.edad)+" "+str(self.altura)
+        return self.nombre + " " + str(self.edad) + " " + str(self.altura)
+    
+    def __repr__(self):
+        return str(self)
+
+    def __del__(self):
+        # print('se borra a ', self.nombre)
+        pass
 
 
 if __name__ == "__main__":
-    p1 = Persona('Pedro',45, 1.9)
-    print(p1)
-    print(str(p1))
-    print(p1.__str__())
+    p1 = Persona("Pedro", 45, 1.9)
+    p2 = Persona("Sara", 40, 1.88)
+    # del(p1) Se fuerza la llamada al destructor de la clase: __del__
+
+    L = [p1, p2, Persona('Ana',45,1.85)]
+    print(L)
+    for i in L:
+        print(i)
+    #print(str(p1))
+    #print(p1.__str__())
