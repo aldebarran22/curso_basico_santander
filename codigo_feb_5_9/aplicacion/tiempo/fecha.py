6e9fce7ca38bca79929171aaed5b7fe1f22fbfeb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-08 11:46:07 +0100

    11.46

---
+++ codigo_feb_5_9/aplicacion/tiempo/fecha.py
@@ -0,0 +1,17 @@
+class Date:
+
+    def __init__(self, d, m, y):
+        self.d = d
+        self.m = m
+        self.y = y
+
+    def __str__(self):
+        return "%02d/%02d/%04d" % (self.d, self.m, self.y)
+
+    def esBisiesto(self):
+        anyo = self.y
+        if (anyo % 4 == 0 and anyo % 100 != 0) or (anyo % 100 == 0 and anyo % 400 == 0):
+            return True
+        else:
+            return False
+
---
+++ codigo_feb_5_9/aplicacion/tiempo/fechahora.py
@@ -0,0 +1,9 @@
+class DateTime(Time, Date):
+
+    def __init__(self, d=1, m=1, y=1970, hh=0, mm=0, ss=0):
+        Date.__init__(self, d, m, y)
+        Time.__init__(self, hh, mm, ss)
+
+    def __str__(self):
+        return Date.__str__(self) + " " + Time.__str__(self)
+
