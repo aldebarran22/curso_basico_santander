24466ea00055df14e8e6a45a4b3397f654f0f142
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-08 09:30:57 +0100

    9.30

--- codigo_feb_5_9/ficheros.py
+++ codigo_feb_5_9/ficheros.py
@@ -3,11 +3,25 @@ Tratamiento de ficheros en Python
 Leer / escribir en fichero y redirigir la salida
 estandar a un fichero
 """
+
+def leerFichero(path):
+    fich = None
+    try:
+        fich = open(path, 'r')
+        
+    except Exception as e:
+        print(e)
+    finally:
+        if fich: fich.close()
+
+
 def printFile():
-    f = open("exportaciones/prueba.txt","w")
-    print("mensaje de prueba",file=f)
+    f = open("exportaciones/prueba.txt", "w")
+    for i in range(5):
+        print("mensaje de prueba", file=f)
     f.close()
 
 
-if __name__ == '__main__':
-    printFile()
+if __name__ == "__main__":
+    #printFile()
+    leerFichero("../ficheros_curso/Pedidos.txt")
