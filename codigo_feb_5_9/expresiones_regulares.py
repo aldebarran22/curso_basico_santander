5c0d2cd4c59229c32d5b6322a06b41be7175e389
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-08 12:39:59 +0100

    12.39

--- codigo_feb_5_9/expresiones_regulares.py
+++ codigo_feb_5_9/expresiones_regulares.py
@@ -1,15 +1,28 @@
 """
 Expresiones regulares en Python
 """
+
 import re
 
+
 def validar(patron, L):
-    print('Patrón: ', patron)
+    print("Patrón: ", patron)
     for i in L:
-        print(i, re.match(patron, i)!=None)
+        print(i, re.match(patron, i) != None)
     print()
 
+
 # Palabras de 3 letras que terminan en .
-patron = "...\.$"
-L = ["123","abc","ABC.","....","ab.","abc.qqqq"]
+patron = r"...\.$"
+L = ["123", "abc", "ABC.", "....", "ab.", "abc.qqqq"]
+validar(patron, L)
+
+# Palabras de 3 letras minusculas que terminan en _
+patron = r"[a-z][a-z][a-z]_$"
+L = ["abc-", "abc_", "ABC_", "....", "ab_", "abc_qqqq"]
+validar(patron, L)
+
+# Validar DNIs
+patron = r""
+L = ["123", "12345678", "12345678AB", "12345678a", "1A"]
 validar(patron, L)
