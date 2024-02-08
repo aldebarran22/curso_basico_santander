8e066cd481708ea67565773de0b5843377f90ed0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-08 09:43:46 +0100

    9.43

--- codigo_feb_5_9/ficheros.py
+++ codigo_feb_5_9/ficheros.py
@@ -20,6 +20,23 @@ def leerFichero(path):
             fich.close()
 
 
+def filtroPais(path, *paises):
+    fich = None
+    try:
+        fich = open(path, "r")
+        for linea in fich:
+            linea = linea.rstrip()  # machacar la variable: OJO es INMUTABLE!!!
+            L = linea.split(";")
+            if L[-1] in paises:
+                print(linea)
+
+    except Exception as e:
+        print(e)
+    finally:
+        if fich:
+            fich.close()
+
+
 def printFile():
     f = open("exportaciones/prueba.txt", "w")
     for i in range(5):
@@ -29,4 +46,6 @@ def printFile():
 
 if __name__ == "__main__":
     # printFile()
-    leerFichero("../ficheros_curso/Pedidos.txt")
+    # leerFichero("../ficheros_curso/Pedidos.txt")
+    filtroPais("../ficheros_curso/Pedidos.txt", "Alemania")
+    filtroPais("../ficheros_curso/Pedidos.txt", "Alemania", "Suiza", "Italia")
