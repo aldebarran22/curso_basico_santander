50a7cee21580d84e0304f84b1dd4ce3c62a17e0e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-08 12:29:00 +0100

    12.28

---
+++ codigo_feb_5_9/expresiones_regulares.py
@@ -0,0 +1,15 @@
+"""
+Expresiones regulares en Python
+"""
+import re
+
+def validar(patron, L):
+    print('Patrón: ', patron)
+    for i in L:
+        print(i, re.match(patron, i)!=None)
+    print()
+
+# Palabras de 3 letras que terminan en .
+patron = ""
+L = ["123","abc","ABC.","...."]
+validar(patron, L)
--- codigo_feb_5_9/importaciones.py
+++ codigo_feb_5_9/importaciones.py
@@ -2,11 +2,10 @@
 Importar un módulo de Python que no está en la 
 carpeta actual y tampoco en las lib de python
 """
+
 import sys
 
-sys.path.append('D:/temp')
+sys.path.append("D:/temp")
 import funcion_abc
-print(funcion_abc.operar(12,34))
 
-
-
+print(funcion_abc.operar(12, 34))
