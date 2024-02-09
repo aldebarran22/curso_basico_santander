20ab3cff5d220abcc4524782eae7bece79d6a8e1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 09:26:04 +0100

    9.25

--- codigo_feb_5_9/base_datos.py
+++ codigo_feb_5_9/base_datos.py
@@ -2,11 +2,19 @@ import sqlite3 as db
 from os.path import isfile
 
 def conexion(path):
-    if not isfile(path):
-        raise FileNotFoundError(f"El fichero {path} no existe")
-    else:
-        pass
+    con = None
+    try:
+        if not isfile(path):
+            raise FileNotFoundError(f"El fichero {path} no existe")
+        else:
+            con = db.connect(path)
+            print('conexión ok')
 
+    except Exception as e:
+        #raise e
+        print(e)
+    finally:
+        if con: con.close()
 
 if __name__ == '__main__':
     conexion('../bd/empresa3.db')
