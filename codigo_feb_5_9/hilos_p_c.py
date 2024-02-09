7b948fb18f46ffeb3e55b43104086a325a67a04c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 12:48:02 +0100

    12.47

---
+++ codigo_feb_5_9/hilos_p_c.py
@@ -0,0 +1,78 @@
+"""
+Implementación del productor-consumidor en Python
+Solución M productores-N consumidores
+"""
+
+from threading import Thread, Lock, Semaphore
+from random import randint
+from time import sleep
+
+num_muestras = 10
+tam_buffer = 5
+
+num_productores = 2
+num_consumidores = 1
+
+
+class Productor(Thread):
+    def __init__(self, buffer, num_muestras, nombre):
+        Thread.__init__(self, name=nombre)
+        self.buffer = buffer
+        self.num_muestras = num_muestras
+
+    def run(self):
+        pass
+
+
+class Consumidor(Thread):
+    def __init__(self, buffer, num_muestras, nombre):
+        Thread.__init__(self, name=nombre)
+        self.buffer = buffer
+        self.num_muestras = num_muestras
+
+    def run(self):
+        pass
+
+
+class TBuffer:
+    def __init__(self):
+        self.ind_c = 0
+        self.ind_p = 0
+        self.buffer = [-1] * tam_buffer
+        self.mutex = Lock()
+        self.sem_huecos = Semaphore(tam_buffer)
+        self.sem_items = Semaphore(0)
+
+
+if __name__ == "__main__":
+    if num_muestras % num_productores != 0 or num_muestras % num_consumidores != 0:
+        print("El número tiene que estar equilibrado entre el n. de prod/con")
+        exit()
+
+    # Calcular el numero de muestra por productor y consumidor:
+    num_muestras_prod = num_muestras // num_productores
+    num_muestras_con = num_muestras // num_consumidores
+
+    buf = TBuffer()
+
+    # Crear los hilos:
+    LProd = []
+    LCon = []
+
+    for i in range(num_productores):
+        nombre = f"P-{i+1}"
+        p = Productor(buf, num_muestras_prod, nombre)
+        p.start()
+        LProd.append(p)
+
+    for i in range(num_consumidores):
+        nombre = f"C-{i+1}"
+        c = Consumidor(buf, num_muestras_con, nombre)
+        c.start()
+        LCon.append(c)
+
+    for i in LProd:
+        i.join()
+
+    for i in LCon:
+        i.join()
