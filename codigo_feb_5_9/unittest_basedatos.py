7a0ba6d7207f54720ae25b61b128445e3097eb51
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 14:13:08 +0100

    14.13

---
+++ codigo_feb_5_9/unittest_basedatos.py
@@ -0,0 +1,19 @@
+"""
+Pruebas sobre el módulo: basedatosCRUD.
+"""
+
+import unittest
+from basedatosCRUD import BaseDatos
+
+
+class PruebaBD(unittest.TestCase):
+
+    def test_exceptions(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
