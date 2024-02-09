7b7eb4e781825f238906660f11c17d0566c7a17a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 13:52:51 +0100

    13.52

--- codigo_feb_5_9/unittest_ecuacion.py
+++ codigo_feb_5_9/unittest_ecuacion.py
@@ -11,10 +11,11 @@ class Pruebas1(unittest.TestCase):
 
     def testEcuacionOK(self):
         resultado = ecuGrado2(1, 5, 4)
-        t = (-1, -4)
+        t = (-1, 4)
         self.assertEqual(
             resultado, t, msg="No coincide el resultado para a=1, b=5, c=4"
         )
 
-    if __name__ == "__main__":
-        unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
