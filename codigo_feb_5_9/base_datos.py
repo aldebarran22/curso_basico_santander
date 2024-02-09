f16a7c6e4cdc0e6bdc3cc28ce04b7ecf265107d5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-09 09:31:12 +0100

    9.31

--- codigo_feb_5_9/base_datos.py
+++ codigo_feb_5_9/base_datos.py
@@ -1,20 +1,30 @@
 import sqlite3 as db
 from os.path import isfile
 
-def conexion(path):
+
+def conexion(path, tabla):
     con = None
+    cur = None
     try:
         if not isfile(path):
             raise FileNotFoundError(f"El fichero {path} no existe")
         else:
             con = db.connect(path)
-            print('conexión ok')
+            cur = con.cursor()
+            sql = f"select * from {tabla}"
+            cur.execute(sql)
+            for t in cur.fetchall():
+                print(t)
 
     except Exception as e:
-        #raise e
+        # raise e
         print(e)
     finally:
-        if con: con.close()
+        if cur:
+            cur.close()
+        if con:
+            con.close()
 
-if __name__ == '__main__':
-    conexion('../bd/empresa3.db')
+
+if __name__ == "__main__":
+    conexion("../bd/empresa3.db","pedidos")
