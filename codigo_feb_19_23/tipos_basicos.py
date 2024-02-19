d49da97ff8c94e2c0ec47ebfcc1894ddf6c2df52
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 10:01:52 +0100

    10.01

---
+++ codigo_feb_19_23/tipos_basicos.py
@@ -0,0 +1,15 @@
+"""
+Pruebas con los tipos básicos de Python
+"""
+
+numero = 100
+print(numero, type(numero))
+
+numero2 = 23.643
+print(numero2, type(numero2))
+
+nombre = "Ana María"
+print(nombre, type(nombre))
+
+print(numero + numero2)
+#print(numero + nombre)
