35ef448cb782c3924de6aaa65b8ab40d65a36547
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 13:41:35 +0100

    13.41

---
+++ codigo_feb_19_23/diccionario.py
@@ -0,0 +1,19 @@
+"""
+Diccionarios en Python
+"""
+
+d = {'a':1, 'b':2, 'c':3, 'd':4}
+
+# Acceso a los elementos:
+print(d['a'])
+# print(d['x']) Error si la clave no existe
+
+d['e'] = 5
+d['d'] = 400
+print(d, type(d))
+
+# in
+if 'e' in d:
+    print('la clave e existe')
+if 400 in d.values():
+    print('400 existe')
