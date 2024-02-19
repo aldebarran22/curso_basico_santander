ea663a8220ac40d214a6f7b7e90117da0dfe32c6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 10:59:18 +0100

    11.00

---
+++ codigo_feb_19_23/cadenas.py
@@ -0,0 +1,15 @@
+"""
+Cadenas de texto en Python
+Operadores: + *
+"""
+cad1 = "12"
+cad2 = "34"
+print(cad1 + cad2)
+
+print(cad1 * 10)
+print('-' * 10)
+
+n1 = int(cad1)
+n2 = int(cad2)
+print(n1 + n2)
+
