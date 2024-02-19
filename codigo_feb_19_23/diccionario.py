ed5b46c329ebcc9d837699142dc52709656755d8
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 14:17:31 +0100

    14.17

--- codigo_feb_19_23/diccionario.py
+++ codigo_feb_19_23/diccionario.py
@@ -2,33 +2,33 @@
 Diccionarios en Python
 """
 
-d = {'a':1, 'b':2, 'c':3, 'd':4}
+d = {"a": 1, "b": 2, "c": 3, "d": 4}
 
 # Acceso a los elementos:
-print(d['a'])
+print(d["a"])
 # print(d['x']) Error si la clave no existe
 
-d['e'] = 5
-d['d'] = 400
+d["e"] = 5
+d["d"] = 400
 print(d, type(d))
 
 # in
-if 'e' in d:
-    print('la clave e existe')
+if "e" in d:
+    print("la clave e existe")
 if 400 in d.values():
-    print('400 existe')
+    print("400 existe")
 
-print('claves: ', list(d.keys()))
-print('valores: ', list(d.values()))
-print('items: ', list(d.items()))
-for k,v in d.items():
-    print(k,v)
+print("claves: ", list(d.keys()))
+print("valores: ", list(d.values()))
+print("items: ", list(d.items()))
+for k, v in d.items():
+    print(k, v)
 
-L = [1,2,3,1,1,2,3,4,5,6,3,2,1,2]
+L = [1, 2, 3, 1, 1, 2, 3, 4, 5, 6, 3, 2, 1, 2]
 d = dict()
 for i in L:
     if i in d:
-        d[i]+=1
+        d[i] += 1
     else:
         d[i] = 1
 print(d)
@@ -40,7 +40,7 @@ print(d)
 
 # Crear un diccionario  a partir de dos colecciones:
 cad = "adios"
-L = [1,2,3,4,5]
+L = [1, 2, 3, 4, 5]
 
 d1 = dict(zip(cad, L))
 print(d1)
@@ -49,7 +49,20 @@ d2 = dict(zip(L, cad))
 print(d2)
 
 cad = "ana"
-L = [1,2,3]
+L = [1, 2, 3]
 d3 = dict(zip(cad, L))
 print(d3)
 
+
+cad = "acsbdedzxq"
+L = [1, 2, 3]
+d3 = dict(zip(cad, L))
+print(d3)
+
+# Generar un diccionario a partir de dos cadenas:
+cabs = "id;nombre;cargo"
+datos = "1;Davolio;Representante de ventas"
+Lcabs = cabs.split(";")
+Ldatos = datos.split(";")
+emp = dict(zip(Lcabs, Ldatos))
+print(emp)
