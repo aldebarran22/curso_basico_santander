a1afa6acd3f538b59849b88c6168fb9c829725f1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 13:19:01 +0100

    13.18

--- codigo_feb_19_23/listas.py
+++ codigo_feb_19_23/listas.py
@@ -50,3 +50,25 @@ for i in L:
 print() # salto de linea
 
 # slicing, índices negativos
+L = [1,2,3,5,-1,12]
+print('El primero:',L[0], L[-6])
+print('El último: ', L[-1], L[5])
+# slicing: L[ini:fin-1:salto=1]
+print('los 3 primeros:', L[0:3])
+print('los 3 primeros:', L[:3])
+print('quitar el último:', L[:-1])
+print('los 3 últimos:',L[-3:])
+print('quitar primero y último:',L[1:-1])
+print('toda la lista: ', L[:])
+print('toda la lista de dos en dos: ', L[::2])
+print('invertida: ', L[::-1])
+
+path = "C:/mis documentos/excel/libro1.xlsx"
+L = path.split("/")
+print(L[-1])
+
+cad = "ana"
+if cad == cad[::-1]:
+    print('es un palindromo')
+else:
+    print('no lo es')
