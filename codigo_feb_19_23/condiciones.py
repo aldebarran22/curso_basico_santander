3ce47875011f94ce20334ed2fe4f0332cd4a5cb9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 11:45:03 +0100

    11.44

--- codigo_feb_19_23/condiciones.py
+++ codigo_feb_19_23/condiciones.py
@@ -2,7 +2,7 @@
 A partir de dos números calcular el menor
 """
 
-n1 = 23
+n1 = 46
 n2 = 23
 
 if n1 < n2:
@@ -13,3 +13,18 @@ elif n1 == n2:
 
 else:
     print(f"El menor es {n2}")
+
+# Comprobar si una variable se encuentra dentro
+# un intervalo:
+ini = 10
+fin = 40
+num = 41
+if num >= ini and num <= fin:
+    print("dentro del intervalo")
+else:
+    print("fuera del intervalo")
+
+if ini <= num <= fin:
+    print("dentro del intervalo")
+else:
+    print("fuera del intervalo")
