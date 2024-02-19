5225d9ba1bf033abf12f16cc044890336d784821
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 10:40:15 +0100

    10.40

--- codigo_feb_19_23/ecuacion.py
+++ codigo_feb_19_23/ecuacion.py
@@ -2,8 +2,10 @@
 Resolver ecuaciones de segundo grado.
 """
 import math
+# from math import sqrt
 
 a, b, c = 1,5,4 # a = 1, b = 5, c = 4
 
-x1 = -b +
-x2 = -b -
+x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+print('x1:', x1, 'x2:', x2)
