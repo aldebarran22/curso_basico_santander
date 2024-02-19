0978783bcca515fe97a5996586821637f2a14285
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 11:39:51 +0100

    11.39

---
+++ codigo_feb_19_23/condiciones.py
@@ -0,0 +1,15 @@
+"""
+A partir de dos números calcular el menor
+"""
+
+n1 = 23
+n2 = 23
+
+if n1 < n2:
+    print(f"El menor es {n1}")
+
+elif n1 == n2:
+    print("Los valores son iguales")
+
+else:
+    print(f"El menor es {n2}")
