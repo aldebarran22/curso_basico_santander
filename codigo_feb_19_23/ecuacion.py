7cd17d8ccd720f3a499a7882b0f1589dfc0162ba
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 10:46:08 +0100

    10.46

--- codigo_feb_19_23/ecuacion.py
+++ codigo_feb_19_23/ecuacion.py
@@ -1,11 +1,18 @@
 """
 Resolver ecuaciones de segundo grado.
 """
+
 import math
+
 # from math import sqrt
 
-a, b, c = 1,5,4 # a = 1, b = 5, c = 4
+a, b, c = 1, 2, 3  # a = 1, b = 5, c = 4
 
-x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
-print('x1:', x1, 'x2:', x2)
+raiz = b**2 - 4 * a * c
+
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2 * a)
+    x2 = (-b - math.sqrt(raiz)) / (2 * a)
+    print("x1:", x1, "x2:", x2)
+else:
+    print("No hay solución")
