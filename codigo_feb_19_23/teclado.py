96d44f3dbe64a9e18afd6d7d7f64f4d05ce71d38
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-19 11:34:00 +0100

    11.33

--- codigo_feb_19_23/teclado.py
+++ codigo_feb_19_23/teclado.py
@@ -1,6 +1,9 @@
 """
 Leer de teclado en Python
+float(): de str a float
+int(): de str a int
 """
-n1 = int(input("Teclear número 1:"))
-n2 = int(input("Teclear número 2:"))
+
+n1 = float(input("Teclear número 1:"))
+n2 = float(input("Teclear número 2:"))
 print(n1 + n2)
