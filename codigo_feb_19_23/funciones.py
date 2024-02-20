1808d42db65bbfa9ab746e7d695d052b026fe7cb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-20 11:33:08 +0100

    11.33

---
+++ codigo_feb_19_23/funciones.py
@@ -0,0 +1,12 @@
+"""
+Implementación de funciones en Python
+Llamadas a las funciones:
+Paso de parámetros
+Tipos anotados
+"""
+
+def sumar(a,b):
+    """
+    Sumar los dos parámetros
+    """
+    return a+b
