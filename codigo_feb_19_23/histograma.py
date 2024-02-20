f802ec68de607e3c1b9ecc0dcfe581b2263aecb7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-20 10:24:19 +0100

    10.24

--- codigo_feb_19_23/histograma.py
+++ codigo_feb_19_23/histograma.py
@@ -4,9 +4,7 @@ creando un histograma: list, dict, set
 """
 L = [1, 2, 3, 1, 1, 2, 3, 4, 5, 6, 3, 2, 1, 2]
 d = dict()
-for i in L:
-    if i in d:
-        d[i] += 1
-    else:
-        d[i] = 1
+for i in set(L):
+    print(i)
+    
 print(d)
