2625bc466cdcd5a7bcc09b145331ed832d092a36
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-20 12:29:37 +0100

    12.29

--- codigo_feb_19_23/funciones_curso.py
+++ codigo_feb_19_23/funciones_curso.py
@@ -2,7 +2,19 @@
 Funciones del curso de  Python
 """
 
-L = [1, 2, 3, 1, 1, 2, 3, 4, 5, 6, 3, 2, 1, 2]
-d = dict()
-for i in set(L):
-    d[i] = L.count(i)
+
+def histograma(L):
+    d = dict()
+    for i in set(L):
+        d[i] = L.count(i)
+    return d
+
+
+if __name__ == "__main__":
+    L = [1, 2, 3, 1, 1, 2, 3, 4, 5, 6, 3, 2, 1, 2]
+    d = histograma(L)
+    print(d)
+
+    cad = "hola que tal"
+    d2 = histograma(cad)
+    print(d2)
