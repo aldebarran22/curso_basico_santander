19611f3f07eb5d15069f94b1a0ddb3c1b56efb25
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-20 13:58:49 +0100

    13.58

--- codigo_feb_19_23/param_ref_copia.py
+++ codigo_feb_19_23/param_ref_copia.py
@@ -3,13 +3,19 @@ Parámetros por referencia (los mutables)
 P. por copia (los inmutables)
 """
 
+
 def funcion(numero, lista):
     numero += 100
     lista.append(100)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     numero = 1
-    L = [1,2,3]
-    print(f'numero: {numero}, lista: {L}')
+    L = [1, 2, 3]
+    print(f"numero: {numero}, lista: {L}")
+    # La lista L pasa por referencia
     funcion(numero, L)
-    print(f'numero: {numero}, lista: {L}')
+    # La lista L pasa por copia
+    # funcion(numero, L.copy())
+
+    print(f"numero: {numero}, lista: {L}")
