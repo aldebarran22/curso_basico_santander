768e48698dfaa9425564ef4a239a8b7d522e21f3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-20 11:56:45 +0100

    11.56

--- codigo_feb_19_23/funciones.py
+++ codigo_feb_19_23/funciones.py
@@ -21,7 +21,16 @@ def sumar3(a, b, c):
 
 
 if __name__ == "__main__":
-    print(sumar(1, 2))
+    print(sumar(1, 2))  # Forma posicional
+    print(sumar(b=2, a=1))  # Forma nominal
+    t = (1, 2)
+    print(sumar(*t))  # Con una tupla
+    L = [1, 2]
+    print(sumar(*L))  # Con una lista
+    d = {"a": 1, "b": 2}
+    print(sumar(**d))  # Con un diccionario
+    """
     print(sumar("1", "2"))
     print(sumar([1], [2]))
     print(__name__)
+    """
