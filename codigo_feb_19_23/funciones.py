f95b441059288dc26969e203bf6c6c84604d2168
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-20 11:36:11 +0100

    11.36

--- codigo_feb_19_23/funciones.py
+++ codigo_feb_19_23/funciones.py
@@ -5,8 +5,19 @@ Paso de parámetros
 Tipos anotados
 """
 
+
 def sumar(a,b):
     """
     Sumar los dos parámetros
     """
     return a+b
+
+def sumar(a,b,c):
+    """
+    Sumar los tres parámetros
+    """
+    return a+b+c
+
+print(sumar(1,2))
+print(sumar("1","2"))
+print(sumar([1],[2]))
