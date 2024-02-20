c1fd89de2bea2d2a4bce481918926a1616e6e2be
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-20 13:40:00 +0100

    13.39

--- codigo_feb_19_23/funciones_parametros.py
+++ codigo_feb_19_23/funciones_parametros.py
@@ -13,3 +13,6 @@ def funcion(ob1, ob2, op1=1, op2=2, *args, **kwargs):
 
 if __name__ == "__main__":
     funcion(10, 20)
+    funcion(10,20,op2=200)
+    funcion(1,2,3,4,5,6,7,8)
+    funcion(1,2,3,4,5,6,7,8,44,x=10,y=20)
