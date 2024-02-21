88989d7b65fc4c5bbb438775a3d18bbae4b33ad7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-21 11:59:11 +0100

    11.59

--- codigo_feb_19_23/objetos.py
+++ codigo_feb_19_23/objetos.py
@@ -20,11 +20,17 @@ class Persona:
     def __repr__(self):
         return str(self)  # Red. a __str__
 
+    def __lt__(self, other):
+        return self.edad < other.edad
+
     def __del__(self):
-        print("se borra a: ", self.nombre)
+        # print("se borra a: ", self.nombre)
+        pass
 
 
 if __name__ == "__main__":
+    p2 = Persona("Juan")
+    del p2  # Fuerza la llamada al destructor --> __del__()
     p1 = Persona("Andrés", 34, 1.8)
     print(p1)  # str(p1) --> p1.__str__()
     # print(str(p1))
@@ -32,4 +38,6 @@ if __name__ == "__main__":
     L = [p1, Persona("Ana", 45, 1.77), Persona("Jose", 66, 1.9)]
     print(L)
     L.sort(key=lambda obj: obj.altura)
+    print(L)
+    L.sort()  # Utiliza __lt__()
     print(L)
