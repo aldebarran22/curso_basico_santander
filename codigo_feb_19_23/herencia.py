c1d5fa0bb763d723ad48cf2fdcd9813dd0575e1f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-21 13:10:47 +0100

    13.10

--- codigo_feb_19_23/herencia.py
+++ codigo_feb_19_23/herencia.py
@@ -13,16 +13,28 @@ class Guia(Persona):
         self.ambito = ambito
         self.idiomas = idiomas
 
+    def hablarCon(self, other=None):
+        if other is None:
+            Persona.hablarCon(self)
+        else:
+            c1 = set(self.idiomas)
+            c2 = set(other.idiomas)
+            comun = c1 & c2
+            if len(comun)==0:
+                raise ValueError("No pueden hablar")
+            else:
+                print('Puede hablar en: ', " o ".join(comun))
+
     def __str__(self):
         return Persona.__str__(self) + " " + self.ambito + " " + ",".join(self.idiomas)
         # return super().__str__() + " " + self.ambito + " " + ",".join(self.idiomas)
 
 
 def testGuia():
-    g1 = Guia("Andrés", 34, 1.8, "I", ["Inglés", "Francés"])
+    g1 = Guia("Andrés", 34, 1.8, "I", ["Inglés", "Francés","Alemán"])
     g1.cumple()
     print(g1)
-    g2 = Guia("Eva", 34, 1.8, "I", ["Alemán", "Chino"])
+    g2 = Guia("Eva", 34, 1.8, "I", ["Alemán", "Chino","Inglés"])
     g1.hablarCon(g2)
 
 if __name__ == "__main__":
