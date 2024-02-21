9d3dba2a19b0f98721105e759c5302e49dad206d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-21 13:39:32 +0100

    13.39

--- codigo_feb_19_23/fecha_hora.py
+++ codigo_feb_19_23/fecha_hora.py
@@ -1,36 +1,40 @@
-
-
 class Time:
 
-    def __init__(self,hh=0,mm=0,ss=0):
-        self.hh = hh
-        self.mm = mm
-        self.ss = ss
+    def __init__(self, hh=0, mm=0, ss=0):
+        self.h = hh
+        self.m = mm
+        self.s = ss
 
-   
-   
-    def __str__(self):        
-        return '%02d:%02d:%02d' % (self.hh,self.mm,self.ss)
+    def __str__(self):
+        return "%02d:%02d:%02d" % (self.h, self.m, self.s)
 
-
-   
 
 class Date:
 
-    def __init__(self, dd,mm,yy):
+    def __init__(self, dd, mm, yy):
         self.dd = dd
         self.mm = mm
         self.yy = yy
 
     def __str__(self):
-        return "%02d/%02d/%04d" % (self.dd,self.mm,self.yy)
+        return "%02d/%02d/%04d" % (self.dd, self.mm, self.yy)
 
     def esBisiesto(self):
-        anyo = self.yy    
-        if  (anyo % 4 == 0 and anyo % 100 != 0) or (anyo%100 == 0 and anyo % 400 == 0):
-            return True    
+        anyo = self.yy
+        if (anyo % 4 == 0 and anyo % 100 != 0) or (anyo % 100 == 0 and anyo % 400 == 0):
+            return True
         else:
             return False
 
- 
 
+class DateTime(Date, Time):
+    """01/01/1970 00:00:00"""
+    pass
+
+
+if __name__ == "__main__":
+    t = Time(1, 5, 7)
+    print(t)
+
+    d = Date(1, 4, 2024)
+    print(d)
