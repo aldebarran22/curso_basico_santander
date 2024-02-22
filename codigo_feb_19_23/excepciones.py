2db2597073b6e6bc281404c0144e2f26052d072f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-22 09:16:19 +0100

    9.16

--- codigo_feb_19_23/excepciones.py
+++ codigo_feb_19_23/excepciones.py
@@ -3,33 +3,41 @@ Captura de excepciones en Python
 try except finally
 raise
 """
+
 from os import listdir
+
 
 def prueba1():
     try:
         b = 10
-        L = [1,2,3,4]
+        L = [1, 2, 3, 4]
         print(L[1])
         print(L[0] / b)
     except Exception as e:
         print(e.__class__.__name__, e)
 
+
 def procesarFich(path):
-    ext = path.partition('.')[2]
-    if ext == 'ipynb':
-        raise ValueError(f'Error al procesar el fichero: {path}')
+    ext = path.partition(".")[2]
+    if ext == "ipynb":
+        raise ValueError(f"Error al procesar el fichero: {path}")
     else:
-        print(f'Se ha procesado el fichero: {path}')
+        print(f"Se ha procesado el fichero: {path}")
+
 
 def prueba2():
-    try:
-        for f in listdir():
+    fallos = []
+    for f in listdir():
+        try:
             procesarFich(f)
-    except Exception as e:
-        print(e.__class__.__name__, e)
-
+        except Exception as e:
+            fallos.append(f)
+            print(e.__class__.__name__, e)
+    print(f"Han fallado {len(fallos)} ficheros")
+    if len(fallos) > 0:
+        print(fallos)
 
 
 if __name__ == "__main__":
-    #prueba1()
+    # prueba1()
     prueba2()
