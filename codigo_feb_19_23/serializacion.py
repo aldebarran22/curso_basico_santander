e885fd1bef2bedada998099a2de3ffe24f04b312
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-22 14:04:57 +0100

    14.04

--- codigo_feb_19_23/serializacion.py
+++ codigo_feb_19_23/serializacion.py
@@ -7,17 +7,31 @@ import pickle as p
 import shelve as s
 from fecha_hora import Date, Time, DateTime
 
+
 def serializarPickle(path, obj):
     f = None
     try:
-        f = open(path, 'wb')
+        f = open(path, "wb")
         p.dump(obj, f, protocol=2)
     except Exception as e:
         print(e)
     finally:
-        if f: f.close()
+        if f:
+            f.close()
 
-if __name__ == '__main__':
-    L = [Date(1,5,2045), Time(12), DateTime()]
+
+def imprimir(L):
+    for i in L:
+        print(i, end=" ")
+    print()
+
+
+def deserializarPickle(path):
+    pass
+
+
+if __name__ == "__main__":
+    L = [Date(1, 5, 2045), Time(12), DateTime()]
+    imprimir(L)
     serializarPickle("../ficheros/ser_pickle.bin", L)
-
+    L2 = deserializarPickle("../ficheros/ser_pickle.bin")
