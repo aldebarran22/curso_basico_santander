676c3f0130aac72583d90b05320fd748cf0bd937
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-22 09:58:48 +0100

    9.58

--- codigo_feb_19_23/ficheros.py
+++ codigo_feb_19_23/ficheros.py
@@ -5,7 +5,9 @@ def leerFichero(path):
     f = None
     try:
         f = open(path, "r")
-        
+        for linea in f:
+            linea = linea.rstrip()
+            print(linea)        
 
     except Exception as e:
         print(e)
@@ -13,3 +15,6 @@ def leerFichero(path):
     finally:
         if f:
             f.close()
+
+if __name__ == "__main__":
+    leerFichero("../ficheros_curso/Pedidos.txt")
