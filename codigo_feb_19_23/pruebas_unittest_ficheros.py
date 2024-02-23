eb917dd75ff7f6ddb361dca5fcd8f5ea8c5ff89b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-23 14:31:17 +0100

    14.31

--- codigo_feb_19_23/pruebas_unittest_ficheros.py
+++ codigo_feb_19_23/pruebas_unittest_ficheros.py
@@ -5,12 +5,17 @@ Pruebas con unittest y el módulo de ficheros
 import unittest
 
 
+
+
+
 class PruebasFicheros(unittest.TestCase):
 
     def setUp(self):
+        self.num = 100
         print('Preparar contexto')
 
     def test(self):
+        print(self.num)
         pass
 
     def tearDown(self):
