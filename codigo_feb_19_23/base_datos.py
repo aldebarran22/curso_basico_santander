48966ca09cb17f97f14d667f8b8b6884b6329dcc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-23 09:28:44 +0100

    9.28

--- codigo_feb_19_23/base_datos.py
+++ codigo_feb_19_23/base_datos.py
@@ -8,14 +8,25 @@ from os.path import isfile
 
 def listar(path, tabla):
     con = None
+    cur = None
     try:
         if not isfile(path):
             raise FileNotFoundError(f"No existe el fichero: {path}")
         else:
             con = db.connect(path)
-            print("conexión Ok!")
+            cur = con.cursor()
+            sql = f"select * from {tabla}"
+            cur.execute(sql)
+            print(cur.description)
     except Exception as e:
         print(e)
     finally:
+        if cur:
+            cur.close()
         if con:
             con.close()
+
+
+if __name__ == "__main__":
+    path = "../bd/empresa3.db"
+    listar(path, "pedidos")
