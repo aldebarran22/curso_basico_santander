a89d568820b846dd6bc0d5cb3dabaf4ec7a689c4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-23 14:40:27 +0100

    14.40

--- codigo_feb_19_23/pruebas_unittest_ficheros.py
+++ codigo_feb_19_23/pruebas_unittest_ficheros.py
@@ -4,24 +4,23 @@ Pruebas con unittest y el módulo de ficheros
 
 import unittest
 
-
-
+from ficheros_paises import leerEscribirFichero
 
 
 class PruebasFicheros(unittest.TestCase):
 
     def setUp(self):
-        self.num = 100
-        print('Preparar contexto')
+        f = open("../ficheros_curso/nombre_paises.txt", "r")
+        txt = f.read()
+        self.paises = txt.split("\n")
+        f.close()
 
     def test(self):
-        print(self.num)
-        pass
+        print(self.paises)
 
     def tearDown(self):
-        print('limpiar contexto')
+        print("limpiar contexto")
 
-    
-   
+
 if __name__ == "__main__":
     unittest.main()
