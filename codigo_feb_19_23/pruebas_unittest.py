565aefca7d2b0c37b4465f4c57e88d06d87be79e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-23 13:46:16 +0100

    13.46

--- codigo_feb_19_23/pruebas_unittest.py
+++ codigo_feb_19_23/pruebas_unittest.py
@@ -10,6 +10,12 @@ class PruebasFicheros(unittest.TestCase):
     def test(self):
         pass
 
+    def test2(self):
+        raise AssertionError("error")
+
+    def test3(self):
+        raise ValueError("error 2")
+
 
 if __name__ == "__main__":
     unittest.main()
