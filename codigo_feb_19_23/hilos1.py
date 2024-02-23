fa127b5941432f889418f36ece0a6b817636faeb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-23 11:49:02 +0100

    11.48

--- codigo_feb_19_23/hilos1.py
+++ codigo_feb_19_23/hilos1.py
@@ -9,8 +9,8 @@ from time import sleep
 
 class HiloRandom(Thread):
 
-    def __init__(self, n=10):
-        Thread.__init__(self, name="HiloRandom")
+    def __init__(self, n=10, name="HiloRandom"):
+        Thread.__init__(self, name=name)
         self.n = n
 
     def run(self):
@@ -22,8 +22,8 @@ class HiloRandom(Thread):
 
 class HiloMensaje(Thread):
 
-    def __init__(self, n=10):
-        Thread.__init__(self, name="HiloMensajes")
+    def __init__(self, n=10, name="HiloMensajes"):
+        Thread.__init__(self, name=name)
         self.n = n
 
     def run(self):
@@ -33,7 +33,7 @@ class HiloMensaje(Thread):
         print("Fin: ", self.getName())
 
 
-if __name__ == "__main__":
+def test1():
     hiloR = HiloRandom(10)
     hiloM = HiloMensaje(8)
 
@@ -44,3 +44,18 @@ if __name__ == "__main__":
     hiloM.join()
 
     print("Termina main")
+
+
+def testLista():
+    L = []
+    for i in range(5):
+        h = HiloRandom(5, f"R-{i}")
+        h.start()
+        L.append(h)
+
+    for h in L:
+        h.join()
+
+
+if __name__ == "__main__":
+    testLista()
