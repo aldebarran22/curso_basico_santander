ad955fe831e0ccb6dcbdffa670ea0170edace21a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-23 13:43:52 +0100

    13.43

---
+++ codigo_feb_19_23/pruebas_unittest.py
@@ -0,0 +1,15 @@
+"""
+Pruebas con unittest
+"""
+
+import unittest
+
+
+class PruebasFicheros(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
