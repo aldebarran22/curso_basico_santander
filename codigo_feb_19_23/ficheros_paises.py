3de57abb547c3aac57bcdc8a5393f608a478084b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-02-23 14:35:42 +0100

    14.35

--- codigo_feb_19_23/ficheros_paises.py
+++ codigo_feb_19_23/ficheros_paises.py
@@ -4,31 +4,7 @@ Ficheros. Entrada/Salida
 import sys
 
 
-def testStdout(fichero=sys.stdout):
-    productos = [
-        {"id": 1, "producto": "portatil", "precio": 1200.567},
-        {"id": 2, "producto": "HP", "precio": 2250.6},
-        {"id": 3, "producto": "Impresora", "precio": 566.0},
-    ]
-    print("%-5s\t%-15s\t%8s" % tuple(productos[0].keys()), file=fichero)
-    for d in productos:
-        # print("%05d\t%-15s\t%8.2f" % (d["id"], d["producto"],  d["precio"]))
-        print("%05d\t%-15s\t%8.2f" % tuple(d.values()), file=fichero)
 
-
-def leerFichero(path):
-    f = None
-    try:
-        f = open(path, "r")
-        for linea in f:
-            # Limpiar el \n de cada linea:
-            linea = linea.rstrip()
-            print(linea)
-    except Exception as e:
-        raise e
-    finally:
-        if f:
-            f.close()
 
 
 def isFloat(numero):
@@ -76,12 +52,3 @@ def leerEscribirFichero(path, pais):
         if fout:
             fout.close()
 
-
-if __name__ == "__main__":
-    leerFichero("../ficheros/Empleados.txt")
-    #leerEscribirFichero("../ficheros/Pedidos.txt", "Suiza")
-    """
-    f = open("productos.txt", "w")
-    testStdout(f)
-    f.close()
-    """
