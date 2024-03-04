9e70dfc42e8d159d748d1a4e770fb866551f50b0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 10:36:57 +0100

    10.36

---
+++ codigo_mar_4_8/operadores.py
@@ -0,0 +1,11 @@
+"""
+Pruebas con operadores aritméticos
+"""
+
+# Calcular la media de dos números:
+n1 = 123
+n2 = 900
+media = (n1 + n2) / 2
+print('media: ', media)
+# f-string
+print(f"La media de {n1} y {n2} es {media}")
