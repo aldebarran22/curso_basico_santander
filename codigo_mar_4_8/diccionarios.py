77256e51f95957f450d0846c7b9efcbfc6cf9e6f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 13:44:05 +0100

    13.44

--- codigo_mar_4_8/diccionarios.py
+++ codigo_mar_4_8/diccionarios.py
@@ -4,12 +4,13 @@ Diccionarios en Python:
 acceso a los elementos
 crear nuevas claves
 """
-d = {'a':1, 'b':2, 'c':3, 'd':4}
-print(d['a'])
-d['e'] = 5
+
+d = {"a": 1, "b": 2, "c": 3, "d": 4}
+print(d["a"])
+d["e"] = 5
 print(d, type(d))
 
-L = [1,2,3,4,5]
+L = [1, 2, 3, 4, 5]
 cad = "adios"
 d2 = dict(zip(L, cad))
 print(d2)
@@ -17,7 +18,10 @@ d3 = dict(zip(cad, L))
 print(d3)
 
 # in, for
-d = {'a':1, 'b':2, 'c':3, 'd':4}
-print('Existe la clave: c: ', 'c' in d)
-print('Existe el valor: 2: ', 2 in d.values())
+d = {"a": 1, "b": 2, "c": 3, "d": 4}
+print("Existe la clave: c: ", "c" in d)
+print("Existe el valor: 2: ", 2 in d.values())
 
+print("keys:", d.keys())
+print("values: ", d.values())
+print("items: ", d.items())
