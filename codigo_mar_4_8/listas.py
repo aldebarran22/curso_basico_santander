b2f9d09e164261d777e38e745a18b7e903d5a662
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 12:24:20 +0100

    12.24

--- codigo_mar_4_8/listas.py
+++ codigo_mar_4_8/listas.py
@@ -10,5 +10,10 @@ cad = "Hola que tal"
 L = list(cad)
 print(L)
 
-L2 = [1,2,3,4,5,4,3,2,1,2,4]
+L2 = [1,2,3,4,5]
 print(L2)
+
+# Las listas se pueden modificar:
+L2[0] = 400
+print(L2)
+print('EL último de la lista:', L2[4], L2[-1])
