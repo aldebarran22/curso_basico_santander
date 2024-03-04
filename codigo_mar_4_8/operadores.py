b5f1031fab5d62351b015958786f6c500be2211e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 11:33:48 +0100

    11.33

--- codigo_mar_4_8/operadores.py
+++ codigo_mar_4_8/operadores.py
@@ -24,7 +24,11 @@ print(
 # Traducir la fórmula de la Ec. de 2º grado:
 # ax^2 + bx + c = 0
 
-a, b, c = 1, 5, 4  # -1, -4
-x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
-x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
-print("x1", x1, "x2", x2)
+a, b, c = 1, 2, 3  # 1,5,4
+raiz = b**2 - 4 * a * c
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2 * a)
+    x2 = (-b - math.sqrt(raiz)) / (2 * a)
+    print("x1", x1, "x2", x2)
+else:
+    print("no hay solución")
