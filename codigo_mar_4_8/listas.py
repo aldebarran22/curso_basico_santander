07a7edff7e28a8d84e1cfd11a7d067d3c8ad2108
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 12:50:53 +0100

    12.50

--- codigo_mar_4_8/listas.py
+++ codigo_mar_4_8/listas.py
@@ -6,33 +6,45 @@ Listas en Python:
 - Funciones sobre listas
 - Slicing
 """
+
 cad = "Hola que tal"
 L = list(cad)
 print(L)
 
-L2 = [1,2,3,4,5]
+L2 = [1, 2, 3, 4, 5]
 print(L2)
 
 # Las listas se pueden modificar:
 L2[0] = 400
 print(L2)
-print('EL último de la lista:', L2[4], L2[-1])
-print('El primero de la lista: ', L2[0], L2[-5])
+print("EL último de la lista:", L2[4], L2[-1])
+print("El primero de la lista: ", L2[0], L2[-5])
 
 # Añadir elementos a la lista:
-L2 += [120] # L2 = L2 + [120]
+L2 += [120]  # L2 = L2 + [120]
 print(L2)
 
-print([1,2,3]+[4,5,6])
-print([1,2,3]*10)
+print([1, 2, 3] + [4, 5, 6])
+print([1, 2, 3] * 10)
 
-print('L2',L2)
+print("L2", L2)
 numero = 5
 if numero in L2:
-    print('existe')
+    print("existe")
 else:
-    print('no existe')
+    print("no existe")
 
 # Crear una lista vacia:
 L = []  # L = list()
 print(L)
+
+# Dadas 2 listas de números calcular los elementos comunes (sin repetidos)
+L1 = [3, 4, 5, 6, 1, 2, 3, 3, 1]
+L2 = [3, 3, 4, 5, 6, 1, 9, 9, 0]
+R = []
+for i in L1:
+    if i in L2 and i not in R:
+        R += [i]
+print(R)
+
+
