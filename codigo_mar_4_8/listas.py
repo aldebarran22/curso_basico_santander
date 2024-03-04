1b7ff9f125a88074a95fcef1bd4b9cea286bd8bc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 12:20:28 +0100

    12.20

---
+++ codigo_mar_4_8/listas.py
@@ -0,0 +1,14 @@
+"""
+Listas en Python:
+- Crear: [] o con list()
+- Acceso a los elementos
+- Operadores: +, *, in
+- Funciones sobre listas
+- Slicing
+"""
+cad = "Hola que tal"
+L = list(cad)
+print(L)
+
+L2 = [1,2,3,4,5,4,3,2,1,2,4]
+print(L2)
