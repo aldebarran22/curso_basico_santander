2b9196aaefad2b5fd025ee3f7ded95b3ba144684
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 12:06:48 +0100

    12.06

--- codigo_mar_4_8/cadenas.py
+++ codigo_mar_4_8/cadenas.py
@@ -1,10 +1,20 @@
 """
 Operadores a nivel de cadenas (+, *, in)
 """
+
 cad1 = "123"
 cad2 = "456"
 print(cad1 + cad2)
+print(cad1 * 10)
 
 n1 = int(cad1)
 n2 = int(cad2)
 print(n1 + n2)
+print(n1 * 10)
+
+# Operador in
+cad = "hola que tal"
+if "hola" in cad:
+    print("si está")
+else:
+    print("no está")
