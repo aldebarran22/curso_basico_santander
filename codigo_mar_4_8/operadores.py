11026d9678730514f78e2cc6576e31990ac0de88
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 11:03:06 +0100

    11.03

--- codigo_mar_4_8/operadores.py
+++ codigo_mar_4_8/operadores.py
@@ -1,6 +1,7 @@
 """
 Pruebas con operadores aritméticos
 """
+
 import math
 
 # Calcular la media de dos números:
@@ -16,11 +17,14 @@ importe = 230
 billete = 50
 numBilletes50 = importe // billete
 restante = importe % billete
-print(f"Billetes de {billete} para el importe: {importe} son {numBilletes50}, resto: {restante}")
+print(
+    f"Billetes de {billete} para el importe: {importe} son {numBilletes50}, resto: {restante}"
+)
 
 # Traducir la fórmula de la Ec. de 2º grado:
 # ax^2 + bx + c = 0
 
-a,b,c = 1,5,4 # -1, -4
-x1 = -b + math.sqrt(b**2)
-x2 = -b - math.sqrt()
+a, b, c = 1, 5, 4  # -1, -4
+x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+print("x1", x1, "x2", x2)
