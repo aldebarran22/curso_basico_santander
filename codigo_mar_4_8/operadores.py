1c84cb8605fb399fcb72d981ab70859aa2adde63
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 10:39:31 +0100

    10.39

--- codigo_mar_4_8/operadores.py
+++ codigo_mar_4_8/operadores.py
@@ -6,6 +6,13 @@ Pruebas con operadores aritméticos
 n1 = 123
 n2 = 900
 media = (n1 + n2) / 2
-print('media: ', media)
+print("media: ", media)
 # f-string
 print(f"La media de {n1} y {n2} es {media}")
+
+# Cuanto billetes de 50 necesito para desglosar 230 euros
+importe = 230
+billete = 50
+numBilletes50 = importe // billete
+restante = importe % billete
+print(f"Billetes de {billete} para el importe: {importe}, resto: {restante}")
