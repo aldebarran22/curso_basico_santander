9eeef1ef8f21b39769f3a0a4288703a064bafda2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-04 14:44:25 +0100

    14.44

---
+++ codigo_mar_4_8/conjuntos.py
@@ -0,0 +1,15 @@
+"""
+Conjuntos en Python:
+- {}, set
+- Quitar repetidos
+- Operadores a nivel de conjuntos:
+& intersección
+| unión
+- diferencia
+^ diferencia simétrica
+"""
+# Quitar repetidos de una lista:
+L = [1, 2, 3, 4, 5, 6, 7, 7 , 8, 1,2,3,7,8]
+c = set(L)
+print(c)
+
