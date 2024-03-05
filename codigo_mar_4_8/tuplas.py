bfd2fdee972a94e7cb2c740d883656378cc404c8
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 10:10:10 +0100

    10.10

--- codigo_mar_4_8/tuplas.py
+++ codigo_mar_4_8/tuplas.py
@@ -4,6 +4,7 @@ Tuplas en Python
 - intercambiar variables
 - paso de parámetros a funciones
 - resultados de una consulta a la BD
+- select * from clientes where pais = ? ('España',)
 - llamada a funciones ...
 - operadores: +, *, in
 - Slicing, indices + / -
@@ -14,3 +15,9 @@ t2 = 5, 6, 7, 8
 
 print(t, type(t))
 print(t2, type(t2))
+
+t3 = tuple('hola que tal')
+print(t3)
+print('los 3 últimos: ', t3[-3:])
+print('a' in t3)
+
