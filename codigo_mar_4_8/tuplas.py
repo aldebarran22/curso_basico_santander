469372957107857216456bf0e9a5a8c82f381c33
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 10:03:59 +0100

    10.03

---
+++ codigo_mar_4_8/tuplas.py
@@ -0,0 +1,16 @@
+"""
+Tuplas en Python
+- inicializar múltiples variables
+- intercambiar variables
+- paso de parámetros a funciones
+- resultados de una consulta a la BD
+- llamada a funciones ...
+- operadores: +, *, in
+- Slicing, indices + / -
+"""
+
+t = (1, 2, 3, 4)
+t2 = 5, 6, 7, 8
+
+print(t, type(t))
+print(t2, type(t2))
