f2f7c86a36524b6ba799048d13d60551c12ccce2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 13:53:17 +0100

    13.53

--- codigo_mar_4_8/funciones2_prueba.py
+++ codigo_mar_4_8/funciones2_prueba.py
@@ -3,7 +3,20 @@ Importar el módulo: funciones2 y ejecutar la
 función: filtroFicheros
 """
 
+"""
 # opción 1: import modulo
 import funciones2
 
 funciones2.filtroFicheros(".", "py")
+
+
+# opción 2: import modulo as alias
+import funciones2 as f
+
+f.filtroFicheros(".", "py")
+"""
+
+# opción 3: from modulo import funcion
+from funciones2 import filtroFicheros
+
+filtroFicheros(".", "py")
