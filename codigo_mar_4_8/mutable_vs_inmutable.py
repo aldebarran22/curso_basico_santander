4260e7c7465e63b81255c242cffca821489c563c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 09:45:19 +0100

    9.45

--- codigo_mar_4_8/mutable_vs_inmutable.py
+++ codigo_mar_4_8/mutable_vs_inmutable.py
@@ -31,3 +31,9 @@ L[0] = 1000
 # modificar el objeto: inmutable error!
 # cad[0] = 'X'
 
+# Copiar objetos mutables: list, dict y set
+L1 = [1, 2, 3, 4, 5]
+L2 = L1  # Crea una referencia a los mismos datos
+L1[0] = 1000
+print("L1", L1, id(L1))
+print("L2", L2, id(L2))
