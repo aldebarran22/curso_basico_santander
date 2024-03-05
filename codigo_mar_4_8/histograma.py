2a107d4b047ca50ac94aba788c0081832ceaa7fa
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 09:17:33 +0100

    9.17

---
+++ codigo_mar_4_8/histograma.py
@@ -0,0 +1,13 @@
+"""
+Generar un histograma a partir de una lista con
+elementos repetidos
+"""
+
+cad = "Generar un histograma a partir de una lista con elementos repetidos"
+L = list(cad)
+letras = set(L)
+histo = dict()
+for letra in letras:
+    histo[letra] = L.count(letra)
+print(histo)
+
