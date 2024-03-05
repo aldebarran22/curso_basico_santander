666802fdabd9a4a88f2a07f1030929c33d66bd1f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 10:35:56 +0100

    10.35

---
+++ codigo_mar_4_8/filtro_ficheros.py
@@ -0,0 +1,8 @@
+"""
+Listar los ficheros de una carpeta del equipo e imprimir
+solo los ficheros de una determinada extensión.
+"""
+import os
+
+L = os.listdir()
+print(L)
--- codigo_mar_4_8/tuplas.py
+++ codigo_mar_4_8/tuplas.py
@@ -16,22 +16,21 @@ t2 = 5, 6, 7, 8
 print(t, type(t))
 print(t2, type(t2))
 
-t3 = tuple('hola que tal')
+t3 = tuple("hola que tal")
 print(t3)
-print('los 3 últimos: ', t3[-3:])
-print('a' in t3)
+print("los 3 últimos: ", t3[-3:])
+print("a" in t3)
 print(t3 + t)
-print((1,2)*3)
+print((1, 2) * 3)
 
 L = [(40.4, -3.68), (34, -77), (45.6, -12), (-45, 77)]
 print(L[0][0])
-print('len:',len(L))
+print("len:", len(L))
 for t in L:
     print(t)
 
 # Desempaquetar/expandir tuplas
 for lat, lon in L:
-    dirLat = 'N' if lat > 0 else 'S'
-    dirLon = 'E' if lon > 0 else 'W'
-    print(f"{lat}{dirLat}, {lon}{dirLon}")
-
+    dirLat = "N" if lat > 0 else "S"
+    dirLon = "E" if lon > 0 else "W"
+    print(f"{abs(lat)}{dirLat}, {abs(lon)}{dirLon}")
