9555bb1763a8e8294479443b56337a3a9c806edb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 12:49:08 +0100

    12.49

--- codigo_mar_4_8/funciones_param.py
+++ codigo_mar_4_8/funciones_param.py
@@ -6,12 +6,16 @@ Tipos de parámetros en una función:
 - en un diccionario
 """
 
+
 def funcion(ob, op=0, *args, **kwargs):
-    print('ob: ', ob)
-    print('op: ', op)
-    print('*args: ', args)
-    print('**kwargs: ', kwargs)
+    print("ob: ", ob)
+    print("op: ", op)
+    print("*args: ", args)
+    print("**kwargs: ", kwargs)
     print()
 
+
 funcion(100)
-funcion(1,2,3,4)
+funcion(1, 2, 3, 4)
+funcion(1, 2, x=3, y=4)
+funcion(1, 2, 3, 4, 5, 6, x=300, y=400)
