29a3e056ceb7c94c16e820fbbeb062249e738837
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 10:45:32 +0100

    10.45

--- codigo_mar_4_8/filtro_ficheros.py
+++ codigo_mar_4_8/filtro_ficheros.py
@@ -5,7 +5,8 @@ solo los ficheros de una determinada extensión.
 import os
 
 L = os.listdir('../ficheros_curso')
+extensiones = ('csv','txt')
 for fich in L:
     ext = fich.partition('.')[2]
-    if ext == 'csv':
+    if ext in extensiones:
         print(fich)
