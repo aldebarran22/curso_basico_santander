54ed6f895390846cdf5a1bd72e7d14589eb04273
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 12:44:17 +0100

    12.44

---
+++ codigo_mar_4_8/funciones_param.py
@@ -0,0 +1,16 @@
+"""
+Tipos de parámetros en una función:
+- obligatorios
+- opcionales
+- en una tupla
+- en un diccionario
+"""
+
+def funcion(ob, op=0, *args, **kwargs):
+    print('ob: ', ob)
+    print('op: ', op)
+    print('*args: ', args)
+    print('**kwargs: ', kwargs)
+    print()
+
+funcion(100)
