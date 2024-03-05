2f3f095a702648e17e661595756668bad0e5c201
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-05 11:44:39 +0100

    11.44

--- codigo_mar_4_8/control_flujo.py
+++ codigo_mar_4_8/control_flujo.py
@@ -3,5 +3,8 @@ range, bucle while, break y continue
 """
 
 # range(ini, fin-1, salto=1)
-print(list(range(5))) # del 0 al 4
-print(list(range(1,11))) # del 1 al 10
+print(list(range(5)))  # del 0 al 4
+print(list(range(1, 11)))  # del 1 al 10
+print(list(range(10, 0, -1)))  # del 10 al 1
+print(list(range(0, 101, 5)))  # del 0 al 100 de 5 en 5
+print(tuple(range(0, 101, 5)))  # del 0 al 100 de 5 en 5
