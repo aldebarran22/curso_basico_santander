1dca6c73e7f1514a5a57745524e52792f218f61b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-06 10:12:18 +0100

    10.12

---
+++ codigo_mar_4_8/ordenaciones.py
@@ -0,0 +1,9 @@
+"""
+Ordenar colecciones en Python
+- Para listas: método sort
+- Para otras colecciones: función sorted
+ (devuelve una lista ordenada con los datos de la colección)
+"""
+L = ["Miguel","Juan","Andrea","Sara","Ana", "Alberto"]
+L.sort()
+print(L)
