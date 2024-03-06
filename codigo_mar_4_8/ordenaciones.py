c9900248744cf74b7ca431688d8613cc584917a8
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-06 10:17:34 +0100

    10.17

--- codigo_mar_4_8/ordenaciones.py
+++ codigo_mar_4_8/ordenaciones.py
@@ -4,6 +4,15 @@ Ordenar colecciones en Python
 - Para otras colecciones: función sorted
  (devuelve una lista ordenada con los datos de la colección)
 """
-L = ["Miguel","Juan","Andrea","Sara","Ana", "Alberto"]
+
+L = ["Miguel", "Juan", "Andrea", "Sara", "Ana", "Alberto"]
 L.sort()
 print(L)
+# Ordenar por la longitud del nombre:
+L.sort(key=len)
+print(L)
+
+t = (45, 3, 11, 23, 67, 88, 90)
+R = sorted(t, reverse=True)
+print(t)
+print(R)
