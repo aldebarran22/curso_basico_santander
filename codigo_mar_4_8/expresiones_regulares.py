e7af5eeb3986079ea4422959065393dd47295558
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-07 13:40:47 +0100

    13.40

--- codigo_mar_4_8/expresiones_regulares.py
+++ codigo_mar_4_8/expresiones_regulares.py
@@ -14,16 +14,16 @@ def validar(patron, L):
 
 if __name__ == "__main__":
     # Cadenas de 3 caracteres:
-    patron = "...$"
+    patron = r"...$"
     L = ["***", "1234", "abc", "--a", "12345"]
     validar(patron, L)
 
     # Cadenas de 3 caracteres y que terminan en punto:
-    patron = "...\.$"
-    L = ["abc.","***.", "1234.", "abc.", "--a.", "12345."]
+    patron = r"...\.$"
+    L = ["abc.", "***.", "1234.", "abc.", "--a.", "12345.", "123"]
     validar(patron, L)
 
     # DNI:
-    patron = ""
-    L = ["AWWEE", "51000123", "65000123J","12345678AA"]
-    # validar(patron, L)
+    patron = r"[0-9]{8}[A-Z]$"
+    L = ["AWWEE", "51000123", "65000123J", "12345678AA","12345678b"]
+    validar(patron, L)
