ec9db150fe8951d09edec5985d6c72505c56eb37
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-07 11:33:22 +0100

    11.33

---
+++ codigo_mar_4_8/ficheros_empleados.py
@@ -0,0 +1,11 @@
+"""
+Cargar en dos conjuntos de string los ficheros de
+empleados: empleados2.txt y empleados3.txt
+"""
+
+def cargaFichero(path):
+    pass
+
+if __name__ == "__main__":
+    c1 = cargaFichero("../ficheros_curso/Empleados2.txt")
+    c2 = cargaFichero("../ficheros_curso/Empleados3.txt")
