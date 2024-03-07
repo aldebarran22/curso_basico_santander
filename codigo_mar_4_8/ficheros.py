a8e448ed96b70cd1031e519d0fa63ecab2c3d78c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-07 10:52:52 +0100

    10.52

--- codigo_mar_4_8/ficheros.py
+++ codigo_mar_4_8/ficheros.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def leerFichero(path, pais, file=sys.stdout):
     fich = None
     try:
@@ -18,14 +19,23 @@ def leerFichero(path, pais, file=sys.stdout):
         if fich:
             fich.close()
 
- 
+
+def exportar(path, pais):
+    pathPais = f"../ficheros/{pais}.csv"
+    fich = open(pathPais, "w")
+    leerFichero(path, pais, file=fich)
+    fich.close()
+
+
 def grabarFichero(path):
     fich = open(path, "w")
     for i in range(10):
         print(f"linea de pedido {i+1}", file=fich)
     fich.close()
 
+
 if __name__ == "__main__":
-    #leerFichero(r"..\ficheros_curso\Pedidos.csv")   
-    #leerFichero("../ficheros_curso/Pedidos.csv", "Noruega")
-    grabarFichero("../ficheros/prueba.txt")
+    # leerFichero(r"..\ficheros_curso\Pedidos.csv")
+    # leerFichero("../ficheros_curso/Pedidos.csv", "Noruega")
+    # grabarFichero("../ficheros/prueba.txt")
+    exportar("../ficheros_curso/Pedidos.csv", "Noruega")
