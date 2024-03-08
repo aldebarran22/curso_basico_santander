c36681738344e8ec1a5bcda2259418f1d9725098
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-08 09:29:15 +0100

    9.29

--- codigo_mar_4_8/base_datos.py
+++ codigo_mar_4_8/base_datos.py
@@ -8,17 +8,24 @@ from os.path import isfile
 
 def imprimirTabla(path, tabla):
     con = None
+    cur = None
     try:
         if not isfile(path):
             raise FileNotFoundError(f"No se encontró el fichero: {path}")
 
         else:
-            print("conectado!")
             con = dbapi.connect(path)
+            sql = f"select * from {tabla}"
+            cur = con.cursor()
+            cur.execute(sql)
+            for t in cur.fetchall():
+                print(t)
 
     except Exception as e:
         print(e)
     finally:
+        if cur:
+            cur.close()
         if con:
             con.close()
 
