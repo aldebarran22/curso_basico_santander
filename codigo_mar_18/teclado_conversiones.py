3a984971352f384cd7cc55ba74ebe37eacfaaa2c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 12:34:10 +0100

    12.34

--- codigo_mar_18/teclado_conversiones.py
+++ codigo_mar_18/teclado_conversiones.py
@@ -17,5 +17,11 @@ n2 = int(input('Dame número:'))
 print(n1 + n2)
 
 # Trunca los decimales:
-num = int(12.4)
+num = int(12.4) # De float a int
 print(num)
+
+resul = str(n1) + str(n2)
+print(resul, type(resul))
+
+r = resul != ""
+print(r, type(r))
