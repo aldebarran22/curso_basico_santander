40996a37bdeeb9e58e193a3091cd1a1aaa9396c3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 10:34:21 +0100

    10.34

--- codigo_mar_18/tipos_basicos.py
+++ codigo_mar_18/tipos_basicos.py
@@ -2,12 +2,16 @@
 Uso de tipos básicos en python, obtener el tipo de variable
 """
 
-numero = 100
-print(numero, type(numero))
+numero1 = 100
+print(numero1, type(numero1))
 
-numero = 23.44
-print(numero, type(numero))
+numero2 = 23.44
+print(numero2, type(numero2))
+
+print(numero1 + numero2)
 
 n1 = "23"
 n2 = "55"
 print(n1 + n2)
+
+# print(n1 + numero) # tipos incompatibles
