d428a66e771e17809d102961ad773e2c8aa0d7c3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 12:18:48 +0100

    12.18

---
+++ codigo_mar_18/teclado_conversiones.py
@@ -0,0 +1,13 @@
+"""
+Tipos numéricos: int, float, complex
+Funciones de conversión de tipos: int(), float(), complex() y str()
+Leer de teclado: input()
+"""
+
+# Operar con datos numéricos: int -> float -> complex
+
+resul = 12 + 45.6
+print(resul, type(resul))
+
+resul = 12 + 45.6 + 9+4j
+print(resul, type(resul))
