a0bd5a49ae15345b785baf49f4a61033e3139444
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 10:31:40 +0100

    10.31

---
+++ codigo_mar_18/tipos_basicos.py
@@ -0,0 +1,13 @@
+"""
+Uso de tipos básicos en python, obtener el tipo de variable
+"""
+
+numero = 100
+print(numero, type(numero))
+
+numero = 23.44
+print(numero, type(numero))
+
+n1 = "23"
+n2 = "55"
+print(n1 + n2)
