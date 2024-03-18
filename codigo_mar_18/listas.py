bb919e2136275cf220231735fbcca39cde873706
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 12:49:22 +0100

    12.49

--- codigo_mar_18/listas.py
+++ codigo_mar_18/listas.py
@@ -11,3 +11,14 @@ Listas en Python:
 L = [1,4,5,4,2,3,1]
 L2 = [True, 123, [4,5,6], "hola"]
 print(L, type(L))
+
+L3 = list('hola que tal')
+print(L3)
+
+# Funciones de uso general:
+L = [12, 33, 45, 77]
+print('Recuento: ', len(L))
+print('Suma: ', sum(L))
+print('Media:', sum(L) / len(L))
+print('minimo: ', min(L))
+print('maximo: ', max(L))
