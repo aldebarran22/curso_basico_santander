59e6e67b474765e2db8aaf425d67d31381bf610d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 12:55:04 +0100

    12.54

--- codigo_mar_18/listas.py
+++ codigo_mar_18/listas.py
@@ -1,6 +1,7 @@
 """
 Listas en Python:
 - crear listas
+- operadores: +, *, in
 - índices + y -, slicing, 
 - bucle: for
 - funciones de uso general
@@ -22,3 +23,17 @@ print('Suma: ', sum(L))
 print('Media:', sum(L) / len(L))
 print('minimo: ', min(L))
 print('maximo: ', max(L))
+
+# operadores: +, *, in
+L1 = [1,2,3]
+L2 = [4,5,6]
+print(L1 + L2)
+print(L1 * 5)
+print("5 in L1:", 5 in L1)
+print("2 in L1: ", 2 in L1)
+
+# Añadir elementos a una lista:
+R = []
+R += [2]
+R += [3]
+print(R)
