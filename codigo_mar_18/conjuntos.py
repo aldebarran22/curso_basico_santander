afb48eb1be0978ff3e4156bc20248480d573dbac
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 14:45:56 +0100

    14.45

--- codigo_mar_18/conjuntos.py
+++ codigo_mar_18/conjuntos.py
@@ -4,9 +4,16 @@ crear, recorrer, operadores, quitar repetidos
 """
 
 c1 = {5,7,8,8,7,2,1,2,4}
+c1.add(8)
 print(c1, type(c1))
 
 # Quitar repetidos a un lista:
 L = [4,5,6,2,1,2,1,2,4,5,6,7,8,8]
 L2 = list(set(L))
 print(L2)
+
+comida = {"Sara", "Jorge","Ana", "Juan", "Miguel"}
+cena = {"Ana", "Raúl","Alberto", "María","Miguel"}
+
+print('Quien va a comer y a cenar: ', comida & cena)  # intersección
+
