880fa42716300ddac1d6f6970602d51ce85ac45e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 14:40:03 +0100

    14.39

--- codigo_mar_18/conjuntos.py
+++ codigo_mar_18/conjuntos.py
@@ -4,4 +4,9 @@ crear, recorrer, operadores, quitar repetidos
 """
 
 c1 = {5,7,8,8,7,2,1,2,4}
-print(c1, set(c1))
+print(c1, type(c1))
+
+# Quitar repetidos a un lista:
+L = [4,5,6,2,1,2,1,2,4,5,6,7,8,8]
+L2 = list(set(L))
+print(L2)
