6285d786f2dc1f08502aa48388cf4176218ed4c3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 13:43:08 +0100

    13.43

---
+++ codigo_mar_18/diccionarios.py
@@ -0,0 +1,14 @@
+"""
+Crear diccionarios
+Funciones para crear diccionarios
+Acceso a elementos
+Añadir claves
+histogramas
+"""
+
+d = {"a":1, "b":2, "c":3}
+print(d, type(d))
+
+d["a"] = 500
+d["d"] = 4
+print(d)
