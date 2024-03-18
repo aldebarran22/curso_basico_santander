a549af8439daa1d8a35c54da65f07a6ed90e2cce
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-18 12:43:37 +0100

    12.43

---
+++ codigo_mar_18/listas.py
@@ -0,0 +1,13 @@
+"""
+Listas en Python:
+- crear listas
+- índices + y -, slicing, 
+- bucle: for
+- funciones de uso general
+- acceso a los elementos
+- copia
+""" 
+
+L = [1,4,5,4,2,3,1]
+L2 = [True, 123, [4,5,6], "hola"]
+print(L, type(L))
