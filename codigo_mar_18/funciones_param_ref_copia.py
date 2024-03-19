8c4d1b424c2c2c67f6ae6038f2209aa7c840c5b6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 14:39:23 +0100

    14.39

--- codigo_mar_18/funciones_param_ref_copia.py
+++ codigo_mar_18/funciones_param_ref_copia.py
@@ -9,13 +9,15 @@ def copia(n, t, c):
     c += "hola"
 
 
-def referencias(L: list, c: set, d: dict) -> None:
+def referencias(L: list = list(), c: set = set(), d: dict = dict()) -> None:
     L.append(1000)
     c.add(1000)
     d["a"] = 1000
 
 
 if __name__ == "__main__":
+    referencias()
+
     lista = [1, 2, 3, 4, 5]
     conjunto = {1, 2, 3, 4, 5}
     diccionario = {"a": 1, "b": 2, "c": 3, "d": 4}
