7e0e83807c2cc29d771eaa43e437f10671ceff3f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 09:36:52 +0100

    9.36

---
+++ codigo_mar_18/tuplas.py
@@ -0,0 +1,15 @@
+"""
+Tuplas en Python
+- Definir tuplas, 
+- Acceso a los elementos
+- Operadores: +, *, in
+- count, index
+"""
+
+# Inicializar múltiples variables
+a,b,c = 1,2,3
+
+t1 = (1,2,3,4,5,6)
+t2 = 6,5,4,3,1
+print(t1, type(t1))
+print(t2, type(t2))
