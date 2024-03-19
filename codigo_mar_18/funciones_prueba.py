630e786d72e1fde42c0d82add98ef4f740616e58
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 13:53:32 +0100

    13.53

--- codigo_mar_18/funciones_prueba.py
+++ codigo_mar_18/funciones_prueba.py
@@ -2,3 +2,16 @@
 Importar el módulo de funciones y ejecutar 
 una función.
 """
+
+
+import funciones
+print(funciones.filtroFicheros('txt'))
+
+
+import funciones as f
+print(f.filtroFicheros('txt'))
+
+
+# from modulo import funcion
+from funciones import filtroFicheros
+print(filtroFicheros('txt'))
