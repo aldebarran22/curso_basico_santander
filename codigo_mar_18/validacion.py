e073d4133876b71f28c0329daa561f16a6f2efeb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 11:49:39 +0100

    11.49

--- codigo_mar_18/validacion.py
+++ codigo_mar_18/validacion.py
@@ -4,10 +4,14 @@ importes múltiplos de 10
 """
 
 while True:
-    importe = int(input("Teclear importe:>"))
-    if importe % 10 == 0:
-        break
+    sImporte = input("Teclear importe:>")
+    if sImporte.isnumeric():
+        importe = int(sImporte)
+        if importe % 10 == 0:
+            break
+        else:
+            print("El importe debe ser un múltiplo de 10.")
     else:
-        print("El importe debe ser un múltiplo de 10.")
+        print('Sólo se acpetan números')
 
 
