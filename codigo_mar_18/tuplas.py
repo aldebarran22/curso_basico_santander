de5554822daa8fca1c8097222cd4d136cf99f646
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 09:43:11 +0100

    9.43

--- codigo_mar_18/tuplas.py
+++ codigo_mar_18/tuplas.py
@@ -4,12 +4,28 @@ Tuplas en Python
 - Acceso a los elementos
 - Operadores: +, *, in
 - count, index
+- Sentencias SQL parametrizadas:
+- select * from clientes where pais=?
 """
 
 # Inicializar múltiples variables
-a,b,c = 1,2,3
+a, b, c = 1, 2, 3
 
-t1 = (1,2,3,4,5,6)
-t2 = 6,5,4,3,1
+t1 = (1, 2, 3, 4, 5, 6)
+t2 = 6, 5, 4, 3, 1
 print(t1, type(t1))
 print(t2, type(t2))
+
+t3 = 8
+print(t3, type(t3))
+
+t3 = (None,)
+print(t3, type(t3))
+
+# t3[0] = 10 # Error no se puede modificar
+t3 += (8,9,0)
+print(t3)
+
+# intercambio de variables:
+a,b = b,a
+print('a:', a, 'b:',b)
