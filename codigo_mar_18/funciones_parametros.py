ba05ba3bf7b3a985e6a67672e8a647a5164b3e0a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 12:42:40 +0100

    12.42

--- codigo_mar_18/funciones_parametros.py
+++ codigo_mar_18/funciones_parametros.py
@@ -19,7 +19,19 @@ def sumar(a: str, b: str) -> str:
     return a + b
 
 
+# Tipos de parámetros en la función:
+def funcion(ob1, ob2, op1=10, op2=20, *args, **kwargs):
+    print("obligatorios: ", ob1, ob2)
+    print("opcionales: ", op1, op2)
+    print("args: ", args)
+    print("kwargs: ", kwargs)
+    print()
+
+
 print(calcularIVA(100))
 print(calcularIVA(100, 4))
 print(sumar("hola", "adios"))
 print(sumar(12, 34))
+
+###
+funcion(1,2)
