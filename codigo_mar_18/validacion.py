ad7c621e2b898521460df9f1847667b72ea2ba15
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 11:43:52 +0100

    11.43

--- codigo_mar_18/validacion.py
+++ codigo_mar_18/validacion.py
@@ -3,8 +3,11 @@ Validar datos de teclado. Sólo se aceptan
 importes múltiplos de 10
 """
 
-importe = int(input("Teclear importe:>"))
-if importe % 10 == 0:
-    print("es multiplo de 10")
-else:
-    print("no lo es")
+while True:
+    importe = int(input("Teclear importe:>"))
+    if importe % 10 == 0:
+        break
+    else:
+        print("El importe debe ser un múltiplo de 10.")
+
+
