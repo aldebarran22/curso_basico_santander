6e6f869868d79918652d778c851198810af0b6de
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 10:47:56 +0100

    10.47

--- codigo_mar_18/copiar_objetos.py
+++ codigo_mar_18/copiar_objetos.py
@@ -8,3 +8,11 @@ L2 = L1  # No es una copia, es una referencia.
 L1[0] = 1000
 print("L1: ", L1, id(L1))
 print("L2: ", L2, id(L2))
+print('-'*10)
+
+L1 = [1, 2, 3, 4, 5, 6]
+L2 = L1.copy()
+L1[0] = 1000
+print("L1: ", L1, id(L1))
+print("L2: ", L2, id(L2))
+print('-'*10)
