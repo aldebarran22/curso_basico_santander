ca842bb389a921b93df3beb9d063005fe795b9fc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 10:50:23 +0100

    10.50

--- codigo_mar_18/copiar_objetos.py
+++ codigo_mar_18/copiar_objetos.py
@@ -16,3 +16,11 @@ L1[0] = 1000
 print("L1: ", L1, id(L1))
 print("L2: ", L2, id(L2))
 print('-'*10)
+
+L1 = [[1, 2, 3], [4, 5, 6]]
+L2 = L1.copy()
+L1[0] = 1000
+L1[-1].append(999)
+print("L1: ", L1, id(L1))
+print("L2: ", L2, id(L2))
+print('-'*10)
