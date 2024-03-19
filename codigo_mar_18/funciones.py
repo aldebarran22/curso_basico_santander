7f4756ae55e5d1a07dc144b58bad69852e409d9b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 13:18:51 +0100

    13.18

--- codigo_mar_18/funciones.py
+++ codigo_mar_18/funciones.py
@@ -5,8 +5,15 @@ Funciones en Python
 import os
 
 
-L = os.listdir()
-for f in L:
-    ext = f.partition(".")[2]
-    if ext in ("txt", "py"):
-        print(f)
+def filtroFicheros(*extensiones, path=None):
+    L = os.listdir(path)
+    R = []
+    for f in L:
+        ext = f.partition(".")[2]
+        if ext in extensiones:
+            R.append(f)
+    return R
+
+print(filtroFicheros('py','txt', path='.'))
+print(filtroFicheros('txt', path='.'))
+
