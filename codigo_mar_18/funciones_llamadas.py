02636507afe549f263ed191dc4e54a85021a030c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 12:07:12 +0100

    12.07

--- codigo_mar_18/funciones_llamadas.py
+++ codigo_mar_18/funciones_llamadas.py
@@ -8,16 +8,20 @@ Formas de llamar a una función en Python
 Tipos anotados
 """
 
-def sumar(a,b):
-    return a+b
+
+def sumar(a, b):
+    return a + b
+
 
 def segundos(h=0, m=0, s=0):
-    return h*3600 + m*60 + s
+    return h * 3600 + m * 60 + s
+
 
 # posicional:
-print(sumar(1,2))
+print(sumar(1, 2))
+print(segundos(12, 30))  # 12:30:00
 
 # nominal:
 print(sumar(b=2, a=1))
-print(segundos(m=10, s=15))
-
+print(segundos(m=10, s=15))  # 00:10:15
+print(segundos(12, s=10))  # 12:00:10
