5046c145e3d397eadaa227f10b29d2d458c5dfa3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-19 09:10:18 +0100

    9.10

---
+++ codigo_mar_18/histograma.py
@@ -0,0 +1,12 @@
+"""
+Obtener un histograma con los valores que
+se repiten en una lista.
+"""
+
+# range(ini, fin-1, salto=1)
+print(list(range(10)))
+print(list(range(1,11)))
+
+for i in range(10):
+    print(i, end=' ')
+print()
