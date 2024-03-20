5eaaedcf406988511fdeddec273b8bcc1e34308e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-20 10:35:50 +0100

    10.35

--- codigo_mar_18/menu_dinamico.py
+++ codigo_mar_18/menu_dinamico.py
@@ -16,10 +16,18 @@ def salir(*args):
     exit()
 
 
+def menu(L):
+    print("Menú principal")
+    for pos, f in enumerate(L):
+        print(pos + 1, f.__name__)
+    op = int(input("Seleccionar op:"))
+    return op
 
 
 if __name__ == "__main__":
     L = [sumar, restar, salir]
     while True:
+        a = int(input("a: "))
+        b = int(input("b: "))
         op = menu(L)
-
+        L[op-1](a,b)
