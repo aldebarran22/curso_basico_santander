80edf316265e109d4709a78d88c3a7df54f223c1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-20 13:24:50 +0100

    13.24

--- codigo_mar_18/fecha_hora.py
+++ codigo_mar_18/fecha_hora.py
@@ -33,6 +33,9 @@ class DateTime(Date,Time):
         Date.__init__(self, dd, mm, yy)
         Time.__init__(self, h, m, s)
 
+    def __str__(self):
+        return Date.__str__(self)+" "+Time.__str__(self)
+
 
 if __name__ == "__main__":
     t1 = Time(1, 3, 5)  # 01:03:05
