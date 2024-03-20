33cdf1fd200a45d179f250f476a56f25ae53be07
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-20 11:30:52 +0100

    11.30

---
+++ codigo_mar_18/objetos.py
@@ -0,0 +1,19 @@
+"""
+P.O.O en Python
+"""
+
+
+class Persona:
+    """Implementación de la clase persona"""
+
+    def __init__(self, nombre="", peso=0, altura=0.0):
+        # Definición de att.
+        self.nombre = nombre
+        self.peso = peso
+        self.altura = altura
+
+def testPersona():
+    pass
+
+if __name__ == '__main__':
+    testPersona()
