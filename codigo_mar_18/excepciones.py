b3589d277033e4661c75e1b25a8a3d54697b20dd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-20 14:14:10 +0100

    14.14

---
+++ codigo_mar_18/excepciones.py
@@ -0,0 +1,18 @@
+"""
+Capturar excepciones en Python:
+try, except, finally, raise
+"""
+
+
+def test1():
+    # Capturar 2 excepciones distintas
+    pass
+
+
+def test2():
+    # Capturar 2 excepciones distintas en una tupla (agrupar excepciones)
+    pass
+
+
+if __name__ == "__main__":
+    test1()
