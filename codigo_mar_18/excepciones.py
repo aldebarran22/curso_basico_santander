838c5f3bc3d2c6f3551c37218e0a6952786d36b8
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-20 14:22:01 +0100

    14.21

--- codigo_mar_18/excepciones.py
+++ codigo_mar_18/excepciones.py
@@ -7,17 +7,29 @@ try, except, finally, raise
 def test1():
     # Capturar 2 excepciones distintas
     try:
-        L = [1,2,3,4,5]
+        L = [1, 2, 3, 4, 5]
+        print(numero)
         print(L[10])
-        print('mas codigo')
-        
+        print("mas codigo")
+
     except IndexError as e:
+        print(e.__class__.__name__, e)
+
+    except NameError as e:
         print(e.__class__.__name__, e)
 
 
 def test2():
     # Capturar 2 excepciones distintas en una tupla (agrupar excepciones)
-    pass
+    try:
+        L = [1, 2, 3, 4, 5]
+        print(numero)
+        print(L[10])
+        print("mas codigo")
+
+    except (IndexError, NameError) as e:
+        print(e.__class__.__name__, e)
+
 
 
 if __name__ == "__main__":
