79dc740b9f5cf2e963b34c0cf1f186a237bb0c46
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-21 13:07:13 +0100

    13.07

---
+++ codigo_mar_18/expresiones_regulares.py
@@ -0,0 +1,19 @@
+"""
+Expresiones regulares en Python
+"""
+
+import re
+
+
+def validar(patron, L):
+    print("Patrón: ", patron)
+    for cad in L:
+        print(cad, "True" if re.match(patron, cad) else "False")
+    print("-" * 20)
+
+
+if __name__ == "__main__":
+    # Cadenas de 3 caracteres:
+    patron = r"...$"
+    L = ["***", "1234", "abc", "--a", "12345"]
+    validar(patron, L)
