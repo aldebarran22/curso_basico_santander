76e0298f72d2022fdc65066a1c89566c80cd25c6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-21 11:42:42 +0100

    11.42

--- codigo_mar_18/ficheros.py
+++ codigo_mar_18/ficheros.py
@@ -2,6 +2,20 @@
 Leer y escribir ficheros
 """
 
+def extraerPaises(path):
+    """Leer el fichero de pedidos y devolver una colección con los paises"""
+    fich = None
+    try:
+        fich = open(path, "r")
+        for linea in fich:
+            linea = linea.rstrip()
+            print(linea)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if fich:
 
 def imprimirFichero(path):
     fich = None
