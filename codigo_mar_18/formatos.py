496c301211b1361d3e24f429cc0fd960c011b3f2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-21 10:04:39 +0100

    10.04

--- codigo_mar_18/formatos.py
+++ codigo_mar_18/formatos.py
@@ -6,7 +6,16 @@ path = "../ficheros_curso/Pedidos.csv"
 
 
 def formatearEnCols():
-    pass
+
+    def leerFichero():
+        fich = open(path, "r")
+        txt = fich.read()
+        fich.close()
+        return txt
+
+    # formatearEnCols:
+    csv = leerFichero()
+    print(csv)
 
 
 if __name__ == "__main__":
