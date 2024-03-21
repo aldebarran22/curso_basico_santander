6ea29c993a22254dff2fcdb9c004814ef0842858
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-21 12:56:00 +0100

    12.55

--- codigo_mar_18/directorios.py
+++ codigo_mar_18/directorios.py
@@ -1,12 +1,19 @@
 """Explorar directorios"""
 
 from os import walk
-def ls (ruta = "."):
-    #Devuelve una tupla de 3 elementos.
-    dir, subdirs , archivos = next(walk(ruta))
+
+
+def ls(ruta="."):
+    # Devuelve una tupla de 3 elementos.
+    dir, subdirs, archivos = next(walk(ruta))
     print("Actual: ", dir)
     print("Subdirectorios: ", subdirs)
-    print("Archivos: ", archivos) # Devuelve la lista de
+    print("Archivos: ", archivos[:3])  # Devuelve la lista de
 
-if __name__ == '__main__':
+    for subdir in subdirs:
+        ls(dir + "/" + subdir)
+        print("*" * 30)
+
+
+if __name__ == "__main__":
     ls()
