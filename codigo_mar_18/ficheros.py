d8c2af2cc51a840bd45385a90fe5c81d3add6912
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-21 12:14:28 +0100

    12.14

--- codigo_mar_18/ficheros.py
+++ codigo_mar_18/ficheros.py
@@ -2,6 +2,8 @@
 Leer y escribir ficheros
 """
 
+from formatos import isfloat
+
 
 def extraerPaises(path):
     """Leer el fichero de pedidos y devolver una colección con los paises"""
@@ -45,12 +47,34 @@ def imprimirFichero(path):
 
 def acumuladoPaises(path, paises):
     dict_paises = dict.fromkeys(paises, 0)
-    return dict_paises
+    fich = None
+    try:
+        fich = open(path, "r")
+        for pos, linea in enumerate(fich):
+            linea = linea.rstrip()
+            if linea == "" or pos == 0:
+                continue
+
+            L = linea.split(";")
+            pais = L[-1]
+            importe = float(L[-2]) if isfloat(L[-2]) else 0
+
+            # Actualizar diccionario:
+            dict_paises[pais] += importe
+
+        return dict_paises
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if fich:
+            fich.close()
 
 
 if __name__ == "__main__":
     # imprimirFichero("../ficheros_curso/Pedidos.csv")
     paises = extraerPaises("../ficheros_curso/Pedidos.csv")
-    #print(paises)
+    # print(paises)
     histo = acumuladoPaises("../ficheros_curso/Pedidos.csv", paises)
     print(histo)
