2a7f236fdd80bd28fe89c349e24ab1f7f6d2b6cf
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-22 09:26:10 +0100

    9.26

---
+++ codigo_mar_18/base_datos.py
@@ -0,0 +1,12 @@
+"""
+Conectar con la bd, crear cursores, extraer registros
+"""
+
+import sqlite3 as db
+from os.path import isfile
+
+def listar(path):
+    pass
+
+if __name__ == '__main__':
+    listar("../bd/empresa3.db")
