65ff68b7c0eb454abe5f5281e130c10d44e1503d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-22 10:26:35 +0100

    10.26

---
+++ codigo_mar_18/base_datos_CRUD.py
@@ -0,0 +1,63 @@
+"""Operaciones CRUD para un producto
+C - Create -> insert into
+R - Read -> select por PK
+U - Update -> update
+D - Delete -> delete
+"""
+
+class Categoria:
+
+    def __init__(self, id=0, nombre=""):
+        self.id = id
+        self.nombre = nombre
+
+    def __str__(self):
+        return str(self.id) + " " + self.nombre
+
+    def __repr__(self):
+        return str(self)
+
+
+class Producto:
+
+    def __init__(self, id=0, nombre="", cat=Categoria(), precio=0.0, exis=0):
+        self.id = id
+        self.nombre = nombre
+        self.cat = cat
+        self.precio = round(precio, 2)
+        self.exis = exis
+
+    def __str__(self):
+        return (
+            str(self.id)
+            + " "
+            + self.nombre
+            + " "
+            + str(self.cat)
+            + " "
+            + str(self.precio)
+            + " "
+            + str(self.exis)
+        )
+
+    def __repr__(self):
+        return str(self)
+
+    def getTupla(self):
+        return (self.id, self.nombre, self.cat.id, self.precio, self.exis)
+
+    def getTupla2(self):
+        return (self.nombre, self.cat.id, self.precio, self.exis, self.id)
+
+class BaseDatos:
+
+    def __init__(self, path):
+        pass
+
+    def __del__(self):
+        pass
+
+if __name__ == '__main__':
+    bd = BaseDatos("../bd/empresa3.db")
+    prod = bd.read(3)
+    print(prod)
