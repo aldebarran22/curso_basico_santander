c9df9ce225f1a3515d01d44396fa5990fcd885f9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-22 10:14:26 +0100

    10.14

--- codigo_mar_18/base_datos.py
+++ codigo_mar_18/base_datos.py
@@ -24,7 +24,7 @@ def listar(path, tabla, file=sys.stdout):
         print(cabs, file=file)
         for t in cur.fetchall():
             L = [str(i) for i in t]
-            L = [i.replace('.',',') if isfloat(i) else i for i in L]
+            L = [i.replace(".", ",") if isfloat(i) else i for i in L]
             linea = ";".join(L)
             print(linea, file=file)
 
@@ -44,6 +44,32 @@ def exportar(path, tabla):
     fich.close()
 
 
+def exportarBD(path):
+    con = None
+    cur = None
+    try:
+        if not isfile(path):
+            raise FileNotFoundError(f"El fichero: {path} no existe")
+        con = db.connect(path)
+        # print("conexión ok!")
+        cur = con.cursor()
+        sql = """SELECT name FROM sqlite_master 
+        WHERE type='table' and name not like 'sqlite%'"""
+        cur.execute(sql)
+
+        for t in cur.fetchall():
+            tabla = t[0]
+            exportar(path, tabla)
+
+    except Exception as e:
+        print(e)
+    finally:
+        if cur:
+            cur.close()
+        if con:
+            con.close()
+
+
 if __name__ == "__main__":
     # listar("../bd/empresa3.db", "productos")
-    exportar("../bd/empresa3.db", "productos")
+    exportar("../bd/empresa3.db", "pedidos")
