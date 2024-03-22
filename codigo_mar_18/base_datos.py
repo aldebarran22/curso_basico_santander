d2e2263dc597a40591ef9ad0c5e09d461d346e4f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-03-22 09:29:05 +0100

    9.29

--- codigo_mar_18/base_datos.py
+++ codigo_mar_18/base_datos.py
@@ -5,8 +5,20 @@ Conectar con la bd, crear cursores, extraer registros
 import sqlite3 as db
 from os.path import isfile
 
-def listar(path):
-    pass
 
-if __name__ == '__main__':
+def listar(path):
+    con = None
+    try:
+        if not isfile(path):
+            raise FileNotFoundError(f"El fichero: {path} no existe")
+        print("conexión ok!")
+
+    except Exception as e:
+        print(e)
+    finally:
+        if con:
+            con.close()
+
+
+if __name__ == "__main__":
     listar("../bd/empresa3.db")
