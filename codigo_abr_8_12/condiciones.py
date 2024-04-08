62ebdb6b130e772ec14d6ab146a94e6da62fcae7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-08 11:47:54 +0200

    11.47

--- codigo_abr_8_12/condiciones.py
+++ codigo_abr_8_12/condiciones.py
@@ -25,3 +25,24 @@ if num < ini or num > fin:
     print(' no está en el intervalo')
 else:
     print(f'{num} está dentro del intervalo')
+
+# Comparar dos números, teniendo en cuenta que pueden ser iguales:
+n1 = 10
+n2 = 20
+if n1 < n2:
+    print('n1 < n2')
+
+elif n1 > n2:
+    print('n1 > n2')
+
+else:
+    print("n1 == n2")
+
+# if compacto: A if cond else B
+
+# imprimir si el numero es par o impar:
+n = 125
+print("par" if n % 2 == 0 else "impar")
+menor = n1 if n1 < n2 else n2
+
+
