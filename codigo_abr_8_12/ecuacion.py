51c79d8c3ab26edb402811832becf715ca211232
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-08 11:03:46 +0200

    11.03

--- codigo_abr_8_12/ecuacion.py
+++ codigo_abr_8_12/ecuacion.py
@@ -1,9 +1,12 @@
 """
 Resolver ecu de 2 grado
 """
+import math 
 
 # inicializar múltiples variables:
 a,b,c = 1,5,4
 
-x1 = -b + ...
-x2 = -b - ...
+x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+
+print(x1, x2)
