3e24ef801eeead82fc1857c036591c6cde0c45ad
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-08 11:32:33 +0200

    11.32

--- codigo_abr_8_12/ecuacion.py
+++ codigo_abr_8_12/ecuacion.py
@@ -6,7 +6,10 @@ import math
 # inicializar múltiples variables:
 a,b,c = 1,5,4
 
-x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
-
-print(x1, x2)
+raiz = b**2 - 4*a*c
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2*a)
+    x2 = (-b - math.sqrt(raiz)) / (2*a)
+    print(x1, x2)
+else:
+    print('No hay solución')
