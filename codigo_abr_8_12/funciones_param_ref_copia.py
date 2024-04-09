72fbbdd1a403150453f1abb6e97e122f17ff94e2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 13:33:46 +0200

    13.33

---
+++ codigo_abr_8_12/funciones_param_ref_copia.py
@@ -0,0 +1,40 @@
+"""
+Pasar parámetros mutables e inmutables a funciones
+"""
+
+
+def copia(n, t, c):
+    n *= 2
+    t += (8, 9, 0)
+    c += "hola"
+
+
+def referencias(L: list = list(), c: set = set(), d: dict = dict()) -> None:
+    L.append(1000)
+    c.add(1000)
+    d["a"] = 1000
+
+
+if __name__ == "__main__":
+    referencias()
+
+    lista = [1, 2, 3, 4, 5]
+    conjunto = {1, 2, 3, 4, 5}
+    diccionario = {"a": 1, "b": 2, "c": 3, "d": 4}
+
+    referencias(lista, conjunto, diccionario)
+    print(lista)
+    print(conjunto)
+    print(diccionario)
+
+    numero = 100
+    tupla = 1, 2, 3
+    cad = "texto"
+    copia(numero, tupla, cad)
+
+    print(numero)
+    print(tupla)
+    print(cad)
+
+    d = {(1, 2): 33, (3, 4): 34}
+    print(d)
