31b82b8578c404c855c8d8f61071cd410081401c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 09:43:47 +0200

    9.43

--- codigo_abr_8_12/filtro_ficheros.py
+++ codigo_abr_8_12/filtro_ficheros.py
@@ -5,3 +5,9 @@ extensiones. Por ej: docx, xlsx...
 
 import os
 
+extensiones = ('py','pdf')
+L = os.listdir()
+for f  in L:
+    ext = f.partition('.')[2]
+    if ext in extensiones:
+        print(f)
