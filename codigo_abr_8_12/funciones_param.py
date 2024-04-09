6c8c67b995333f1c0874b79de4759052c65ce525
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 12:15:43 +0200

    12.15

--- codigo_abr_8_12/funciones_param.py
+++ codigo_abr_8_12/funciones_param.py
@@ -5,7 +5,7 @@ Tipos de parámetros en una función de Python
 - tupla: *args
 - diccionario: **kwargs
 """
-
+import matplotlib.pyplot as plt
 
 def funcion(ob, op=100, *args, **kwargs):
     print("Obligatorio: ", ob)
@@ -16,3 +16,18 @@ def funcion(ob, op=100, *args, **kwargs):
 
 funcion(1)
 funcion(1,2,3,4,5)
+d = {"x":10, "y":20}
+funcion(1,2, d)
+funcion(1,2,**d)
+funcion(d)
+
+# Rellenar parámetros opcionales de print
+L = list(range(10))
+for i in L:
+    print(i, end=' ')
+
+y = [12,44,3,11,67]
+x = list(range(5))
+
+plt.plot(x, y, c="orange")
+plt.show()
