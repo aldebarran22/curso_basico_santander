a5f55b97fa1919f7bcba949e7d051f8eed1341e2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 12:37:21 +0200

    12.37

---
+++ codigo_abr_8_12/funciones_curso.py
@@ -0,0 +1,19 @@
+"""
+Definición de funciones
+"""
+
+
+def ecuacion():
+    pass
+
+
+def csvTojson():
+    pass
+
+
+def jsonTocsv():
+    pass
+
+
+if __name__ == "__main__":
+    pass
--- codigo_abr_8_12/funciones_param.py
+++ codigo_abr_8_12/funciones_param.py
@@ -9,8 +9,9 @@ Tipos de parámetros en una función de Python
 import matplotlib.pyplot as plt
 
 
-def sumaResta(a,b):
-    return a+b, a-b
+def sumaResta(a, b):
+    return a + b, a - b
+
 
 def funcion(ob, op=100, *args, **kwargs):
     print("Obligatorio: ", ob)
@@ -20,24 +21,30 @@ def funcion(ob, op=100, *args, **kwargs):
     print("-" * 20)
 
 
-funcion(1)
-funcion(1, 2, 3, 4, 5)
-d = {"x": 10, "y": 20}
-funcion(1, 2, d)
-funcion(1, 2, **d)
-funcion(d)
-funcion(1, x=100, y=200, op=999)
+if __name__ == "__main__":
+    # Estamos en ejecución
+    funcion(1)
+    funcion(1, 2, 3, 4, 5)
+    d = {"x": 10, "y": 20}
+    funcion(1, 2, d)
+    funcion(1, 2, **d)
+    funcion(d)
+    funcion(1, x=100, y=200, op=999)
 
-# Rellenar parámetros opcionales de print
-L = list(range(10))
-for i in L:
-    print(i, end=" ")
-print()
+    # Rellenar parámetros opcionales de print
+    L = list(range(10))
+    for i in L:
+        print(i, end=" ")
+    print()
 
-s, r = sumaResta(1,2)
-print(s, r)
+    s, r = sumaResta(1, 2)
+    print(s, r)
 
-y = [12, 44, 3, 11, 67]
-x = list(range(5))
-plt.plot(x, y, lw=3.5)
-plt.show()
+    print("__name__", __name__)
+
+    """
+    y = [12, 44, 3, 11, 67]
+    x = list(range(5))
+    plt.plot(x, y, lw=3.5)
+    plt.show()
+    """
