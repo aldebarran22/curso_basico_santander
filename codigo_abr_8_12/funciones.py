03005e11da39b0ac7daf1487e8528a139ba8f2d6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 11:39:19 +0200

    11.39

--- codigo_abr_8_12/funciones.py
+++ codigo_abr_8_12/funciones.py
@@ -9,20 +9,45 @@ Llamar a una función:
 Tipos anotados.
 """
 
-def segundos(h=0, m=0, s=0):
-    return h*3600 + m*60 + s
 
-def sumar(a,b):
-    return a+b
+def segundos(h=0, m=0, s=0):
+    return h * 3600 + m * 60 + s
+
+
+def sumar(a, b):
+    return a + b
+
 
 def ejemplo():
     """Documentación de la función ejemplo"""
     pass
 
+
 ejemplo()
 
 # 1) posicional
-print(sumar(1,2))
+print(sumar(1, 2))
 # 2) nominal
 print(sumar(b=2, a=1))
 print(segundos(m=40))
+# 12:00:10
+print(segundos(12, s=10))
+# 00:30:45
+# print(segundos(m=30, 45)) Error, no podemos utilizar forma posicional
+# despues de un parámetro nominal
+print(segundos(m=30, s=45))
+
+# 3) Con una tupla
+t = (1, 2)
+print(sumar(*t))
+
+L = [1, 2]
+print(sumar(*L))
+
+# 4) Con un diccionario:
+d = {"a": 1, "b": 2}
+print(sumar(**d))
+
+# Convertir las horas de la lista L en segundos y almacenar
+# el resultado en otra lista:
+L = [(12, 3, 45), (7, 0, 1), (11, 6, 7), (9, 56, 12)]
