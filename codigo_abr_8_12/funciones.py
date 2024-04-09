c1342f43dd25d5b6f25cef5411e0e812362e508a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 11:00:11 +0200

    11.00

--- codigo_abr_8_12/funciones.py
+++ codigo_abr_8_12/funciones.py
@@ -1,6 +1,15 @@
 """
-Funciones en Python
+Funciones en Python:
+Llamar a una función:
+- 1) Forma posicional
+- 2) Forma nominal
+- 3) A partir de una tupla / lista
+- 4) A partir de un diccionario
+
+Tipos anotados.
 """
+def sumar(a,b):
+    return a+b
 
 def ejemplo():
     """Documentación de la función ejemplo"""
