5e4e9b02cc99723f2f4092d55ec74baf9d3ee28d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 10:37:35 +0200

    10.37

--- codigo_abr_8_12/cajero.py
+++ codigo_abr_8_12/cajero.py
@@ -3,6 +3,7 @@ Simular un cajero:
 - Pedir un importe y tiene que ser múltiplo de 10
 - Desglosar el importe en billetes de 50, 20 y 10
 - importe: 230 -> 4 de 50, 1 de 20, 1 de 10.
+- importe: 200 -> 4 de 50
 """
 
 while True:
@@ -13,11 +14,14 @@ while True:
         if importe % 10 == 0:
             break
         else:
-            print('Solo se admiten múltiplos de 10')
+            print("Solo se admiten múltiplos de 10")
     else:
         print("Solo se admiten importes enteros")
 
-print('El importe a retirar es: ', importe)
+print("El importe a retirar es: ", importe)
+desglose = dict()
+billetes = [50, 20, 10]
+
 
 """
 Bucle infinito
