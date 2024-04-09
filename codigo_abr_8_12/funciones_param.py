5ed015c65f7cbace0c37e50fd726c963ba86ba51
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 12:23:12 +0200

    12.23

--- codigo_abr_8_12/funciones_param.py
+++ codigo_abr_8_12/funciones_param.py
@@ -5,7 +5,12 @@ Tipos de parámetros en una función de Python
 - tupla: *args
 - diccionario: **kwargs
 """
+
 import matplotlib.pyplot as plt
+
+
+def sumaResta(a,b):
+    return a+b, a-b
 
 def funcion(ob, op=100, *args, **kwargs):
     print("Obligatorio: ", ob)
@@ -14,20 +19,25 @@ def funcion(ob, op=100, *args, **kwargs):
     print("kwargs: ", kwargs)
     print("-" * 20)
 
+
 funcion(1)
-funcion(1,2,3,4,5)
-d = {"x":10, "y":20}
-funcion(1,2, d)
-funcion(1,2,**d)
+funcion(1, 2, 3, 4, 5)
+d = {"x": 10, "y": 20}
+funcion(1, 2, d)
+funcion(1, 2, **d)
 funcion(d)
+funcion(1, x=100, y=200, op=999)
 
 # Rellenar parámetros opcionales de print
 L = list(range(10))
 for i in L:
-    print(i, end=' ')
+    print(i, end=" ")
+print()
 
-y = [12,44,3,11,67]
+s, r = sumaResta(1,2)
+print(s, r)
+
+y = [12, 44, 3, 11, 67]
 x = list(range(5))
-
-plt.plot(x, y, c="orange")
+plt.plot(x, y, lw=3.5)
 plt.show()
