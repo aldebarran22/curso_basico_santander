d40e849b326fd0a35605f7bbc89d42660768a75a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-09 10:31:02 +0200

    10.30

--- codigo_abr_8_12/cajero.py
+++ codigo_abr_8_12/cajero.py
@@ -2,12 +2,19 @@
 Simular un cajero:
 - Pedir un importe y tiene que ser múltiplo de 10
 - Desglosar el importe en billetes de 50, 20 y 10
+- importe: 230 -> 4 de 50, 1 de 20, 1 de 10.
 """
 
 sImporte = input("Dame importe:> ")
 if sImporte.isnumeric():
     importe = int(sImporte)
+    # Comprobar si es múltiplo de 10
+    # if
 else:
-    print('Solo se admiten importes enteros')
+    print("Solo se admiten importes enteros")
 
-
+"""
+Bucle infinito
+while True:
+    pass
+"""
