82c1a8dada0b321e3a3e0cb4db7a546a124cea47
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-10 14:31:35 +0200

    14.31

--- codigo_abr_8_12/excepciones.py
+++ codigo_abr_8_12/excepciones.py
@@ -4,8 +4,23 @@ Control de excepciones en python
 
 
 def test1():
-    pass
+    try:
+        L = [1,2,3,4,5]
+        print(L[30])
+        print('fin')
+    except IndexError as e:
+        print(e.__class__.__name__, e)
+    
 
+def test2():
+    try:
+        L = [1,2,3,4,5]
+        d = {"a": 1, "b": 2, "c": 3}
+        print(d['a'])
+        print(L[30])
+        print('fin')
+    except (IndexError,KeyError) as e:
+        print(e.__class__.__name__, e)
 
 if __name__ == "__main__":
-    test1()
+    test2()
