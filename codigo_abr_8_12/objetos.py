d75543a9b61a849eb616b33b041d15b39d17b11c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-10 12:25:53 +0200

    12.25

--- codigo_abr_8_12/objetos.py
+++ codigo_abr_8_12/objetos.py
@@ -36,8 +36,12 @@ class Guia(Persona):
     def __init__(self, nombre="", edad=0, altura=0, ambito="", idiomas=[]):
         # Llamar al constructor de la clase padre:
         Persona.__init__(self, nombre, edad, altura)
+        #super().__init__(nombre, edad, altura)
         self.ambito = ambito
         self.idiomas = idiomas
+
+    def __str__(self):
+        return Persona.__str__(self)+ " "+self.ambito+ " "+",".join(self.idiomas)
 
 class Grupo:
 
