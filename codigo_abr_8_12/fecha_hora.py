06834c01653725461e8c9562d09805e6fcd92173
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-10 12:12:16 +0200

    12.12

--- codigo_abr_8_12/fecha_hora.py
+++ codigo_abr_8_12/fecha_hora.py
@@ -44,6 +44,9 @@ class Date:
 
 
 if __name__ == "__main__":
-    t1 = Time(12, 30)
-    t2 = Time(mm=59)
+    t1 = Time(12, 30, 59)
+    t2 = Time(mm=59, ss=10)
     suma = t1 + t2  # suma = t1.__add__(t2)
+    print(t1)
+    print(t2)
+    print(suma)
