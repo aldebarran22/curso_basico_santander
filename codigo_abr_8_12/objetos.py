4bda27a7d0e72537e1dd91aaeff78b288d88ed49
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-10 11:35:59 +0200

    11.35

--- codigo_abr_8_12/objetos.py
+++ codigo_abr_8_12/objetos.py
@@ -11,22 +11,34 @@ class Persona:
         self.edad = edad
         self.altura = altura
 
+    """
     def __str__(self):
         return self.nombre + " " + str(self.edad) + " " + str(self.altura)
 
     def __repr__(self):
         return str(self)
 
+    """
+
+    def __lt__(self, other):
+        return self.edad < other.edad
+
+    def __repr__(self):
+        return self.nombre + " " + str(self.edad) + " " + str(self.altura)
+
     def __del__(self):
-        print("eliminando: ", self.nombre)
+        pass
+        # print("eliminando: ", self.nombre)
 
 
 if __name__ == "__main__":
     p1 = Persona("Ana", 33, 1.8)
     print(p1)
     # del(p1) Fuerza una llamada al destructor: __del__
-    #print(str(p1))
-    #print(p1.__str__())
-    L = [p1, Persona("Juan",44,1.81), Persona("Sara",21,1.9)]
-    L.sort(key=lambda obj: obj.edad)
-    print(L) # repr
+    # print(str(p1))
+    # print(p1.__str__())
+    L = [p1, Persona("Juan", 44, 1.81), Persona("Sara", 21, 1.9)]
+    L.sort(key=lambda obj: obj.altura)
+    print(L)
+    L.sort()
+    print(L)  # repr
