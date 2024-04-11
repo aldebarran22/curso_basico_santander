16393162da7432b1ca60c97be8a9734ee1377fbf
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-11 12:53:29 +0200

    12.53

---
+++ codigo_abr_8_12/app2/principal.py
@@ -0,0 +1,17 @@
+
+from tiempo.fechahora import DateTime
+from tiempo.hora import Time
+from tiempo.fecha import Date
+
+
+if __name__ == "__main__":
+    dt = DateTime(10,4,2024, 12,52,9)
+    print(dt)
+    
+
+    t1 = Time(12, 30, 59)
+    t2 = Time(mm=59, ss=10)
+    suma = t1 + t2  # suma = t1.__add__(t2)
+    print(t1)
+    print(t2)
+    print(suma)
