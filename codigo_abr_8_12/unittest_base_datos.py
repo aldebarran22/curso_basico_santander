5dd0588123802b358914cbe625dd9d68c465003a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-12 14:35:33 +0200

    14.35

--- codigo_abr_8_12/unittest_base_datos.py
+++ codigo_abr_8_12/unittest_base_datos.py
@@ -3,12 +3,28 @@ Test de la base de datos
 """
 
 import unittest
-from base_datos_CRUD import ProductoCRUD
+from base_datos_CRUD import ProductoCRUD, Producto, Categoria
 
 class TestEcuacion(unittest.TestCase):
 
+    def setUp(self):
+        self.bd = ProductoCRUD('../bd/empresa3.db')
+        
+
+    def testRead(self):
+        obj = self.bd.read(1)
+        if obj.id != 1:
+            self.fail("hay un error en el id")
+
+    
     def testExcepcion(self):
         self.assertRaises(FileNotFoundError, ProductoCRUD, path="no existe.db")
+    
+
+    def tearDown(self):
+        del self.bd
+
+    
 
 if __name__=='__main__':
     unittest.main()
