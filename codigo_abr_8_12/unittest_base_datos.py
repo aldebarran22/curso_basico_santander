a10d522464b59ecc37d5437d78def96861cdf63c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-12 14:23:25 +0200

    14.23

---
+++ codigo_abr_8_12/unittest_base_datos.py
@@ -0,0 +1,14 @@
+"""
+Test de la base de datos
+"""
+
+import unittest
+from base_datos_CRUD import ProductoCRUD
+
+class TestEcuacion(unittest.TestCase):
+
+    def testExcepcion(self):
+        self.assertRaises()
+
+if __name__=='__main__':
+    unittest.main()
