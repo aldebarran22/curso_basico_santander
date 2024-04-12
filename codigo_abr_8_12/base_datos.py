1da60550926df89b698f95515b8e261dbade7012
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-12 09:28:47 +0200

    9.28

--- codigo_abr_8_12/base_datos.py
+++ codigo_abr_8_12/base_datos.py
@@ -4,6 +4,7 @@ Base de datos sqlite3
 
 import sqlite3 as db
 from os.path import isfile
+import sys
 
 def conexion(path):
     con = None
@@ -18,5 +19,26 @@ def conexion(path):
     finally:
         if con: con.close()
 
+
+def printTabla(path, tabla, file=sys.stdout):
+    con = None
+    cur = None
+    try:
+        if not isfile(path):
+            raise FileNotFoundError(f"No se encuentra el fichero: {path}")
+        else:
+            con = db.connect(path)
+            cur = con.cursor()
+            sql = f"select * from {tabla}"
+
+            cur.execute(sql)
+            print(cur.description)
+
+    except Exception as e:
+        print(e)
+    finally:
+        if cur: cur.close()
+        if con: con.close()
+
 if __name__=='__main__':
-    conexion("../bd/empresa33.db")
+    printTabla("../bd/empresa3.db","pedidos")
