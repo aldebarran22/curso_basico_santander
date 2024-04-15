7a3aba0495c3ca6659c6571c5476a25deaf2769f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 10:54:11 +0200

    10.54

--- codigo_abr_15_19/ecuacion.py
+++ codigo_abr_15_19/ecuacion.py
@@ -8,6 +8,10 @@ import math
 # Inicialización múltiples:
 a,b,c = 1,5,4
 
-x1 = -b + math.sqrt()
-x2 = -b - math.sqrt()
+x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+print("x1:",x1,"x2:",x2)
+
+#f-string
+print(f"x1: {x1}, x2: {x2}")
 
