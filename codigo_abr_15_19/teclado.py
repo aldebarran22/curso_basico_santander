632b5c6376196cd876fed254352c95d64a793611
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 12:10:33 +0200

    12.10

---
+++ codigo_abr_15_19/teclado.py
@@ -0,0 +1,12 @@
+"""
+Leer variables de teclado y cambios de tipo
+Funciones:
+int() : Convertir texto a entero int
+float() : Convertir texto a float
+str() : Convertir a texto
+"""
+
+# Leer dos variables y sumarlas:
+n1 = input("Dame número 1: ")
+n2 = input("Dame número 2: ")
+print(n1 + n2)
