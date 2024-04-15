27eeefc3c6bb26df56aa518bccc9622a448fdb7c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 11:39:29 +0200

    11.39

---
+++ codigo_abr_15_19/condiciones.py
@@ -0,0 +1,13 @@
+"""
+Operadores relacionales y lógicos
+"""
+
+# Comprobar si una variable se encuentra dentro de un intervalo
+ini = 10
+fin = 40
+num = 14
+
+if num >= ini and num <= fin:
+    print(f"El número {num} está dentro del intervalo")
+else:
+    print('Fuera del intervalo')
