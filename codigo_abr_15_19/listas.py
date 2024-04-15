0d51ebdabac0e420371ef7657d06abed68f8919e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 13:05:53 +0200

    1.3.05

---
+++ codigo_abr_15_19/listas.py
@@ -0,0 +1,13 @@
+"""
+Crear listas
+Funciones sobre listas
+Recorrer listas con bucles: for
+Función list
+Acceso a los elementos
+modificar elementos
+Slicing
+copiar listas
+"""
+
+L = [45, 12, 34, 77, 9]
+print(L, type(L))
