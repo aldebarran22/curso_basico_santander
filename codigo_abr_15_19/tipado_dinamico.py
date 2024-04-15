41e59a09898275c018a77150d62b394daefc93cc
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 10:19:56 +0200

    10.19

---
+++ codigo_abr_15_19/tipado_dinamico.py
@@ -0,0 +1,19 @@
+"""
+Comprobar como funciona el tipado en Python
+"""
+
+n = 100
+print(n, type(n))
+
+n = "hola"
+print(n, type(n))
+
+n1 = 120
+n2 = 300
+print(n1+n2)
+
+cad1 = "120"
+cad2 = "300"
+print(cad1+cad2)
+
+print(n1+cad1)
