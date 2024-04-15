ed131775fbca4b2a48d20f3f789ae4c21c047a99
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 12:50:49 +0200

    12.50

---
+++ codigo_abr_15_19/control_de_flujo.py
@@ -0,0 +1,14 @@
+"""
+if compacto -> A if cond else B
+range
+while
+"""
+
+# Imprimir si un número es par o impar:
+numero = 22
+if numero % 2  == 0:
+    print('par')
+else:
+    print('impar')
+
+print("par" if numero % 2 == 0 else "impar")
