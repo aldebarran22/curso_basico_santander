88c2600c00084d5448e4ce567fde23b306b06851
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 12:03:49 +0200

    12.03

--- codigo_abr_15_19/condiciones.py
+++ codigo_abr_15_19/condiciones.py
@@ -33,9 +33,18 @@ nombre2 = 'Sara'
 if nombre1 < nombre2:
     print(nombre1, type(nombre1))
 
+    if len(nombre1) < len(nombre2):
+        print(nombre1,'es mas corto')
+    else:
+        #  Pass no hace nada, rellena la instrucción de bloque con una inst. vacía
+        pass 
+        
+
 elif nombre1 == nombre2:
     print('Es el mismo nombre: ', nombre1)
-    
+
 else:
     print(nombre2, type(nombre2))
+
+print('fin de programa')
 
