047640265e5b195b3e14ae0eeeae91d21a661e0c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-15 14:32:42 +0200

    14.32

--- codigo_abr_15_19/diccionarios.py
+++ codigo_abr_15_19/diccionarios.py
@@ -10,8 +10,17 @@ d = {"a":1, "b":2, "c":3}
 print(d, type(d))
 
 L = [1,2,3]
-cad = "ABC"
+cad = "ABCDE"
 d2 = dict(zip(cad, L))
 print(d2)
 d3 = dict(zip(L, cad))
 print(d3)
+
+cabs = "id;nombre;cargo"
+val = "1;Davolio;Representante de ventas"
+Lcabs = cabs.split(";")
+Lval = val.split(";")
+print(Lcabs)
+print(Lval)
+emp = dict(zip(Lcabs, Lval))
+print(emp)
