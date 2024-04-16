419e473080f8737e5f4b210375f9ed5863a02e9c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-16 11:37:51 +0200

    11.37

--- codigo_abr_15_19/mutable_vs_inmutable.py
+++ codigo_abr_15_19/mutable_vs_inmutable.py
@@ -3,12 +3,12 @@ Objetos mutables: list, set, dict
 Inmutables: tuple, str, int, float, bool
 """
 
-L = [1,3,4,1,-8, 9,-34]
+L = [1, 3, 4, 1, -8, 9, -34]
 L.sort()
 print(L)
 
 cad = "hola"
-print(cad.upper()) # Devuelve una copia de la cadena
+print(cad.upper())  # Devuelve una copia de la cadena
 print(cad)
 
 # Se suele hacer:
@@ -18,8 +18,8 @@ print(cad)
 # Cambiar el ; por la , en una cadena (objeto inmutable)
 csv = "10248;WILMK;5;3;32,38;Finlandia"
 print(csv)
-csv = csv.replace(',','.') # Convertir el float a formato inglés
+csv = csv.replace(",", ".")  # Convertir el float a formato inglés
 print(csv)
-csv = csv.replace(";",",")
-L = csv.replace(';', ',').split(',')
+csv = csv.replace(";", ",")
+L = csv.split(",")
 print(L)
