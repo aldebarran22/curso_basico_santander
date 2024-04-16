3d932c56590f389e5e41ed8e2937d177b7dc3588
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-16 12:37:50 +0200

    12.37

--- codigo_abr_15_19/funciones.py
+++ codigo_abr_15_19/funciones.py
@@ -1,5 +1,9 @@
 """
 Implementación de funciones en Python
+
+Formas de llamar a una función:
+1) posicional: la posición que ocupan de izq a der
+2) nominal: con los nombres de los parámetros
 """
 
 
@@ -7,6 +11,11 @@ def sumar(a, b):
     return a + b
 
 
+def sumar3(a, b, c):
+    return a + b + c
+
+
+# Con tipos anotados: funcionan a título informativo.
 def concatenar(a: str, b: str) -> str:
     return a + b
 
@@ -15,4 +24,7 @@ print("Resul: ", sumar(12, 34))
 print("Resul: ", sumar("12", "34"))
 print("Resul: ", sumar(12.4, 34.7))
 print("Resul: ", sumar([12, 12], [34, 34]))
-print('Concatenar: ', concatenar(34,77))
+print("Concatenar: ", concatenar(34, 77))
+
+# Nominal:
+print("Resul: ", sumar(b=34, a=12))
