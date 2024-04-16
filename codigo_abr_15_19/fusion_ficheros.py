5d7f9185116f25b1070f83ee7d73381fe9e5f7ea
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-16 10:02:34 +0200

    10.02

---
+++ codigo_abr_15_19/fusion_ficheros.py
@@ -0,0 +1,35 @@
+"""
+Fusionar dos ficheros de empleados en uno.
+Quitando las filas repetidas
+"""
+
+emp2 = """id;nombre;cargo
+1;Davolio;Representante de ventas
+2;Fuller;Vicepresidente comercial
+3;Leverling;Representante de ventas
+4;Peacock;Representante de ventas
+6;Suyama;Representante de ventas
+7;King;Representante de ventas
+9;Dodsworth;Representante de ventas"""
+
+
+emp3 = """id;nombre;cargo
+2;Fuller;Vicepresidente comercial
+3;Leverling;Representante de ventas
+4;Peacock;Gerente de ventas
+5;Buchanan;Gerente de ventas
+6;Suyama;Representante de ventas
+7;King;Representante de ventas
+8;Callahan;Coordinador ventas interno
+9;Dodsworth;Representante de ventas
+10;George;Representante de ventas"""
+
+# Partir el contenido de emp2 en filas:
+
+# Partir el contenido de emp3 en filas:
+
+# Convertir las dos listas en conjuntos
+
+# Unión de conjuntos:
+
+# Imprimir el resultado:
