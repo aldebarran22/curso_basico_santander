741e0e479eeef6346cc53aa058d1e6d2527a812d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-16 14:12:33 +0200

    14.12

--- codigo_abr_15_19/funcion_ecuacion.py
+++ codigo_abr_15_19/funcion_ecuacion.py
@@ -7,6 +7,7 @@ import math
 
 
 def ecuacion(a, b, c):
+    """Calcular la ecu. de 2 grado"""
     raiz = b**2 - 4 * a * c
     if raiz > 0:
         x1 = (-b + math.sqrt(raiz)) / (2 * a)
@@ -15,16 +16,18 @@ def ecuacion(a, b, c):
     else:
         return None
 
+if __name__=='__main__':
+    #print("__name__:", __name__)
 
-# Inicialización múltiples:
-num1, num2, num3 = 1, 5, 4
+    # Inicialización múltiples:
+    num1, num2, num3 = 1, 5, 4
 
-# llamada a la función:
-resul = ecuacion(num1, num2, num3)
-if resul:
-    x1, x2 = resul
-    print("Sol1: ", x1)
-    print("Sol2: ", x2)
-    # print("Solución: ", resul)
-else:
-    print("No hay Solución")
+    # llamada a la función:
+    resul = ecuacion(num1, num2, num3)
+    if resul:
+        x1, x2 = resul
+        print("Sol1: ", x1)
+        print("Sol2: ", x2)
+        # print("Solución: ", resul)
+    else:
+        print("No hay Solución")
--- codigo_abr_15_19/funciones2.py
+++ codigo_abr_15_19/funciones2.py
@@ -15,4 +15,15 @@ def funcion(ob, op=0, *args, **kwargs):
     print("kwargs:", kwargs)
     print("-" * 30)
 
-funcion()
+
+funcion(1)
+funcion(1, 2, 3, 4, 5, 6, 7, 8, 9)
+funcion(1, 2, 3, 4, 5, 6, 7, 8, 9, x=10, y=20)
+
+################################################################
+import matplotlib.pyplot as plt
+
+x = list(range(1, 6))
+y = [12, 45, 6, 10, 30]
+plt.plot(x, y, color="orange", lw=5.0)
+plt.show()
