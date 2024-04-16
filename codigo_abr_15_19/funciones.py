b56fa4235ebd29216e383150587f3b286d751364
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-16 12:27:31 +0200

    12.27

--- codigo_abr_15_19/funciones.py
+++ codigo_abr_15_19/funciones.py
@@ -2,10 +2,17 @@
 Implementación de funciones en Python
 """
 
-def sumar(a, b):
-    return a+b
 
-print('Resul: ', sumar(12,34))
-print('Resul: ', sumar("12","34"))
-print('Resul: ', sumar(12.4,34.7))
-print('Resul: ', sumar([12,12],[34,34]))
+def sumar(a, b):
+    return a + b
+
+
+def concatenar(a: str, b: str) -> str:
+    return a + b
+
+
+print("Resul: ", sumar(12, 34))
+print("Resul: ", sumar("12", "34"))
+print("Resul: ", sumar(12.4, 34.7))
+print("Resul: ", sumar([12, 12], [34, 34]))
+print('Concatenar: ', concatenar(34,77))
