0e5a1239b5b9b230f1efdc642f975a030637d0bd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-16 13:01:45 +0200

    13.01

--- codigo_abr_15_19/funcion_ecuacion.py
+++ codigo_abr_15_19/funcion_ecuacion.py
@@ -6,19 +6,25 @@ ax^2 + bx + c = 0
 import math
 
 
-def ecuacion(a,b,c):
-    raiz = b**2 - 4*a*c
+def ecuacion(a, b, c):
+    raiz = b**2 - 4 * a * c
     if raiz > 0:
-        x1 = (-b + math.sqrt(raiz)) / (2*a)
-        x2 = (-b - math.sqrt(raiz)) / (2*a)
+        x1 = (-b + math.sqrt(raiz)) / (2 * a)
+        x2 = (-b - math.sqrt(raiz)) / (2 * a)
         return x1, x2
     else:
         return None
 
 
 # Inicialización múltiples:
-num1, num2, num3 = 1,5,4
+num1, num2, num3 = 1, 5, 4
 
 # llamada a la función:
-ecuacion(num1, num2, num3)
-
+resul = ecuacion(num1, num2, num3)
+if resul:
+    x1, x2 = resul
+    print("Sol1: ", x1)
+    print("Sol2: ", x2)
+    # print("Solución: ", resul)
+else:
+    print("No hay Solución")
