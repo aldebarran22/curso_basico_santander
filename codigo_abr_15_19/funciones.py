1950daeb91cfc73a27185afbbdb1bc14020980ef
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-16 12:25:46 +0200

    12.25

---
+++ codigo_abr_15_19/funciones.py
@@ -0,0 +1,11 @@
+"""
+Implementación de funciones en Python
+"""
+
+def sumar(a, b):
+    return a+b
+
+print('Resul: ', sumar(12,34))
+print('Resul: ', sumar("12","34"))
+print('Resul: ', sumar(12.4,34.7))
+print('Resul: ', sumar([12,12],[34,34]))
