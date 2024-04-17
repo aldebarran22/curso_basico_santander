5281d974420bdddf84cf145a68a736dda0dd2eca
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-17 14:42:29 +0200

    14.42

--- codigo_abr_15_19/fecha_hora.py
+++ codigo_abr_15_19/fecha_hora.py
@@ -1,50 +1,48 @@
-
-
 class Time:
 
-    def __init__(self,hh=0,mm=0,ss=0):
-        self.hh = hh
-        self.mm = mm
-        self.ss = ss
+    def __init__(self, h=0, m=0, s=0):
+        self.h = h
+        self.m = m
+        self.s = s
 
-    def __add__(self,other):
-          tsuma = Time(self.hh+other.hh, self.mm+other.mm, self.ss+other.ss)
-          tsuma.ajustar()
-          return tsuma
-    
+    def __add__(self, other):
+        tsuma = Time(self.h + other.h, self.m + other.m, self.s + other.s)
+        tsuma.ajustar()
+        return tsuma
+
     def ajustar(self):
-        minutos = self.ss // 60
-        self.ss %= 60
-        self.mm += minutos
+        minutos = self.s // 60
+        self.s %= 60
+        self.m += minutos
 
-        horas = self.mm // 60
-        self.mm %= 60
-        self.hh += horas
+        horas = self.m // 60
+        self.m %= 60
+        self.h += horas
+
+    def __str__(self):
+        return "%02d:%02d:%02d" % (self.h, self.m, self.s)
 
 
-
-    def __str__(self):        
-        return '%02d:%02d:%02d' % (self.hh,self.mm,self.ss)
 class Date:
 
-    def __init__(self, dd,mm,yy):
+    def __init__(self, dd, mm, yy):
         self.dd = dd
         self.mm = mm
         self.yy = yy
 
     def __str__(self):
-        return "%02d/%02d/%04d" % (self.dd,self.mm,self.yy)
+        return "%02d/%02d/%04d" % (self.dd, self.mm, self.yy)
 
     def esBisiesto(self):
-        anyo = self.yy    
-        if  (anyo % 4 == 0 and anyo % 100 != 0) or (anyo%100 == 0 and anyo % 400 == 0):
-            return True    
+        anyo = self.yy
+        if (anyo % 4 == 0 and anyo % 100 != 0) or (anyo % 100 == 0 and anyo % 400 == 0):
+            return True
         else:
             return False
 
- 
-if __name__ == '__main__':
-    t1 = Time(12,4,6) # 12:04:06
+
+if __name__ == "__main__":
+    t1 = Time(12, 4, 6)  # 12:04:06
     print(t1)
 
     t2 = Time(5, 59, 59)
@@ -52,3 +50,9 @@ if __name__ == '__main__':
 
     suma = t1 + t2  #    20:04:05
     print(suma)
+
+    f = Date(17, 4, 2024)
+    print(f)
+
+    dt = DateTime(17, 4, 2024, 14, 37, 21)
+    print(dt)  # 17/04/2024 14:37:21
