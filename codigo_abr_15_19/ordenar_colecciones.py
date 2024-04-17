c8c7dffc3b5a51d7591d14e7d700be324e36b97c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-17 09:43:10 +0200

    9.43

--- codigo_abr_15_19/ordenar_colecciones.py
+++ codigo_abr_15_19/ordenar_colecciones.py
@@ -25,8 +25,8 @@ L = [(1, 2000, 3), (900,), (10, 20, 50), (0, 0, 0, 1)]
 L.sort(key=sum)
 print(L)
 
-L = [("Raúl",55,1.9),("Ana",34,1.7), ("Roberto",23,1.86)]
-L.sort(key=lambda t : t[1]) # ASC por Edad
+L = [("Raúl", 55, 1.9), ("Ana", 34, 1.7), ("Roberto", 23, 1.86)]
+L.sort(key=lambda t: t[1])  # ASC por Edad
 print(L)
-L.sort(key=lambda t : t[2], reverse=True)
+L.sort(key=lambda t: t[2], reverse=True)  # DESC por Altura
 print(L)
