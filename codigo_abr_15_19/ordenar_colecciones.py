26482e88174c5d3fe9d57acbd87611cb9eb0a94c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-17 09:22:16 +0200

    9.22

---
+++ codigo_abr_15_19/ordenar_colecciones.py
@@ -0,0 +1,12 @@
+"""
+Ordenar listas con el método: sort
+Función sorted para ordenar otras colecciones.
+"""
+
+L = [4,5,6,71,2,3]
+L.sort()
+print(L)
+
+comida = {"Ana", "Raúl", "Roberto", "Sara", "Olga"}
+L2 = sorted(comida)
+print(comida)
