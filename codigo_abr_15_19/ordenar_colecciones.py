64e04920e62b256342e730713692dd4f007cc1be
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-17 09:28:03 +0200

    9.27

--- codigo_abr_15_19/ordenar_colecciones.py
+++ codigo_abr_15_19/ordenar_colecciones.py
@@ -7,6 +7,15 @@ L = [4,5,6,71,2,3]
 L.sort()
 print(L)
 
-comida = {"Ana", "Raúl", "Roberto", "Sara", "Olga"}
+comida = {"Ana", "Raúl", "Roberto", "Jose Antonio" "Sara", "Olga"}
 L2 = sorted(comida)
+print(L2)
 print(comida)
+
+# DESC:
+L2 = sorted(comida, reverse=True)
+print(L2)
+
+# Ordenar por la longitud del nombre:
+L2 = sorted(comida, key=len)
+print(L2)
