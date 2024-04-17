a122ebc57a2e73c4bb277e3ded3017ce1672c49c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-17 12:12:33 +0200

    12.12

--- codigo_abr_15_19/objetos.py
+++ codigo_abr_15_19/objetos.py
@@ -11,7 +11,10 @@ class Persona:
         self.altura = altura
 
     def __str__(self):
-        return self.nombre + " " + str(self.edad) + " " + str(self.altura)    
+        return self.nombre + " " + str(self.edad) + " " + str(self.altura)
+
+    def __repr__(self):
+        return str(self)
 
     def __del__(self):
         print("Eliminando a: ", self.nombre)
@@ -22,6 +25,8 @@ if __name__ == "__main__":
     p2 = Persona("Pedro", 33)
     p3 = Persona("Juan", altura=1.8)
 
-    print(p1) # str(p1) p1.__str__()
-    print(str(p1))
-    print(p1.__str__())
+    L = [p1, p2, p3, Persona("Sonia", 23, 1.77)]
+    print(L)
+    print(p1)
+    # print(str(p1))
+    # print(p1.__str__())
