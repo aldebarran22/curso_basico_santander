5a63a6a5f60aa969f0ecda899b9aba8cdd6d9007
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-18 09:21:04 +0200

    9.20

--- codigo_abr_15_19/excepciones.py
+++ codigo_abr_15_19/excepciones.py
@@ -7,17 +7,34 @@ try, except, finally, raise
 def prueba1():
     # Capturar una excepción
     try:
-        L = [1,4,5,6,7]
-        print(L[10])
+        L = [1, 4, 5, 6, 7]
+        print(L[11])  # Falla y salta al except
+        n = int("12f")  # Esta inst. no llega a ejecutarse!
         for i in L:
             print(i, end=" ")
         print()
 
     except IndexError as e:
-        print("ERROR: ", e)
-        
+        print("ERROR: ", e.__class__.__name__, e)
 
+
+def prueba2():
+    # Capturar dos posibles excepciones
+    try:
+        L = [1, 4, 5, 6, 7]
+        print(L[11])  # Falla y salta al except
+        n = int("12f")  # Esta inst. no llega a ejecutarse!
+        for i in L:
+            print(i, end=" ")
+        print()
+
+    except IndexError as e:
+        print("ERROR: ", e.__class__.__name__, e)
+
+    except ValueError as e:
+        print("ERROR: ", e.__class__.__name__, e)
 
 
 if __name__ == "__main__":
-    prueba1()
+    # prueba1()
+    prueba2()
