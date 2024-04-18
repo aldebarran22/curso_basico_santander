bfe7de61e7b22e47ac5a93057452d9a4526ea0aa
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-18 12:00:58 +0200

    12.00

--- codigo_abr_15_19/ficheros.py
+++ codigo_abr_15_19/ficheros.py
@@ -67,6 +67,28 @@ def grabarFicheroDePrueba():
     fich.close()
 
 
+def grabarFichero(path):
+    L = [
+        {"producto": "portatil", "importe": 450.9},
+        {"producto": "hp", "importe": 1450.69},
+        {"producto": "teclado", "importe": 45.0},
+    ]
+    fich = None
+    cabs = L[0].keys()
+    try:
+        fich = open(path, "w")
+        print("%-12s\t%10s" % tuple(cabs), file=fich)
+        for d in L:
+            print("%-12s\t%10.2f" % tuple(d.values()), file=fich)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if fich:
+            fich.close()
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) == 4:
@@ -80,3 +102,4 @@ if __name__ == "__main__":
         )
 
     # grabarFicheroDePrueba()
+    grabarFichero("../ficheros/productos.txt")
