45318a91ea5c98b097c85f78dc41917a71bf6529
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-18 14:24:44 +0200

    14.24

--- codigo_abr_15_19/expresiones_regulares.py
+++ codigo_abr_15_19/expresiones_regulares.py
@@ -4,9 +4,15 @@ Expresiones regulares en Python
 
 import re
 
+
 def validar(patron, L):
-    print('Patrón: ', patron)
+    print("Patrón: ", patron)
     for cad in L:
         print(cad, "True" if re.match(patron, cad) else "False")
-    print('-'*30)
+    print("-" * 30)
 
+
+# Validar DNI:
+patron = ""
+L = ["12345678", "12345678A", "12345678AA", "12B", "1A", ""]
+validar(patron, L)
