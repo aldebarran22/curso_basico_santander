30c12982d5de5b83c23a5e26d488cb53a3e4e24f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-18 09:28:18 +0200

    9.28

--- codigo_abr_15_19/excepciones.py
+++ codigo_abr_15_19/excepciones.py
@@ -22,8 +22,8 @@ def prueba2():
     # Capturar dos posibles excepciones
     try:
         L = [1, 4, 5, 6, 7]
-        print(L[11])  # Falla y salta al except
-        n = int("12f")  # Esta inst. no llega a ejecutarse!
+        print(L[1])
+        n = int("12f")
         for i in L:
             print(i, end=" ")
         print()
@@ -35,6 +35,27 @@ def prueba2():
         print("ERROR: ", e.__class__.__name__, e)
 
 
+def prueba3():
+    # Capturar dos posibles excepciones agrupar en el
+    # mismo tratamiento (except)
+    # Y después capturar la excepción genérica.
+    try:
+        L = [1, 4, 5, 6, 7]
+        print(L[1])
+        n = int("12")
+        print(n / 0)
+        for i in L:
+            print(i, end=" ")
+        print()
+
+    except (IndexError, ValueError) as e:
+        print("ERROR: ", e.__class__.__name__, e)
+
+    except Exception as e:
+        print("ERROR: ", e.__class__.__name__, e)
+
+
 if __name__ == "__main__":
     # prueba1()
-    prueba2()
+    # prueba2()
+    prueba3()
