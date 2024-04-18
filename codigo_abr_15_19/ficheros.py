0c84b3d25bec7231b30954fc0a7f04d5d5520ad3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-18 10:24:47 +0200

    10.24

--- codigo_abr_15_19/ficheros.py
+++ codigo_abr_15_19/ficheros.py
@@ -4,6 +4,7 @@ Gestión de ficheros en Python
 
 
 def leerFicheroEntero(path):
+    """Devuelve todo el contenido de un fichero"""
     fich = None
     try:
         fich = open(path, "r")
@@ -17,7 +18,38 @@ def leerFicheroEntero(path):
         if fich != None:
             fich.close()
 
+def criterio(linea):
+    col = linea.partition(";")[0]
+    if col.isnumeric():
+        return int(col)
+    else:
+        return 0
+
+def fusionFicheros(path1, path2):
+    emp2 = leerFicheroEntero(path1)
+    emp3 = leerFicheroEntero(path2)
+    # Partir el contenido de emp2 en filas:
+    Lemp2 = emp2.split("\n")
+
+    # Partir el contenido de emp3 en filas:
+    Lemp3 = emp3.split("\n")
+    print(Lemp3[:3])
+
+    # Convertir las dos listas en conjuntos
+    cemp2 = set(Lemp2)
+    cemp3 = set(Lemp3)
+
+    # Unión de conjuntos:
+    empleados = cemp2 | cemp3
+
+    # Ordenar:
+    Lemp = sorted(empleados, key=criterio)
+
+    # Imprimir el resultado:
+    for e in Lemp:
+        print(e)
 
 if __name__ == "__main__":
-    txt1 = leerFicheroEntero("../ficheros_curso/Empleados2.txt")
-    print(txt1)
+    fusionFicheros(
+        "../ficheros_curso/Empleados2.txt", "../ficheros_curso/Empleados3.txt"
+    )
