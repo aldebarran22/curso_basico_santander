9bcd74477dc55862f852ec29c65ca52a812f4553
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-19 14:20:23 +0200

    14.20

--- codigo_abr_15_19/servidor_file.py
+++ codigo_abr_15_19/servidor_file.py
@@ -4,15 +4,18 @@ Implementación de un servidor TCP
 
 import socket as s
 import sys
-
+from datetime import datetime
 
 if len(sys.argv) == 2:
     puerto = int(sys.argv[1])
     print("Puerto: ", puerto)
 
     s_server = s_client = None
+    fich = None
     try:
-
+        t = datetime.now()
+        cad = t.strftime("%Y_%m_%d_%H_%M_%S")
+        fich = open(f"../ficheros/pedidos_{cad}.csv", "w")
         # Crear un socket TCP / AF_INET
         s_server = s.socket()
         s_server.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
@@ -36,7 +39,7 @@ if len(sys.argv) == 2:
             if mensaje.lower() == "fin":
                 break
 
-            print(mensaje)
+            print(mensaje, file=fich)
             s_client.send(mensaje.upper().encode("utf-8"))
 
         print("Fin de comunicación")
@@ -52,6 +55,8 @@ if len(sys.argv) == 2:
             s_client.close()
         if s_server:
             s_server.close()
+        if fich:
+            fich.close()
 
 else:
     print("No se ha indicado el puerto para conectar ...")
