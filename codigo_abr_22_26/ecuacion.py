1ca7dfd34f5ec06dfb7d650f71efc36440309c04
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-22 10:58:20 +0200

    10.58

--- codigo_abr_22_26/ecuacion.py
+++ codigo_abr_22_26/ecuacion.py
@@ -5,7 +5,13 @@ ax^2 + bx + c = 0
 import math
 
 # Inicialización múltiple:
-a,b,c = 1,5,4
+#a,b,c = 1,5,4 # -1, -4
+a,b,c = 1,2,3
 
-x1 = ...
-x2 = ...
+raiz = b**2 - 4*a*c
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2*a)
+    x2 = (-b - math.sqrt(raiz)) / (2*a)
+    print(f"x1: {x1} x2: {x2}")
+else:
+    print('No hay solución')
