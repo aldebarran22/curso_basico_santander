b68bd15586f4e4208d9102f98756eae20dfd0fab
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-22 11:53:26 +0200

    11.53

--- codigo_abr_22_26/condiciones.py
+++ codigo_abr_22_26/condiciones.py
@@ -21,7 +21,7 @@ if num < inf or num > sup:
     print("Fuera del intervalo")
 else:
     print("En el intervalo")
-    
+
 if not (num >= inf and num <= sup):
     print("Fuera del intervalo")
 else:
@@ -30,8 +30,20 @@ else:
 n1 = 10
 n2 = 20
 if n1 < n2:
-    print('el menor es: ', n1)
+    print("el menor es: ", n1)
+
+    if n1 % 2 == 0:
+        print("n1 es par")
+    print()
+
 elif n2 < n1:
-    print('el menor es: ', n2)
+    print("el menor es: ", n2)
 else:
-    print('son iguales')
+    print("son iguales")
+
+# if compacto --> A if cond else B
+# imprimir si un número es par o impar:
+print("par" if n1 % 2 == 0 else "impar")
+menor = n1 if n1 < n2 else n2
+sem = "ambar"
+print("pasar" if sem == "verde" else "parar" if sem == "rojo" else "precaución")
