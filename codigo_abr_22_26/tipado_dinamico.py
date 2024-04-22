0b3a406a316a255c820ca1c0e980255eabc60c84
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-22 10:20:45 +0200

    10.20

---
+++ codigo_abr_22_26/tipado_dinamico.py
@@ -0,0 +1,21 @@
+"""
+Tipado dinámico y leng. fuertemente tipado
+"""
+
+n = 100
+print(n, type(n))
+
+n = 23.5
+print(n, type(n))
+
+n = "hola"
+print(n, type(n))
+print(n * 5)
+
+n1 = 100
+n2 = "200"
+# print(n1 + n2) Error tipos incompatibles!
+
+resul = 23 + 2.55 + 5+8j
+print(resul, type(resul))
+
