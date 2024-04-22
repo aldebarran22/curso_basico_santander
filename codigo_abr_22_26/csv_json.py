bb60bcd9099be845d8ea04c9585d3ae7a2dbacb9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-22 14:34:12 +0200

    14.34

---
+++ codigo_abr_22_26/csv_json.py
@@ -0,0 +1,13 @@
+"""
+Convertir un formato en CSV a json y luego al revés:
+Lista de dict --> csv
+"""
+
+csv = """id;nombre;cargo
+1;Davolio;Representante de ventas
+2;Fuller;Vicepresidente comercial
+3;Leverling;Representante de ventas
+4;Peacock;Representante de ventas
+6;Suyama;Representante de ventas
+7;King;Representante de ventas
+9;Dodsworth;Representante de ventas"""
