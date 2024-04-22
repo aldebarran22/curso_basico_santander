2d3e6d69134aea5a9bc901842399da45551c5c09
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-22 11:37:03 +0200

    11.37

--- codigo_abr_22_26/condiciones.py
+++ codigo_abr_22_26/condiciones.py
@@ -4,10 +4,15 @@ Condiciones en Python: and, or, not
 
 inf = 10
 sup = 20
-num = 15
+num = 25
 
 # Saber si num está dentro del intervalo:
 if num >= inf and num <= sup:
+    print("En el intervalo")
+else:
+    print("Fuera del intervalo")
+
+if inf <= num <= sup:
     print("En el intervalo")
 else:
     print("Fuera del intervalo")
