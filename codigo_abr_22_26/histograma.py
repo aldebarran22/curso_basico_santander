a564afc8dcb5de248a812ec863746532705c3604
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-23 10:09:41 +0200

    10.09

---
+++ codigo_abr_22_26/histograma.py
@@ -0,0 +1,24 @@
+"""
+Histograma:
+Lista: Almacenar valores con repeticiones
+Conjunto: Extraer los valores sin repetidos
+Diccionario: Un recuento de cada valor
+"""
+
+import random
+
+n = 10
+L = []
+histo = dict()
+# El intervalo de números:
+ini = 1
+fin = 25
+
+# Almacenar n números aleatorios en la lista L
+
+# Quitar los repetidos y almacenarlos en un conjunto
+
+# Contar los valores del conjunto en la lista y almacenar
+# en el diccionario (la clave -> el número), (el valor -> el recuento)
+
+# Imprimir resultados:
