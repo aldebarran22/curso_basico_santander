d350ff476cf0f05ba63da0662707e33951f6e287
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-23 11:36:37 +0200

    11.36

--- codigo_abr_22_26/filtro_ficheros.py
+++ codigo_abr_22_26/filtro_ficheros.py
@@ -4,3 +4,11 @@ que sean de extensión "txt" o "png"
 """
 
 import os
+
+L = os.listdir()
+extensiones = ("txt", "png")
+for f  in L:
+    ext = f.partition('.')[2]
+    if ext in extensiones:
+        print(f)
+
