28b458d2972befa1a353a5b9e4f818e773098343
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-23 10:49:28 +0200

    10.49

--- codigo_abr_22_26/tuplas.py
+++ codigo_abr_22_26/tuplas.py
@@ -11,21 +11,25 @@ select * from clientes where pais=?
 Operadores: + * in slicing
 Métodos: count, index
 """
-a,b,c = 3,4,5
 
-a,b = b,a
-print(a,b)
+a, b, c = 3, 4, 5
 
-t = tuple(range(1,11))
+a, b = b, a
+print(a, b)
+
+t = tuple(range(1, 11))
 print(t)
 
-t2 = (1,2,3,1,22,3,1,2,3,1)
+t2 = (1, 2, 3, 1, 22, 3, 1, 2, 3, 1)
 print(t2.index(1))
 print(t2.index(1, 1))
 print(t2.index(1, 4))
 
 # Obtener las posiciones  que ocupa un valor dentro de la tupla
-valor = 1
-
-
-
+valor = 3
+inicio = 0
+n = t2.count(valor)
+for i in range(n):
+    pos = t2.index(valor, inicio)
+    print(pos, end=' ')
+    inicio = pos + 1
