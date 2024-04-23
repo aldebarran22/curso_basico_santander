95ddb515c2c8e1d61e99636f0e2ba0b682d712dd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-23 12:53:21 +0200

    12.53

--- codigo_abr_22_26/funciones_parametros.py
+++ codigo_abr_22_26/funciones_parametros.py
@@ -25,15 +25,29 @@ def calcularIVA(importe, iva=21):
     return round(importe * iva / 100, 2)
 
 
-# 1) Forma posicional
-print(sumar(3, 4))
+def funcion(ob, op=0, *args, **kwargs):
+    print("obligatorio: ", ob)
+    print("opcional: ", op)
+    print("args: ", args)
+    print("kwargs: ", kwargs)
+    print("-" * 20)
 
-# 2) Nominal:
-print(sumar(b=4, a=3))
-print(segundos(12, 30))
-print(segundos(m=5))
-print(segundos(m=5, s=30))
-print(segundos(12, s=30))
 
-print(calcularIVA(100))
-print(calcularIVA(100, 4))
+if __name__ == "__main__":
+    funcion(1)
+    funcion(1,2,3,4,5,6,7,8,x=100, y=200)
+
+    exit()
+
+    # 1) Forma posicional
+    print(sumar(3, 4))
+
+    # 2) Nominal:
+    print(sumar(b=4, a=3))
+    print(segundos(12, 30))
+    print(segundos(m=5))
+    print(segundos(m=5, s=30))
+    print(segundos(12, s=30))
+
+    print(calcularIVA(100))
+    print(calcularIVA(100, 4))
