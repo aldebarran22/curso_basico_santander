5407003a0e72356c96427212c880947e8f5a6059
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-23 10:20:34 +0200

    10.20

--- codigo_abr_22_26/histograma.py
+++ codigo_abr_22_26/histograma.py
@@ -17,11 +17,17 @@ fin = 25
 # Almacenar n números aleatorios en la lista L
 for i in range(n):
     L.append(random.randint(ini, fin))
-print(L[:5])
+#print(L[:5])
 
 # Quitar los repetidos y almacenarlos en un conjunto
+numeros = set(L)
+# print(numeros)
 
 # Contar los valores del conjunto en la lista y almacenar
 # en el diccionario (la clave -> el número), (el valor -> el recuento)
+for i in numeros:
+    histo[i] = L.count(i)
 
 # Imprimir resultados:
+for numero, cuenta in histo.items():
+    print(f"{numero} se repite {cuenta} veces")
