0e1ce6dc372679a3d3200fe1c5dbd08178b5fcba
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-23 12:46:54 +0200

    12.46

--- codigo_abr_22_26/funciones_parametros.py
+++ codigo_abr_22_26/funciones_parametros.py
@@ -12,19 +12,28 @@ Formas de llamar a una función (con respecto a los parámetros):
 - Con un diccionario
 """
 
+
 def sumar(a, b):
     return a + b
 
+
 def segundos(h=0, m=0, s=0):
-    return h*3600 + m*60 + s
+    return h * 3600 + m * 60 + s
+
+
+def calcularIVA(importe, iva=21):
+    return round(importe * iva / 100, 2)
+
 
 # 1) Forma posicional
-print(sumar(3,4))
+print(sumar(3, 4))
 
 # 2) Nominal:
 print(sumar(b=4, a=3))
-print(segundos(12,30))
+print(segundos(12, 30))
 print(segundos(m=5))
 print(segundos(m=5, s=30))
-print(segundos(12,s=30))
+print(segundos(12, s=30))
 
+print(calcularIVA(100))
+print(calcularIVA(100, 4))
