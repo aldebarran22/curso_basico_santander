25f6a29479190c6026b387425f290cd98c154aec
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-24 10:36:12 +0200

    10.36

--- codigo_abr_22_26/fecha_hora.py
+++ codigo_abr_22_26/fecha_hora.py
@@ -1,36 +1,46 @@
-
-
 class Time:
 
-    def __init__(self,h=0,m=0,s=0):
+    def __init__(self, h=0, m=0, s=0):
         self.h = h
         self.m = m
         self.s = s
 
-   
-   
-    def __str__(self):        
-        return '%02d:%02d:%02d' % (self.h,self.m,self.s)
+    def __ajustar(self):
+        pass
 
+    def __add__(self, other):
+        obj = Time(self.h + other.h, self.m + other.m, self.s + other.s)
+        obj.__ajustar()
+        return obj
 
-   
+    def __str__(self):
+        return "%02d:%02d:%02d" % (self.h, self.m, self.s)
+
 
 class Date:
 
-    def __init__(self, dd,mm,yy):
+    def __init__(self, dd, mm, yy):
         self.dd = dd
         self.mm = mm
         self.yy = yy
 
     def __str__(self):
-        return "%02d/%02d/%04d" % (self.dd,self.mm,self.yy)
+        return "%02d/%02d/%04d" % (self.dd, self.mm, self.yy)
 
     def esBisiesto(self):
-        anyo = self.yy    
-        if  (anyo % 4 == 0 and anyo % 100 != 0) or (anyo%100 == 0 and anyo % 400 == 0):
-            return True    
+        anyo = self.yy
+        if (anyo % 4 == 0 and anyo % 100 != 0) or (anyo % 100 == 0 and anyo % 400 == 0):
+            return True
         else:
             return False
 
- 
 
+if __name__ == "__main__":
+    t = Time(2, 4, 340)
+    print(t)
+
+    t2 = Time(12, 14, 51)
+    print(t2)
+
+    suma = t + t2  # suma = t.__add__(t2)
+    print(suma)
