83c4360e126c5cfa13882c7cbaa4140df2733f6b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-24 14:33:44 +0200

    14.33

--- codigo_abr_22_26/excepciones.py
+++ codigo_abr_22_26/excepciones.py
@@ -8,11 +8,24 @@ def prueba1():
     """Capturar y tratar una excepción"""
     try:
         L = [1, 2, 3]
-        print(L[10])
+        print(L[1])
         print("mas código")
     except IndexError as e:
         print(e.__class__.__name__, e)
 
 
+def prueba2():
+    """Capturar y tratar varias excepciones"""
+    try:
+        L = [1, 2, 3]
+        print(L[1] / 0)
+        print("mas código")
+    except IndexError as e:
+        print(e.__class__.__name__, e)
+    except ZeroDivisionError as e:
+        print(e.__class__.__name__, e)
+
+
 if __name__ == "__main__":
-    prueba1()
+    # prueba1()
+    prueba2()
