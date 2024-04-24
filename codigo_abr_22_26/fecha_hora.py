a2e7bcc48e627db877944e80fde9c00980453f73
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-24 11:48:30 +0200

    11.48

--- codigo_abr_22_26/fecha_hora.py
+++ codigo_abr_22_26/fecha_hora.py
@@ -70,6 +70,20 @@ class DateTime(Time, Date):
 
     def __str__(self):
         return Date.__str__(self) + " " + Time.__str__(self)
+    
+class DateTime2:
+    """Implementación de la clase DateTime por composición"""
+
+    def __init__(self, dd=1, mm=1, yy=1970, h=0, m=0, s=0):
+        self.fecha = Date(dd,mm,yy)
+        self.hora = Time(h,m,s)
+
+    def esBisiesto(self):
+        return self.fecha.esBisiesto()
+
+    def __str__(self):
+        return str(self.fecha) + " " + str(self.hora)
+
 
 
 if __name__ == "__main__":
