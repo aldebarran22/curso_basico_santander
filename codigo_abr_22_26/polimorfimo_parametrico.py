9d8ddcf170cfa4444b7e1f5b13dd0604c39ec119
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-24 12:06:00 +0200

    12.05

--- codigo_abr_22_26/polimorfimo_parametrico.py
+++ codigo_abr_22_26/polimorfimo_parametrico.py
@@ -1,37 +1,44 @@
 from functools import singledispatch
 
+
 @singledispatch
 def func(arg):
-	print('Comportamiento por defecto')
-	
-	
+    print("Comportamiento por defecto")
+
+
 @func.register(int)
 @func.register(float)
 def _(arg):
-	print('Recibo el numero ',arg)
-	
+    print("Recibo el numero ", arg)
+
+
 @func.register(str)
 def _(arg):
-	print('Viene la cadena ', arg, ' de long: ', len(arg))
-	
+    print("Viene la cadena ", arg, " de long: ", len(arg))
+
+
 @func.register(list)
+@func.register(set)
 def _(arg):
-	for i in arg:
-		print(i)
-	
+    print("Tipo: ", type(arg))
+    for i in arg:
+        print(i)
+
+
 class Clase:
-	pass
-	
+    pass
+
+
 @func.register(Clase)
 def _(arg):
-	print('un objeto de Clase')
-	
-	
-if __name__ == '__main__':
-	func(list('hola'))
-	func('hola')
-	func(888)
-	func(8.99)
-	a = Clase()
-	func(a)
-	func(set(range(10)))
+    print("un objeto de Clase")
+
+
+if __name__ == "__main__":
+    func(list("hola"))
+    func("hola")
+    func(888)
+    func(8.99)
+    a = Clase()
+    func(a)
+    func(set(range(10)))
