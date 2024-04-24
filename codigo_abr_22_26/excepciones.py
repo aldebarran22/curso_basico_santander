96ee5580ebcec96f58204bf5ccf8eca4623cc92c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-24 14:26:58 +0200

    14.26

---
+++ codigo_abr_22_26/excepciones.py
@@ -0,0 +1,11 @@
+"""
+Capturar excepciones en Python
+try, except, finally, raise
+"""
+
+def prueba1():
+    """Capturar y tratar una excepción"""
+    pass
+
+if __name__ == '__main__':
+    prueba1()
