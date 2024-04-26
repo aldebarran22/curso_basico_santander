4165718743eea896ca3d070709ac402f0716e3f9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-04-26 14:56:34 +0200

    14.56

--- codigo_abr_22_26/unittest_funciones.py
+++ codigo_abr_22_26/unittest_funciones.py
@@ -9,14 +9,19 @@ from ecuacion import calcularEcuacion
 
 class TestFunciones(unittest.TestCase):
 
+    def setUp(self):
+        self.tupla = 1,5,4
+
     def test_resultado(self):
-        resul = calcularEcuacion(1,5,4)
+        resul = calcularEcuacion(*self.tupla)
         self.assertEqual(resul, (-1, -4), msg="No coincide el resultado")
         return True
 
     def test_exception(self):
         self.assertRaises(ValueError, calcularEcuacion, 1,2,3)
         
+    def tearDown(self):
+        del self.tupla
 
 if __name__ == '__main__':
     unittest.main()
