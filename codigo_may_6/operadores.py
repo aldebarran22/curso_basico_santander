5183baecee0cfaa3980899d8f98cb4a30759c5e6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 11:42:47 +0200

    11.42

---
+++ codigo_may_6/operadores.py
@@ -0,0 +1,15 @@
+"""
+Operadores aritméticos
+"""
+
+# Div. real, la media de dos números
+n1 = 12
+n2 = 7
+media = (n1 + n2) / 2
+print("media: ", media)
+
+# Div entera //
+importe = 230
+num50 = importe // 50
+resto = importe % 50
+print("num50: ", num50, "resto: ", resto)
