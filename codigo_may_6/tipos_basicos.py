ec55321b5a2a8e25b7e94d1acf9ecb0d7dcfba1f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 10:18:36 +0200

    10.18

---
+++ codigo_may_6/tipos_basicos.py
@@ -0,0 +1,18 @@
+"""
+Tipo de las variables.
+Operaciones con variables
+"""
+
+# Operaciones con variables enteras
+numero = 100
+print(numero, type(numero))
+numero2 = 200
+print(numero + numero2)
+
+# Operaciones con variables de texto:
+txt1 = "100"
+print(txt1, type(txt1))
+txt2 = '200'
+print(txt1 + txt2)
+
+
