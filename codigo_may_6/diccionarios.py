59a950fce3ffb6116d79dfda7b8338d51d5ca623
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 14:35:12 +0200

    14.35

--- codigo_may_6/diccionarios.py
+++ codigo_may_6/diccionarios.py
@@ -5,10 +5,16 @@ Histogramas
 Json
 """
 
-d = {"1":100, "2":200, "3":300}
+d = {"1": 100, "2": 200, "3": 300}
 print(d, type(d))
 
 # Añadir nuevas claves:
 d["4"] = 400
 d["1"] = 1111
 print(d)
+print(d["2"])
+# print(d["7"]) keyError una clave que no existe en el diccionario.
+
+# Cargar un dicc con dos cadenas (con un sep)
+claves = "id;nombre;cargo"
+valores = "1;Davolio;Representante de ventas"
