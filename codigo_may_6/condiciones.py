96e7d22ff5a0b78e031d8e628c60630348572fc2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 12:15:58 +0200

    12.15

--- codigo_may_6/condiciones.py
+++ codigo_may_6/condiciones.py
@@ -2,10 +2,10 @@
 Operadores relacionales en Python y ope. lógicos: and, or, not
 """
 
-# Comprobar si una var. está en un rango:
+# Comprobar si una var. está en un rango:[10,50]
 ini = 10
 fin = 50
-num = 341
+num = 34
 if num >= ini and num <= fin:
     print(f"{num} cumple el intervalo")
 else:
@@ -15,3 +15,8 @@ if ini <= num <= fin:
     print(f"{num} cumple el intervalo")
 else:
     print("Fuera del intervalo")
+
+if num < ini or num > fin:
+    print("Fuera del intervalo")
+else:
+    print(f"{num} cumple el intervalo")
