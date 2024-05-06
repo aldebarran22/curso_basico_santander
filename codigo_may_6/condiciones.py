5e883eb04075adbdf0fdf4908734e11a52cb14fd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 12:10:37 +0200

    12.10

---
+++ codigo_may_6/condiciones.py
@@ -0,0 +1,12 @@
+"""
+Operadores relacionales en Python y ope. lógicos: and, or, not
+"""
+
+# Comprobar si una var. está en un rango:
+ini = 10
+fin = 50
+num = 34
+if num >= ini and num <= fin:
+    print(f"{num} cumple el intervalo")
+else:
+    print("Fuera del intervalo")
--- codigo_may_6/ecuaciones.py
+++ codigo_may_6/ecuaciones.py
@@ -1,17 +1,18 @@
 """
 Ecuaciones de segundo grado
 """
-import math 
+
+import math
 
 # Inicialización múltiple
-a,b,c = 1,5,4
+a, b, c = 1, 2, 3
 
-raiz = b**2 - 4*a*c
+raiz = b**2 - 4 * a * c
 if raiz > 0:
-    x1 = (-b + math.sqrt(raiz)) / (2*a)
-    x2 = (-b - math.sqrt(raiz)) / (2*a)
+    x1 = (-b + math.sqrt(raiz)) / (2 * a)
+    x2 = (-b - math.sqrt(raiz)) / (2 * a)
     print("x1: ", x1)
     print("x2: ", x2)
+
 else:
-    print('No hay solución')
-print('fin de programa')
+    print("No hay solución")
