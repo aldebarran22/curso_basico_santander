d4b7aa0117af0bd3e7a7d00011a71c4fedcf1c69
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 14:30:33 +0200

    14.30

--- codigo_may_6/diccionarios.py
+++ codigo_may_6/diccionarios.py
@@ -1,7 +1,14 @@
 """
 Diccionarios en Python
-Pares de clave  - valor
-
+Pares de clave(inmutable)  - valor(lo que queramos)
 Histogramas
 Json
 """
+
+d = {"1":100, "2":200, "3":300}
+print(d, type(d))
+
+# Añadir nuevas claves:
+d["4"] = 400
+d["1"] = 1111
+print(d)
