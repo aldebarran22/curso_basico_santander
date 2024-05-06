7cb3ac3a5963c7af0ec7d352ea7e57fb4452fddb
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 12:48:43 +0200

    12.48

---
+++ codigo_may_6/listas.py
@@ -0,0 +1,17 @@
+"""
+Listas en Python:
+- Crear listas: [], list()
+- Acceso a los elementos con los índices
+- Funciones: sum, len, min, max
+- Slicing
+- Bucles
+- Copia de listas
+"""
+
+L = [12, 33, 1, 90, 120, 12]
+print(L, type(L))
+print("suma: ", sum(L))
+print("recuento: ", len(L))
+print("max:", max(L))
+print("min:", min(L))
+print("promedio: ", sum(L) / len(L))
