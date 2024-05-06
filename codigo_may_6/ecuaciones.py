887576a579d02081bfa121841c378cb8ff2dfe01
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-06 12:05:04 +0200

    12.04

--- codigo_may_6/ecuaciones.py
+++ codigo_may_6/ecuaciones.py
@@ -4,10 +4,14 @@ Ecuaciones de segundo grado
 import math 
 
 # Inicialización múltiple
-a,b,c = 1,2,3
+a,b,c = 1,5,4
 
 raiz = b**2 - 4*a*c
-x1 = (-b + math.sqrt(raiz)) / (2*a)
-x2 = (-b - math.sqrt(raiz)) / (2*a)
-print("x1: ", x1)
-print("x2: ", x2)
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2*a)
+    x2 = (-b - math.sqrt(raiz)) / (2*a)
+    print("x1: ", x1)
+    print("x2: ", x2)
+else:
+    print('No hay solución')
+print('fin de programa')
