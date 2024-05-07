de48e3898afc59e45085ed3c1dc3c51cda28935c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 13:54:24 +0200

    13.54

--- codigo_may_6/funcion_parametros.py
+++ codigo_may_6/funcion_parametros.py
@@ -3,14 +3,18 @@ Declaración de parámetros en las funciones de Python
 """
 
 
-def funcion(ob, op=0, *args):
+def funcion(ob, op=0, *args, **kwargs):
     print("obligatorio: ", ob)
     print("opcional: ", op)
-    print('args: ', args)
+    print("args: ", args)
+    print("kwargs: ", kwargs)
     print("-" * 30)
 
 
 if __name__ == "__main__":
     funcion(1)
     funcion(1, 2)
-    funcion(1,2,3,4,5,6)
+    funcion(1, 2, 3, 4, 5, 6)
+    funcion(1, 2, 3, 4, 5, 6, x=10, y=20)
+
+    # print("hola", "adios", sep="_")
