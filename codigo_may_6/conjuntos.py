5f3aaa9a1e4d594a25b7fa0f0b1ae8c110e656b6
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 09:53:05 +0200

    9.53

---
+++ codigo_may_6/conjuntos.py
@@ -0,0 +1,13 @@
+"""
+Operaciones a nivel de conjunto.
+& intersección
+| unión (quita repetidos)
+- diferencia
+^ diferencia simétrica
+"""
+
+comida = {"Ana","Gema", "Luis", "Pedro", "Raquel"}
+cena = {"Gema", "Luis", "Sofia","Andres", "Sonia"}
+
+print("Quien va a comer y cenar: ", comida & cena)
+
