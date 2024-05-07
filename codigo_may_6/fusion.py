fcd8aadfbde3fc7dd9178b0673968a8488afb74a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 10:02:36 +0200

    10.02

---
+++ codigo_may_6/fusion.py
@@ -0,0 +1,23 @@
+"""
+Fusionar dos ficheros de empleados en uno solo, quitando repetidos
+"""
+
+csv2 = """id;nombre;cargo
+1;Davolio;Representante de ventas
+2;Fuller;Vicepresidente comercial
+3;Leverling;Representante de ventas
+4;Peacock;Representante de ventas
+6;Suyama;Representante de ventas
+7;King;Representante de ventas
+9;Dodsworth;Representante de ventas"""
+
+csv3 = """id;nombre;cargo
+2;Fuller;Vicepresidente comercial
+3;Leverling;Representante de ventas
+4;Peacock;Gerente de ventas
+5;Buchanan;Gerente de ventas
+6;Suyama;Representante de ventas
+7;King;Representante de ventas
+8;Callahan;Coordinador ventas interno
+9;Dodsworth;Representante de ventas
+10;George;Representante de ventas"""
