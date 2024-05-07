7ec3ef035f5eef17621a56f757494363f4d02c57
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 12:43:45 +0200

    12.43

--- codigo_may_6/ecuaciones_funcion.py
+++ codigo_may_6/ecuaciones_funcion.py
@@ -4,15 +4,13 @@ Ecuaciones de segundo grado
 
 import math
 
-# Inicialización múltiple
-a, b, c = 1, 2, 3
 
-raiz = b**2 - 4 * a * c
-if raiz > 0:
-    x1 = (-b + math.sqrt(raiz)) / (2 * a)
-    x2 = (-b - math.sqrt(raiz)) / (2 * a)
-    print("x1: ", x1)
-    print("x2: ", x2)
-
-else:
-    print("No hay solución")
+def ecuacion(a, b, c):
+    """Calculo de ecuaciones de 2 grado"""
+    raiz = b**2 - 4 * a * c
+    if raiz > 0:
+        x1 = (-b + math.sqrt(raiz)) / (2 * a)
+        x2 = (-b - math.sqrt(raiz)) / (2 * a)
+        return x1, x2
+    else:
+        return None
