f0cd344f73b0c72610a07cad3c56cac4feb7018c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 11:56:02 +0200

    11.55

---
+++ codigo_may_6/funciones.py
@@ -0,0 +1,16 @@
+"""
+Funciones en Python.
+- Tipos anotados
+- Formas de llamar a una función
+"""
+
+
+def sumar(a, b):
+    return a + b
+
+
+def sumar(a, b, c):
+    return a + b + c
+
+
+print(sumar(12, 34))
