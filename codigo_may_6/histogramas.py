4f63a38d8d03a6ecd03464ad2b384235d032ead1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 09:36:38 +0200

    9.36

--- codigo_may_6/histogramas.py
+++ codigo_may_6/histogramas.py
@@ -6,6 +6,11 @@ Conjunto: obtener las claves para el diccionario.
 """
 
 L = [1, 2, 3, 4, 4, 3, 1, 1, 1, 5, 6, 7, 8, 9, 0, 8, 6, 4, 32, 1, 1, 2]
+c = set(L)
+print(c)
+
+for i in c:
+    print(i,'se repite',L.count(i))
 
 
 # histogramas
