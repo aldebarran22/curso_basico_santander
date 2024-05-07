efae2b272b2b2d842ed1dd7d5b86cc2fe590863f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 14:16:36 +0200

    14.16

--- codigo_may_6/funciones2.py
+++ codigo_may_6/funciones2.py
@@ -12,9 +12,13 @@ def restar(a, b):
 
 
 def ejecutar(funcion, *args, **kwargs):
+    print("Vamos a ejecutar: ", funcion.__name__)
     print(funcion(*args, **kwargs))
 
 
 if __name__ == "__main__":
     ejecutar(restar, 23, 45)
     ejecutar(sumar, 1, 2, 3, 4)
+
+    # L = [1, 2, 3, 4]
+    # L(0) Error, intenta ejecutar una función pero es una lista
