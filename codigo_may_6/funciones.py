62c637f729e68edb84beb9b929ed9984df4a71f9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-07 12:34:00 +0200

    12.33

--- codigo_may_6/funciones.py
+++ codigo_may_6/funciones.py
@@ -4,6 +4,8 @@ Funciones en Python.
 - Formas de llamar a una función:
 -1) forma posicional
 -2) forma nominal (el nombre de los parámetros)
+-3) Con una tupla / lista
+-4) Con un diccionario
 """
 
 
@@ -19,22 +21,24 @@ def sumar(a: int, b: int) -> int:
 def segundos(h=0, m=0, s=0):
     return h * 3600 + m * 60 + s
 
+
 def calcularIVA(importe, iva=0.21):
     return round(importe * iva, 2)
+
 
 # Forma posicional:
 print(sumar("12", "34"))
 print(sumar([12, 34], [1, 2, 3, 4]))
 print(sumar(b=34, a=12))
 
-print(segundos(12,30))
+print(segundos(12, 30))
 
 # Forma nominal:
 print(segundos(m=30, s=15))
 print(segundos(s=60))
 print(segundos(12, s=60))
 
-L = [(12,), (5,30), (8,32,45),(17,30,40)]
+L = [(17, 30, 40), (12,), (5, 30), (8, 32, 45)]
 """
 for h,m,s in L:
     print(segundos(h,m,s))
@@ -48,3 +52,6 @@ for t in L:
 for t in L:
     print(segundos(*t))
 
+# Con un diccionario:
+d = {"h": 12, "m": 45, "s": 3}
+print(segundos(**d))
