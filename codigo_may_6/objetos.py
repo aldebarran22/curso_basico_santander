196120c5d2a2f448a16635d3cf79ba5c873ea323
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 10:40:20 +0200

    10.40

--- codigo_may_6/objetos.py
+++ codigo_may_6/objetos.py
@@ -6,8 +6,6 @@ Diseño de clases
 - Listas de objetos
 """
 
-from typing import Any
-
 
 class Persona:
     """Implementación de la clase Persona"""
@@ -35,15 +33,43 @@ class Persona:
                 return False
         return True
 
+    def hablarCon(self, other=None):
+        if other is None:
+            print(self.nombre, "habla solo")
+        else:
+            print(self.nombre, "y", other.nombre, "están hablando")
+
     def __del__(self):
         pass
         # print('Se está borrando: ', self.nombre)
 
 
+class Guia(Persona):
+
+    def __init__(self, nombre="", edad=0, altura=0.0, ambito="", idiomas=[]):
+        Persona.__init__(self, nombre, edad, altura)
+        # super().__init__(nombre, edad, altura)
+
+        self.ambito = ambito
+        self.idiomas = idiomas
+
+
+def pruebaGuia():
+    g1 = Guia("Pedro", 45, 1.77, "N", ["inglés", "francés"])
+    g2 = Guia("José", 55, 1.78, "I", ["inglés", "alemán"])
+
+    if g1 < g2:
+        print(g1.nombre, "es menor que", g2.nombre)
+    else:
+        print(g2.nombre, "es menor que", g1.nombre)
+
+    g1.hablarCon(g2)
+
+
 def pruebaPersona():
     p1 = Persona("Miguel", 34, 1.8)
-    p3 = Persona("Miguel", 34, 1.8)
-    p4 = Persona("Miguel", 34, 1.81)
+    p3 = Persona("Ana", 32, 1.82)
+    p4 = Persona("Paula", 31, 1.71)
     p2 = Persona("Eva", 44, 1.79)
     print(p1)
     # print(str(p1))
@@ -74,6 +100,9 @@ def pruebaPersona():
         print("iguales")
     else:
         print("no iguales")
+
+    p1.hablarCon()
+    p1.hablarCon(p2)
 
 
 if __name__ == "__main__":
