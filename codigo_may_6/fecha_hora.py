73e708f23eeabbe220126ba47faaa69854f95129
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 11:58:11 +0200

    11.58

--- codigo_may_6/fecha_hora.py
+++ codigo_may_6/fecha_hora.py
@@ -43,11 +43,14 @@ class Date:
             return False
 
 
-class DateTime(Date, Time):
+class DateTime(Time, Date):
 
     def __init__(self, d=1, m=1, y=1970, hh=0, mm=0, ss=0):
         Date.__init__(self, d, m, y)
         Time.__init__(self, hh, mm, ss)
+
+    def __str__(self):
+        return Date.__str__(self) + " " + Time.__str__(self)
 
 
 if __name__ == "__main__":
