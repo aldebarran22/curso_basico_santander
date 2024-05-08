756f9e6cf40c37f69ac305e3e5bf7e39fd68876a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-08 09:10:19 +0200

    9.10

--- codigo_may_6/prueba_global.py
+++ codigo_may_6/prueba_global.py
@@ -1,48 +1,57 @@
-
 myGlobal = 5
 
+
 def func1():
-	global myGlobal
-	myGlobal = 42
+    global myGlobal
+    myGlobal = 42
+
 
 def func2():
-    print (myGlobal)
-
-func1()
-func2()
-
-
-
+    print(myGlobal)
 
 
 # datos globales:
 num = 234
-L=[1,2,3]
+L = [1, 2, 3]
 
 
 def funcion():
-	# Accede a las variables globales en modo lectura:
-	# No hace global, ni se distingue entre mutable e inmutable
-	print("num:",num)
-	print("L:",L)
-	
+    # Accede a las variables globales en modo lectura:
+    # No hace global, ni se distingue entre mutable e inmutable
+    print("num:", num)
+    print("L:", L)
+
+
 def funcion2():
-	# Si la variable global es un tipo inmutable y la vamos a modificar
-	# es necesario poner global:
-	
-	# Si el objeto es mutable y lo vamos a modificar MEDIANTE UNA ASIGNACION
-	# en este si es obligatorio poner global
-	global num,L
-	num+=2
-	L+=[45]
-		
+    # Si la variable global es un tipo inmutable y la vamos a modificar
+    # es necesario poner global:
+
+    # Si el objeto es mutable y lo vamos a modificar MEDIANTE UNA ASIGNACION
+    # en este si es obligatorio poner global
+    global num, L
+    num += 2
+    L += [45]
+
+
 def funcion3():
-	# Si el objeto es mutable y se modifica con un metodo NO ES NECESARIO
-	# PONER global
-	L.append(8)	
-	
-funcion()
-funcion2()
-funcion()
-funcion3()
-funcion()
+    # Si el objeto es mutable y se modifica con un metodo NO ES NECESARIO
+    # PONER global
+    L.append(8)
+
+
+def prueba1():
+    func1()
+    func2()
+
+
+def prueba2():
+    funcion()
+    funcion2()
+    funcion()
+    funcion3()
+    funcion()
+
+
+if __name__ == "__main__":
+    prueba1()
+    # prueba2()
