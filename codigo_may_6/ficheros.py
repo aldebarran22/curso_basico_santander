5b9a8cdc2dc10435c24a4c34881d0b48fa871b8e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-09 11:44:10 +0200

    11.44

--- codigo_may_6/ficheros.py
+++ codigo_may_6/ficheros.py
@@ -6,14 +6,19 @@ exportar datos a ficheros con la función print
 
 def procesarCSV(path, sep=";"):
     fich = None
+    cabs = True    
     try:
         paises = dict()
         fich = open(path, "r")
         for fila in fich:
+            if cabs:
+                cabs = False
+                continue
+            
             fila = fila.rstrip()
             L = fila.split(sep)
             pais = L[-1]
-            importe = L[-2]
+            importe = float(L[-2].replace(",","."))
             print(pais, importe)
 
     except Exception as e:
