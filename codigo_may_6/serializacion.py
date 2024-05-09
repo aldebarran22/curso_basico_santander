7c9c3ccb6908b0cdbb6695a9685878afd0c4599b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-09 12:50:01 +0200

    12.49

--- codigo_may_6/serializacion.py
+++ codigo_may_6/serializacion.py
@@ -7,6 +7,27 @@ shelve -> varios objetos (diccionario)
 from random import randint
 from string import ascii_letters
 
+import pickle as p
+import shelve as s
+
+
+def serializarPickle(path, obj):
+    f = None
+    try:
+        f = open(path, "wb")
+        p.dump(obj, f, 2)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if f:
+            f.close()
+
+
+def deserializarPickle(path):
+    pass
+
 
 def generarDict():
     # Generar una estructura aleatoria, lista de diccionarios, cada dicc representa un candidato.
