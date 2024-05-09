fc2c844bdc52ebd1007d520d475de8585e4e9ce9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-09 09:15:24 +0200

    9.15

--- codigo_may_6/excepciones.py
+++ codigo_may_6/excepciones.py
@@ -7,12 +7,25 @@ try / except / finally / raise
 def test1():
     # Capturar una excepción
     try:
-        L = [1,2,3,4,5]
+        L = [1, 2, 3, 4, 5]
         print(L[10])
         print(L[0] * 2)
 
     except IndexError as e:
         print(e)
 
+
+def test2():
+    # Capturar dos excepciones
+    try:
+        L = [1, 2, 3, 4, 5]
+        print(L[1])
+        print(L[0] / 0)
+
+    except IndexError as e:
+        print(e)
+
+
 if __name__ == "__main__":
-    test1()
+    # test1()
+    test2()
