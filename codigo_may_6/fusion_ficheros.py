48a9f7993b0c6959bffe31cff2f37ee16b78fec9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-09 10:39:39 +0200

    10.39

--- codigo_may_6/fusion_ficheros.py
+++ codigo_may_6/fusion_ficheros.py
@@ -6,7 +6,7 @@ Fusionar dos ficheros de empleados en uno solo, quitando repetidos
 # y devuelva un conjunto a nivel de filas.
 
 
-def fusion(sep: str = "\n") -> str:
+def fusion(path1: str, path2: str, pathD: str, sep: str = "\n") -> str:
 
     def ordenar(fila):
         datos = fila.partition(";")
@@ -15,13 +15,22 @@ def fusion(sep: str = "\n") -> str:
         else:
             return 0
 
-    def csvToSet(csv: str, sep: str = "\n") -> set:
-        lineas = csv.split(sep)
-        return set(lineas)
+    def csvToSet(path: str, sep: str = "\n") -> set:
+        fich = None
+        try:
+            fich = open(path, "r")
+            csv = fich.read()
+            lineas = csv.split(sep)
+            return set(lineas)
+        except Exception as e:
+            print(e)
+        finally:
+            if fich:
+                fich.close()
 
     # Codigo de la funcion fusion
-    c2 = csvToSet(csv2)
-    c3 = csvToSet(csv3)
+    c2 = csvToSet(path1)
+    c3 = csvToSet(path2)
     todo = c2 | c3
     L = sorted(todo, key=ordenar)
     # print(todo)
@@ -30,7 +39,9 @@ def fusion(sep: str = "\n") -> str:
 
 if __name__ == "__main__":
     # Convertir el texto csv a un conjunto de filas:
-    todo = fusion("../ficheros_curso/Empleados2.txt",
-                  "../ficheros_curso/Empleados3.txt",
-                  "../ficheros/empleados_final.txt")
+    todo = fusion(
+        "../ficheros_curso/Empleados2.txt",
+        "../ficheros_curso/Empleados3.txt",
+        "../ficheros/empleados_final.txt",
+    )
     print(todo)
