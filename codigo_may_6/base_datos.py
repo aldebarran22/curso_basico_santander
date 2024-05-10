c42cfa4b15177d155f275fabcb4ecaa827294a39
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-10 09:53:38 +0200

    9.53

--- codigo_may_6/base_datos.py
+++ codigo_may_6/base_datos.py
@@ -58,6 +58,51 @@ def testExportar():
     exportar("../../bd/empresa3.db", "pedidos")
 
 
+class Categoria:
+
+    def __init__(self, id=0, nombre=""):
+        self.id = id
+        self.nombre = nombre
+
+    def __str__(self):
+        return str(self.id) + " " + self.nombre
+
+    def __repr__(self):
+        return str(self)
+
+
+class Producto:
+
+    def __init__(self, id=0, nombre="", cat=Categoria(), precio=0.0, exis=0):
+        self.id = id
+        self.nombre = nombre
+        self.cat = cat
+        self.precio = round(precio, 2)
+        self.exis = exis
+
+    def __str__(self):
+        return (
+            str(self.id)
+            + " "
+            + self.nombre
+            + " "
+            + str(self.cat)
+            + " "
+            + str(self.precio)
+            + " "
+            + str(self.exis)
+        )
+
+    def __repr__(self):
+        return str(self)
+
+    def getTupla(self):
+        return (self.nombre, self.cat.id, self.precio, self.exis)
+
+    def getTupla2(self):
+        return (self.nombre, self.cat.id, self.precio, self.exis, self.id)
+
+
 class BaseDatos:
 
     def __init__(self, path):
