3230b68d01b675390660cc2912380122624be0d0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-05-10 13:06:18 +0200

    13.06

--- codigo_may_6/servidor.py
+++ codigo_may_6/servidor.py
@@ -11,11 +11,11 @@ if len(sys.argv) == 2:
     print("Puerto: ", puerto)
 
     s_server = s_client = None
-    fich = None
+    #fich = None
     try:
-        t = datetime.now()
-        cad = t.strftime("%Y_%m_%d_%H_%M_%S")
-        fich = open(f"../ficheros/pedidos_{cad}.csv", "w")
+        #t = datetime.now()
+        #cad = t.strftime("%Y_%m_%d_%H_%M_%S")
+        #fich = open(f"../ficheros/pedidos_{cad}.csv", "w")
         # Crear un socket TCP / AF_INET
         s_server = s.socket()
         s_server.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
@@ -39,7 +39,7 @@ if len(sys.argv) == 2:
             if mensaje.lower() == "fin":
                 break
 
-            print(mensaje, file=fich)
+            print(mensaje)#, file=fich)
             s_client.send(mensaje.upper().encode("utf-8"))
 
         print("Fin de comunicación")
@@ -55,8 +55,8 @@ if len(sys.argv) == 2:
             s_client.close()
         if s_server:
             s_server.close()
-        if fich:
-            fich.close()
+        #if fich:
+        #    fich.close()
 
 else:
     print("No se ha indicado el puerto para conectar ...")
