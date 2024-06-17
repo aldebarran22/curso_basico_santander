2c9a0749f69bb85eb0e89b43c4c209143e0ee173
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 13:09:51 +0200

    13.0

--- codigo_jun_17/listas.py
+++ codigo_jun_17/listas.py
@@ -12,3 +12,12 @@ print('Suma: ', sum(L))
 print("Promedio: ", sum(L) / len(L))
 print('Mínimo: ', min(L))
 print('Máximo: ', max(L))
+print('El primero: ', L[0], L[-5])
+
+L2 = [[1,2,3,4], [5,6,7,8]]
+print("len de L2: ", len(L2))
+print("El primero: ", L2[0])
+print('El primero del primero: ', L2[0][0])
+print('El ocho: ', L2[-1][-1])
+
+
