79a975f7182595854e21291f52c18875ed755cef
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 13:04:43 +0200

    13.04

---
+++ codigo_jun_17/listas.py
@@ -0,0 +1,14 @@
+"""
+Listas: definión, constructor list()
+acceso a los elementos
+bucles
+funciones
+...
+"""
+
+L = [23,11,6,-6,10]
+print('Recuento: ', len(L))
+print('Suma: ', sum(L))
+print("Promedio: ", sum(L) / len(L))
+print('Mínimo: ', min(L))
+print('Máximo: ', max(L))
