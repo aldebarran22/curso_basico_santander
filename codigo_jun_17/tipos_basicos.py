703e772007a76baf550e2d5dfa81164275ff63c5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 10:34:09 +0200

    10.34

---
+++ codigo_jun_17/tipos_basicos.py
@@ -0,0 +1,15 @@
+"""
+Tipo de las variables y fuertemente tipado.
+"""
+
+numero = 100
+print(numero, type(numero))
+
+numero = numero + 0.5
+print(numero, type(numero))
+
+numero2  = "200"
+print(numero2, type(numero2))
+
+print(numero+ numero2)
+
