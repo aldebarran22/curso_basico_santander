5895c01f7281622c2dc70158d24621ad9caa98ff
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 11:46:40 +0200

    11.46

--- codigo_jun_17/tipos_basicos.py
+++ codigo_jun_17/tipos_basicos.py
@@ -14,3 +14,24 @@ print(numero2, type(numero2))
 
 # print(numero+ numero2)
 
+# Operación con tipos básicos:
+resul = 34 + 12.77 + 4+7j
+print(resul, type(resul))
+
+s1 = "23.5"
+s2 = "90.5"
+
+# Convertir de str a float:
+n1 = float(s1)
+n2 = float(s2)
+
+resul = float(s1) + float(s2)
+print(resul, n1+n2)
+
+#Covertir de float a str
+cad1 = str(n1)
+cad2 = str(n2)
+print(cad1 + cad2)
+
+
+
