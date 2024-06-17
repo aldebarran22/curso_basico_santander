fda2bdcddf8ae7d1d2aae2f41cdb5d32959bca6a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 12:17:28 +0200

    12.17

---
+++ codigo_jun_17/ecuacion.py
@@ -0,0 +1,13 @@
+"""
+Calcular una ecuación de 2 grado en Python
+ax^2 + by + c = 0
+
+x1 = -b + ...
+x2 = -b - ... 
+"""
+
+import math
+
+# Inicialización múltiple:
+a,b,c = 1,5,4
+
--- codigo_jun_17/operadores.py
+++ codigo_jun_17/operadores.py
@@ -4,7 +4,7 @@ Pruebas con operadores aritméticos: tipos numéricos y de texto
 
 # Calcular la media de dos números: div. real
 n1 = 34
-n2 = 55
+n2 = 56
 media = (n1 + n2) / 2
 print('media:', media)
 
@@ -13,3 +13,13 @@ importe = 230
 billetes50 = importe // 50
 resto = importe % 50
 print(f"Importe: {importe}, billetes de 50: {billetes50} y el resto: {resto}")
+
+# Potencia:
+print(2**10)
+
+"""
+Prioriodades: 
+**
+* / // %
++ - 
+"""
