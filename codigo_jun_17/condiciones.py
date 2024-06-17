a3a6500b6797bb3eeaa13a6ef10475236c373d99
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 12:38:56 +0200

    12.38

--- codigo_jun_17/condiciones.py
+++ codigo_jun_17/condiciones.py
@@ -5,7 +5,7 @@ Operadores relacionales y lógicos
 # Comprobar si una variable está dentro de un intervalo
 edad_ini = 18
 edad_fin = 65
-edad = 45
+edad = 4
 
 if edad >= edad_ini and edad <= edad_fin:
     print('edad dentro del intervalo')
@@ -17,7 +17,12 @@ if edad_ini <= edad <= edad_fin:
 else:
     print('no lo cumple')
 
-if xxx
+if not (edad_ini <= edad <= edad_fin):
+    print('no lo cumple')
+else:
+    print('edad dentro del intervalo')
+
+if edad < edad_ini or edad > edad_fin:
     print('no lo cumple')
 else:
     print('edad dentro del intervalo')
