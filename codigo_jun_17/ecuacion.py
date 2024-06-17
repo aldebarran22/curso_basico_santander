1d5230ced387dc57ee42fbc38afe87dd3170074d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 12:27:16 +0200

    12.27

--- codigo_jun_17/ecuacion.py
+++ codigo_jun_17/ecuacion.py
@@ -9,8 +9,12 @@ x2 = -b - ...
 import math
 
 # Inicialización múltiple:
-a,b,c = 1,5,4
+a,b,c = 1,5,4 #1,5,4
 
-x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
-print("x1: ", x1, "x2: ", x2)
+raiz = b**2 - 4*a*c
+if raiz > 0:
+    x1 = (-b + math.sqrt(raiz)) / (2*a)
+    x2 = (-b - math.sqrt(raiz)) / (2*a)
+    print("x1: ", x1, "x2: ", x2)
+else:
+    print('No hay solución')
