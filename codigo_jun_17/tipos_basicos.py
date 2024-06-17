e50fec27969616216f07680637983f46b50eab81
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-17 11:57:18 +0200

    11.57

--- codigo_jun_17/tipos_basicos.py
+++ codigo_jun_17/tipos_basicos.py
@@ -27,11 +27,30 @@ n2 = float(s2)
 
 resul = float(s1) + float(s2)
 print(resul, n1+n2)
+print(n1,"+",n2,"=",(n1+n2))
+
+# f-string
+print(f"{n1} + {n2} = {n1+n2}")
+
 
 #Covertir de float a str
 cad1 = str(n1)
 cad2 = str(n2)
 print(cad1 + cad2)
+
+# En Python es False: 0, "" cadena, [] lista vacía, None
+numero = 0
+if numero:
+    # preguntamos: numero distinto de cero
+    print('numero != 0')    
+else:
+    print('numero es cero')
+
+cadena = ""
+if cadena:
+    print('hay letras')
+else:
+    print("cadena vacía")
 
 
 
