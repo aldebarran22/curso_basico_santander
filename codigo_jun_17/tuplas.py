c8ca9eb367ca6c87520e8a37d5319bd5250f0a0e
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 12:23:52 +0200

    12.23

--- codigo_jun_17/tuplas.py
+++ codigo_jun_17/tuplas.py
@@ -9,10 +9,24 @@ Indices: positivos y negativos
 - [("Alemania", 1000), ("Francia",56)]
 """
 
-t1 = 1,2,3,4
-t2 = (5,6,7,8)
+t1 = 1, 2, 3, 4
+t2 = (5, 6, 7, 8)
 print(t1, type(t1))
 
 t3 = ("Alemania",)
 print(t3, type(t3))
 
+# Intercambiar variables:
+ini = 50
+fin = 10
+print(ini, fin)
+ini, fin = fin, ini
+print(ini, fin)
+
+gps = [(40.4, -3.65), (42.6, 5.7), (36.7, 1.6)]
+for t in gps:
+    print(t[0], t[1])
+
+# Expansión de tuplas:
+for lat, lon in gps:
+    print(lat, lon)
