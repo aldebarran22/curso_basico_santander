282d1ffdcd04f4138e6138a4a6b2b3ad48d271c5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 14:23:51 +0200

    14.23

---
+++ codigo_jun_17/prueba_funciones.py
@@ -0,0 +1,13 @@
+"""
+Importar funciones de otros módulos y probarlas
+"""
+
+"""
+import funciones_curso
+# modulo.funcion
+print(funciones_curso.ecuacion(1,5,4))
+"""
+
+# Con un alias en el módulo:
+import funciones_curso as fc
+print(fc.ecuacion(1,5,4))
