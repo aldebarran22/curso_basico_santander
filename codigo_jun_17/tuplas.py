f45dc7cbd2e21f97d6c5e76cfe1cfce37285ded4
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 12:18:19 +0200

    12.18

---
+++ codigo_jun_17/tuplas.py
@@ -0,0 +1,18 @@
+"""
+Tuplas: definición
+Operadores (+ * in) y slicing, for: igual que en las listas
+Indices: positivos y negativos
+- Paso de parámetros a funciones
+- intercambiar variables
+- SQL: select * from clientes where pais='Alemania'
+- select * from clientes where pais = ? and importe > ? ("Alemania", 1000)
+- [("Alemania", 1000), ("Francia",56)]
+"""
+
+t1 = 1,2,3,4
+t2 = (5,6,7,8)
+print(t1, type(t1))
+
+t3 = ("Alemania",)
+print(t3, type(t3))
+
