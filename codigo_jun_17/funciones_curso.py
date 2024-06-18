8068faa580b583ed9e34c20900a0f5b23cc566f5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 14:19:13 +0200

    14.19

--- codigo_jun_17/funciones_curso.py
+++ codigo_jun_17/funciones_curso.py
@@ -4,13 +4,15 @@ Ejemplos de funciones
 
 import math
 
-raiz = b**2 - 4 * a * c
-if raiz > 0:
-    x1 = (-b + math.sqrt(raiz)) / (2 * a)
-    x2 = (-b - math.sqrt(raiz)) / (2 * a)
-    print("x1: ", x1, "x2: ", x2)
-else:
-    print("No hay solución")
+def ecuacion(a,b,c):
+    raiz = b**2 - 4 * a * c
+    if raiz > 0:
+        x1 = (-b + math.sqrt(raiz)) / (2 * a)
+        x2 = (-b - math.sqrt(raiz)) / (2 * a)
+        return x1, x2
+    else:
+        return "No hay solución"
 
 if __name__ == "__main__":
-    pass
+    solucion = ecuacion(1,5,4)
+    print(solucion)
