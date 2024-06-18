5bf166d12543e5d846b3a7832c59a352bd124aba
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 13:11:39 +0200

    13.11

--- codigo_jun_17/control_flujo.py
+++ codigo_jun_17/control_flujo.py
@@ -29,3 +29,12 @@ while True:
         print("Solo se aceptan número enteros")
 
 print("importe: ", importe)
+
+
+# Imprime los número impares:
+i = 0
+while i < 10:
+    i = i + 1
+    if i % 2 == 0:
+        continue
+    print(i)
