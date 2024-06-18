81908688d54ea6757cb2486e15b305a635dac878
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 13:03:05 +0200

    13.03

--- codigo_jun_17/control_flujo.py
+++ codigo_jun_17/control_flujo.py
@@ -10,7 +10,22 @@ while True:
 """
 
 # Validar un importe: sólo se aceptan múltiplos de 10
+"""
 importe = int(input("Teclear importe:"))
 while importe % 10 != 0:
     print("El importe es incorrecto")
     importe = int(input("Teclear importe:"))
+"""
+
+while True:
+    sImporte = input("Teclear importe:")
+    if sImporte.isnumeric():
+        importe = int(sImporte)
+        if importe % 10 == 0:
+            break
+        else:
+            print("El número no es múltiplo de 10")
+    else:
+        print("Solo se aceptan número enteros")
+
+print("importe: ", importe)
