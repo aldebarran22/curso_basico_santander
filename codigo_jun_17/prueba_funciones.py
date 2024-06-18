a79411596ff9c2c81fc86313b4b117f295ff9a52
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 14:27:01 +0200

    14.26

--- codigo_jun_17/prueba_funciones.py
+++ codigo_jun_17/prueba_funciones.py
@@ -8,6 +8,14 @@ import funciones_curso
 print(funciones_curso.ecuacion(1,5,4))
 """
 
+"""
 # Con un alias en el módulo:
 import funciones_curso as fc
+# alias.funcion
 print(fc.ecuacion(1,5,4))
+"""
+
+# from modulo import funcion
+from funciones_curso import ecuacion
+
+print(ecuacion(1, 5, 4))
