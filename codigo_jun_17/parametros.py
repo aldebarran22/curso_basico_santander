4c9f6c79ed340ce57be0b2b93ece984e59513327
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-18 14:41:22 +0200

    14.41

--- codigo_jun_17/parametros.py
+++ codigo_jun_17/parametros.py
@@ -8,8 +8,22 @@ Tipos de parámetros de una función:
 Tipos anotados
 """
 
-def sumar(a:str, b:str) -> str:
-    return a + " " + b
 
-print(sumar("hola","adios"))
-print(sumar(12,34))
+def sumar(a: str, b: str) -> str:
+    return a + b
+
+def funcion(ob, op=0, *args, **kwargs):
+    print('obligatorio: ', ob)
+    print('opcional:', op)
+    print('args: ', args)
+    print('kwargs: ', kwargs)
+    print()
+
+
+funcion(1)
+
+exit()
+print(sumar("hola", "adios"))
+print(sumar(12, 34))
+
+
