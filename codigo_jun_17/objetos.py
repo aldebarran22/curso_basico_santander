6dae4f47f067adc52aa3912c1a4bd8f91ce430d2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-19 10:49:16 +0200

    10.49

--- codigo_jun_17/objetos.py
+++ codigo_jun_17/objetos.py
@@ -44,13 +44,23 @@ if __name__ == "__main__":
     # del(p1) fuerza la llamada al destructor!
     print(p1)  # print(p1.__str__())
 
-    p2 = Producto(2, "Fanta limón", 1, 1.25, 200)
+    p2 = Producto(2, "Fanta limón", 1, 1.85, 200)
     print(p2)
 
     L = [p1, p2, Producto(3, "Pan", 2, 1.0, 10)]
+    print(L)
+    L.sort(reverse=True)
     print(L)
 
     if p1 < p2:  # if p1.__lt__(p2):
         print(p1.nombre, "mas barato", p2.nombre)
     else:
         print(p2.nombre, "mas barato", p1.nombre)
+
+    p1.iva = round(p1.precio * .21, 2)
+    print(p1.__dict__)
+
+    L2 = []
+    for pro in L:
+        L2.append(pro.__dict__)
+    print(L2)
