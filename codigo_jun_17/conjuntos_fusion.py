f99611f693455d0b7c6e558a4f0df47e93fd9d8b
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-19 13:44:56 +0200

    13.44

--- codigo_jun_17/conjuntos_fusion.py
+++ codigo_jun_17/conjuntos_fusion.py
@@ -22,9 +22,18 @@ txt3 = """id;nombre;cargo
 9;Dodsworth;Representante de ventas
 10;George;Representante de ventas"""
 
+def ordenar(linea):
+    L = linea.split(';')
+    if L[0].isnumeric():
+        numero = int(L[0])
+        return numero
+    else:
+        return 0
+
 c2 = set(txt2.split("\n"))
 c3 = set(txt3.split("\n"))
 
 todo = c2 | c3
-csv = "\n".join(todo)
+L = sorted(todo, key=ordenar)
+csv = "\n".join(L)
 print(csv)
