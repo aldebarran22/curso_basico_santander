cc533b4a925a911953e94808cca941ec33599f04
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-19 11:57:41 +0200

    11.57

--- codigo_jun_17/herencia.py
+++ codigo_jun_17/herencia.py
@@ -27,19 +27,31 @@ class Persona:
             print(self.nombre, "y", other.nombre, "están hablando")
 
 
-class Guia:
-    pass
+class Guia(Persona):
 
+    def __init__(self, nombre="", edad=0, altura=0.0, ambito="", idiomas=[]):
+        Persona.__init__(self, nombre, edad, altura)
+        # super().__init__(nombre, edad, altura)
+        self.ambito = ambito
+        self.idiomas = idiomas
+
+def testGuia():
+    g1 = Guia("Laura", 24, 1.8, "I", ["inglés","francés"])
+    g2 = Guia("Ana", 44, 1.7, "N", ["italiano","francés"])
+
+    print(g1)
+    if g1 < g2:
+        print(g1.nombre,"es menor que",g2.nombre)
+    else:
+        print(g2.nombre,"es menor que",g1.nombre)
+
+    g1.hablarCon(g2)
 
 def testPersona():
     p1 = Persona("José", 23, 1.8)
     p2 = Persona("Paula", 22, 1.81)
     p1.hablarCon()
     p1.hablarCon(p2)
-
-
-def testGuia():
-    pass
 
 
 if __name__ == "__main__":
