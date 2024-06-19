16500cc357e1efe6cde9392c586cd63654e3f8d2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-19 12:46:13 +0200

    12.46

--- codigo_jun_17/fecha_hora.py
+++ codigo_jun_17/fecha_hora.py
@@ -1,17 +1,15 @@
-
-
 class Time:
 
-    def __init__(self,hh=0,mm=0,ss=0):
+    def __init__(self, hh=0, mm=0, ss=0):
         self.hh = hh
         self.mm = mm
         self.ss = ss
 
     def __add__(self, other):
-        resul = Time(self.hh+other.hh,self.mm+other.mm,self.ss+other.ss)
+        resul = Time(self.hh + other.hh, self.mm + other.mm, self.ss + other.ss)
         resul.ajustar()
         return resul
-    
+
     def ajustar(self):
         minutos = self.ss // 60
         self.ss %= 60
@@ -23,47 +21,49 @@ class Time:
         self.hh += horas
         self.hh %= 24
 
-    def __str__(self):        
-        return '%02d:%02d:%02d' % (self.hh,self.mm,self.ss)
-   
+    def __str__(self):
+        return "%02d:%02d:%02d" % (self.hh, self.mm, self.ss)
+
 
 class Date:
 
-    def __init__(self, dd,mm,yy):
+    def __init__(self, dd, mm, yy):
         self.d = dd
         self.m = mm
         self.y = yy
 
-   
     def __str__(self):
-        return "%02d/%02d/%04d" % (self.d,self.m,self.y)
+        return "%02d/%02d/%04d" % (self.d, self.m, self.y)
 
     def esBisiesto(self):
-        anyo = self.y    
-        if  (anyo % 4 == 0 and anyo % 100 != 0) or (anyo%100 == 0 and anyo % 400 == 0):
-            return True    
+        anyo = self.y
+        if (anyo % 4 == 0 and anyo % 100 != 0) or (anyo % 100 == 0 and anyo % 400 == 0):
+            return True
         else:
             return False
-        
-class DateTime(Date, Time):
-    pass
-        
-if __name__ == '__main__':
-    hora1 = Time(12,15,5) # 12:06:05
-    hora2 = Time(8,45,6) # 08:45:06
-    suma = hora1 + hora2 # suma = hora1.__add__(hora2)
+
+
+class DateTime(Time, Date):
+
+    def __init__(self, d=1, m=1, y=1970, hh=0, mm=0, ss=0):
+        Date.__init__(self, d, m, y)
+        Time.__init__(self, hh, mm, ss)
+
+    def __str__(self):
+        return Date.__str__(self) + " " + Time.__str__(self)
+
+
+if __name__ == "__main__":
+    hora1 = Time(12, 15, 5)  # 12:06:05
+    hora2 = Time(8, 45, 6)  # 08:45:06
+    suma = hora1 + hora2  # suma = hora1.__add__(hora2)
 
     print(hora1)
     print(hora2)
     print(suma)
 
-    d = Date(19,6,2024)
+    d = Date(19, 6, 2024)
     print(d)
 
-    dt = DateTime(19,6,2024,12,34,12) 
-    print(dt) # 19/06/2024 12:34:12
-
-
-
- 
-
+    dt = DateTime(19, 6, 2024, 12, 34, 12)
+    print(dt)  # 19/06/2024 12:34:12
