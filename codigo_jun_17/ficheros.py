43b68a057890e77e2fa093d6e088dc6bf100d90a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-20 11:52:18 +0200

    1152

--- codigo_jun_17/ficheros.py
+++ codigo_jun_17/ficheros.py
@@ -2,6 +2,20 @@
 Gestión de ficheros
 """
 
+def leerFichero(path):
+    fich = None
+    try:
+        fich = open(path, "r")
+        for fila in fich:
+            print(fila)            
+
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+    finally:
+        if fich: fich.close()
+    
+
 def grabarFichero(path):
     fich = None
     try:
@@ -18,4 +32,5 @@ def grabarFichero(path):
 
 
 if __name__=='__main__':
-    grabarFichero("../ficheros/pruebas.txt")
+    #grabarFichero("../ficheros/pruebas.txt")
+    leerFichero("../ficheros_curso/Pedidos.csv")
