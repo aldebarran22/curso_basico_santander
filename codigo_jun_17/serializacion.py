f3b9d9a5ed9d6af1ecf8130938c508388dce2a5f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-20 13:04:38 +0200

    13.04

--- codigo_jun_17/serializacion.py
+++ codigo_jun_17/serializacion.py
@@ -18,8 +18,19 @@ def deserializarPickle(path):
     fich.close()
     return obj
 
+def serializarShelve(path, **objetos):
+    Shelf = s.open(path)
+    for k, v in objetos.items():
+        Shelf[k] = v
+    Shelf.close()
+
+
 if __name__ == '__main__':
     p1 = Producto(12, "ColaCola", 1, 1.55, 100)
     serializarPickle("../ficheros/producto.bin", p1)
     obj = deserializarPickle("../ficheros/producto.bin")
     print(obj)
+
+    t1 = Time(12,30,45)
+    d1 = Date(20,6,2024)
+    serializarShelve("../ficheros/objetos", prod=p1, hora=t1, fecha=d1)
