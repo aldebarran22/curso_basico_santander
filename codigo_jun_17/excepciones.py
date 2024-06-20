fcc886070fc66c3c628d457b22aec3a4d4f170bd
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-20 09:37:35 +0200

    9.37

--- codigo_jun_17/excepciones.py
+++ codigo_jun_17/excepciones.py
@@ -24,7 +24,24 @@ def test2():
     except IndexError as e:
         print(e.__class__.__name__, e)
 
+    except ValueError as e:
+        print(e.__class__.__name__, e)
+
+
+def test3():
+    """Agrupar dos excepciones en el mismo except"""
+    try:
+        L = [1,2,3,4,5,6]
+        numero = int("hola")
+        print(L[10])
+        print("continua")
+
+    except (IndexError, ValueError) as e:
+        print(e.__class__.__name__, e)
+  
+
 
 if __name__=='__main__':
     #test1()
-    test2()
+    #test2()
+    test3()
