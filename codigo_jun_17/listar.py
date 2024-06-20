99fc19f4cbc14a9bb0dfe5bf472264bdfcbcb0d7
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-20 12:35:30 +0200

    12.35

--- codigo_jun_17/listar.py
+++ codigo_jun_17/listar.py
@@ -7,7 +7,7 @@ import sys
 def leerFichero(path):
     fich = None
     try:
-        fich = open(path, "r")
+        fich = open(path, "r", encoding="utf-8")
         txt = fich.read()
         print(txt)
 
