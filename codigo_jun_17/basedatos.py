21ed756b7301694702ba9fb171e2f06df4a8e43c
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-21 09:35:07 +0200

    9.35

--- codigo_jun_17/basedatos.py
+++ codigo_jun_17/basedatos.py
@@ -7,21 +7,37 @@ from os.path import isfile
 
 def listarTabla(path, tabla):
     con = None
+    cur = None
     try:
         if not isfile(path):
             raise FileNotFoundError(f"No se encuentra el fichero: {path}")
         else:
             # Abrir la conexión con la BD:
             con = db.connect(path)
-            print("conexión ok")
+            #print("conexión ok")
+
+            # Crear un cursor:
+            cur = con.cursor()
+
+            # Definir el SQL:
+            sql = f"select * from {tabla}"
+
+            # Ejecutar el SQL:
+            cur.execute(sql)
+
+            for t in cur.fetchall():
+                print(t)
+
+
 
     except Exception as e:
         raise e
     finally:
+        if cur: cur.close()
         if con: con.close()
 
 if __name__=='__main__':
     try:
-        listarTabla("empresa3.db", "productos")
+        listarTabla("../bd/empresa3.db", "productos")
     except Exception as e:
         print(e)
