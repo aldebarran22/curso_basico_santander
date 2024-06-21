ef40e222ffad2c05ed2f670a84823c44937cfb23
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-21 10:28:28 +0200

    10.28

--- codigo_jun_17/aplicacion/modulos/productoCRUD.py
+++ codigo_jun_17/aplicacion/modulos/productoCRUD.py
@@ -9,6 +9,7 @@ select: select * from productos
 
 import sqlite3 as db
 from os.path import isfile
+from producto import Producto
 
 class ProductoCRUD:
 
@@ -18,11 +19,28 @@ class ProductoCRUD:
         else:
             self.con = db.connect(path)
 
+    def read(self, id):
+        cur = None
+        try:
+            cur = self.con.cursor()
+            sql = "select * from productos where id=?"
+            cur.execute(sql, (id,))
+            t = cur.fetchone()
+            if t == None:
+                raise ValueError(f"No existe el producto con clave: {id}")
+            else:
+                return Producto(*t)
+                
+        except Exception as e:
+            raise e
+        finally:
+            if cur: cur.close()
+
     def __del__(self):
         if hasattr(self, "con"):
             self.con.close()
 
 if __name__=="__main__":
-    bd = ProductoCRUD("../../empresa33.db")
+    bd = ProductoCRUD("../../empresa3.db")
     #prod = bd.read(1)
     #print(prod)
