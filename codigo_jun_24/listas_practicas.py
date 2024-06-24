a058da361c4c56c77ae825fae2a72eeea4474633
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 13:59:31 +0200

    14.00

--- codigo_jun_24/listas_practicas.py
+++ codigo_jun_24/listas_practicas.py
@@ -2,7 +2,7 @@
 Prácticas sobre listas:
 """
 
-L = [78,1,2,3,5,6,5,4,112,2,4,5,6]
+L = [78, 1, 2, 3, 5, 6, 5, 4, 112, 2, 4, 5, 6]
 # filtrar valores > 50
 R = []
 for i in L:
@@ -12,10 +12,14 @@ for i in L:
 print(R)
 
 # filtrar valores > 50 sin repetidos
-L = [78,1,2,78,5,106,5,4,112,112,4,5,6]
+L = [78, 1, 2, 78, 5, 106, 5, 4, 112, 112, 4, 5, 6]
 R = []
 for i in L:
-    if i > 50:
+    if i > 50 and i not in R:
         R.append(i)
 
 print(R)
+
+# Dada una lista crear dos listas una con los
+# números pares y otra con los impares
+L = [2, 4, 6, 5, 15, 6, 3, 2, 17, 8, 9, 11]
