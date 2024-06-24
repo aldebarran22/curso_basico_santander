88ff9b8a5b61f9e1133a6157e01b1a3c9401b6fe
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 14:22:24 +0200

    14.22

--- codigo_jun_24/diccionarios.py
+++ codigo_jun_24/diccionarios.py
@@ -4,6 +4,7 @@ Diccionarios en Python:
 - Añadir y modificar claves
 """
 
-d = {"a":100, "b":200, "c":300}
-
+d = {"a": 100, "b": 200, "c": 300}
+d["d"] = 4  # Añadir una nueva clave.
+d["a"] = 1  # Modificar una clave
 print(d, type(d))
