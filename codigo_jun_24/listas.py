040c411bcabd434c091b928bb0afa62dd7ddc24d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 12:39:41 +0200

    12.39

---
+++ codigo_jun_24/listas.py
@@ -0,0 +1,12 @@
+"""
+Definición de listas:
+- funciones
+"""
+
+L = [12, 4, 33, 55, 1]
+print(L, type(L))
+print("Suma: ", sum(L))
+print("Recuento: ", len(L))
+print("Promedio: ", sum(L) / len(L))
+print("Máximo: ", max(L))
+print("Mínimo: ", min(L))
