a6d2cdfff5aa9ed34f49fc747a5039364c3451a2
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 11:55:48 +0200

    11.55

--- codigo_jun_24/cajero.py
+++ codigo_jun_24/cajero.py
@@ -1,17 +1,20 @@
 # División entera: calcular el número de billetes necesarios de 50
 # para sacar 240 euros
 importe = int(input("Teclear importe:>"))
-billetes50 = importe // 50
-if billetes50 > 0:
-    importe = importe % 50
-    print("bill de 50: ", billetes50)
+if importe % 10 == 0:
+    billetes50 = importe // 50
+    if billetes50 > 0:
+        importe = importe % 50
+        print("bill de 50: ", billetes50)
 
-billetes20 = importe // 20
-if billetes20 > 0:
-    importe = importe % 20
-    print("bill de 20: ", billetes20)
+    billetes20 = importe // 20
+    if billetes20 > 0:
+        importe = importe % 20
+        print("bill de 20: ", billetes20)
 
-billetes10 = importe // 10
-if billetes10 > 0:
-    importe = importe % 10
-    print("bill de 10: ", billetes10)
+    billetes10 = importe // 10
+    if billetes10 > 0:
+        importe = importe % 10
+        print("bill de 10: ", billetes10)
+else:
+    print("El importe no es correcto")
