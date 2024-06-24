ecdb4487876486887ff86dfaa056afb2286639a3
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 14:56:33 +0200

    14.56

--- codigo_jun_24/diccionarios_practicas.py
+++ codigo_jun_24/diccionarios_practicas.py
@@ -11,3 +11,14 @@ Lvalores = valores.split(";")
 emp = dict(zip(Lcabs, Lvalores))
 print(emp)
 print(emp["nombre"])
+
+txt = """id;nombre;cargo
+1;Davolio;Representante de ventas
+2;Fuller;Vicepresidente comercial
+3;Leverling;Representante de ventas
+4;Peacock;Representante de ventas
+6;Suyama;Representante de ventas
+7;King;Representante de ventas
+9;Dodsworth;Representante de ventas"""
+
+filas = txt.split("\n")
