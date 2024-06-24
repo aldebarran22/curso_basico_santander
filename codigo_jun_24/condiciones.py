04e8aaee0d732dca9ebb4e55d3886d7ee1f2c64f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 12:07:25 +0200

    12.07

--- codigo_jun_24/condiciones.py
+++ codigo_jun_24/condiciones.py
@@ -2,7 +2,7 @@
 Condiciones: operadores relacionales y lógicos.
 """
 
-semaforo = "rojo"
+semaforo = "verde"
 if semaforo == "rojo":
     print("parar")
 
@@ -11,3 +11,12 @@ elif semaforo == "verde":
 
 else:
     print("precaución")
+
+# Comprobar si un número se encuentra dentro de un intervalo:
+ini = 10
+fin = 50
+numero = 9
+if numero >= ini and numero <= fin:
+    print(f"El {numero} está dentro del intervalo: {ini},{fin}")
+else:
+    print("no está en el intervalo")
