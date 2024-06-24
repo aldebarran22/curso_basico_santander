7ce27a7bf9f3d84ce344ba078fe813acaecda989
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 11:43:02 +0200

    11.42

--- codigo_jun_24/teclado.py
+++ codigo_jun_24/teclado.py
@@ -4,3 +4,9 @@ Leer variables de teclado.
 
 numero = input("Dame un número: ")
 print(numero)
+
+numero2 = input("Dame otro número: ")
+print(numero2)
+
+resul = numero + numero2
+print('Resultado: ', resul)
