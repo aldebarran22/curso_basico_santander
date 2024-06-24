6b087f4a2ce264b06633a332116d7e62150ffb7a
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 12:01:57 +0200

    12.01

---
+++ codigo_jun_24/condiciones.py
@@ -0,0 +1,13 @@
+"""
+Condiciones: operadores relacionales y lógicos.
+"""
+
+semaforo = "rojo"
+if semaforo == "rojo":
+    print("parar")
+
+elif semaforo == "verde":
+    print("pasar")
+
+else:
+    print("precaución")
