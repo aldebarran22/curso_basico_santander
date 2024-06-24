aea78e2351b063cfc05b8396ad966b2be59df018
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 10:57:55 +0200

    10.57

--- codigo_jun_24/tipos_basicos.py
+++ codigo_jun_24/tipos_basicos.py
@@ -33,6 +33,14 @@ billetes50 = importe // 50
 importe = importe % 50
 print("bill de 50: ", billetes50)
 
+billetes20 = importe // 20
+importe = importe % 20
+print("bill de 20: ", billetes20)
+
+billetes10 = importe // 10
+importe = importe % 10
+print("bill de 10: ", billetes10)
+
 """
 Prioridades de los operadores:
 ** potencia
