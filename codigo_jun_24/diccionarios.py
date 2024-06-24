ad335efe34a96c49982625e8e19b75c41755c3d1
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 14:40:28 +0200

    14.40

--- codigo_jun_24/diccionarios.py
+++ codigo_jun_24/diccionarios.py
@@ -7,7 +7,15 @@ Diccionarios en Python:
 d = {"a": 100, "b": 200, "c": 300}
 d["d"] = 4  # Añadir una nueva clave.
 d["a"] = 1  # Modificar una clave
-#del(d["b"])
+# del(d["b"])
 print(d, type(d))
 print(d["a"] + d["b"])
 
+# Pruebas con la función dict y zip
+L = [1, 2, 3, 4, 5]
+cad = "adios"
+d2 = dict(zip(L, cad))
+print(d2)
+
+d3 = dict(zip(cad, L))
+print(d3)
