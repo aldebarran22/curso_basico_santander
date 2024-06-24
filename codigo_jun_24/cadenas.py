8ff8976ed76069d61a6b5b4d395f546be4563dfe
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 13:46:31 +0200

    13.46

--- codigo_jun_24/cadenas.py
+++ codigo_jun_24/cadenas.py
@@ -5,11 +5,17 @@ Pruebas con cadenas.
 """
 
 cad = "hola que tal"
-print('La primera letra: ', cad[0])
-print('la última:', cad[-1])
+print("La primera letra: ", cad[0])
+print("la última:", cad[-1])
 
 cad1 = "hola"
 cad2 = "adios"
 resul = cad1 + " " + cad2
 print(resul)
 print(resul * 5)
+
+palabra = input("Dame palabra")
+if palabra in resul:
+    print(palabra, "está en", resul)
+else:
+    print("no está")
