77c194dcfbe1bbe223867508a1ab852b8b84d1d9
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 13:43:52 +0200

    13.43

---
+++ codigo_jun_24/cadenas.py
@@ -0,0 +1,15 @@
+"""
+Pruebas con cadenas.
+- Operadores: + * in
+- indices
+"""
+
+cad = "hola que tal"
+print('La primera letra: ', cad[0])
+print('la última:', cad[-1])
+
+cad1 = "hola"
+cad2 = "adios"
+resul = cad1 + " " + cad2
+print(resul)
+print(resul * 5)
