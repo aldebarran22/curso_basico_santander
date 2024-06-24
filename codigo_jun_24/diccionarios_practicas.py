c3ac673861547cb332442079f55221aecdeb1355
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 14:52:27 +0200

    14.52

--- codigo_jun_24/diccionarios_practicas.py
+++ codigo_jun_24/diccionarios_practicas.py
@@ -5,3 +5,9 @@ Prácticas sobre diccionarios
 # {"id":"1", "nombre":"Davolio", "cargo":"Representante de ventas"}
 cabs = "id;nombre;cargo"
 valores = "1;Davolio;Representante de ventas"
+
+Lcabs = cabs.split(";")
+Lvalores = valores.split(";")
+emp = dict(zip(Lcabs, Lvalores))
+print(emp)
+print(emp["nombre"])
