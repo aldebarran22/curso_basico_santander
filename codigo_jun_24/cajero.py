3c852faf9b0411d4f9017c2554e63531b3503233
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-24 11:37:23 +0200

    11.37

--- codigo_jun_24/cajero.py
+++ codigo_jun_24/cajero.py
@@ -1,4 +1,3 @@
-
 # División entera: calcular el número de billetes necesarios de 50
 # para sacar 240 euros
 importe = 10
@@ -8,9 +7,11 @@ if billetes50 > 0:
     print("bill de 50: ", billetes50)
 
 billetes20 = importe // 20
-importe = importe % 20
-print("bill de 20: ", billetes20)
+if billetes20 > 0:
+    importe = importe % 20
+    print("bill de 20: ", billetes20)
 
 billetes10 = importe // 10
-importe = importe % 10
-print("bill de 10: ", billetes10)
+if billetes10 > 0:
+    importe = importe % 10
+    print("bill de 10: ", billetes10)
