8b31d4988920338a4026f77312b713e0f4645a23
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-25 12:35:00 +0200

    12.34

---
+++ codigo_jun_24/cajero4.py
@@ -0,0 +1,31 @@
+"""
+Incluye la validación del importe tecleado
+"""
+
+# División entera: calcular el número de billetes necesarios de 50
+# para sacar 240 euros
+
+# Almacena los resultados en un diccionario:
+billetes = [100, 50, 20, 10]
+desgloseBilletes = dict()  # Crear un dict vacio
+
+while True:
+    importe = int(input("Teclear importe:>"))
+    if importe % 10 == 0:
+        break
+    else:
+        print("El importe no es correcto")
+
+for b in billetes:
+    if importe >= b:
+        numeroBilletes = importe // b
+        importe = importe % b
+        desgloseBilletes[b] = numeroBilletes
+        # print(f"Billetes de {b} = {numeroBilletes}")
+
+    if importe == 0:
+        break
+
+print("Desglose:", desgloseBilletes)
+for billete, cuenta in desgloseBilletes.items():
+    print(f"{cuenta} de {billete}")
