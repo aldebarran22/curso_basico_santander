e944c7766c560cc34c74dc5e4e02be63a6f79849
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-25 09:17:33 +0200

    9.17

--- codigo_jun_24/listas2.py
+++ codigo_jun_24/listas2.py
@@ -6,3 +6,11 @@ Prácticas con listas de Python
 # L[ini:fin-1:salto=1]
 L = [10, 20, 30, 40, 50, 60, 70, 80]
 print("los 3 primeros: ", L[0:3])
+print("los 3 primeros: ", L[:3])
+print('los 3 últimos: ', L[-3:])
+print('Quitar el primero y el último: ', L[1:-1])
+print('Toda la lista de dos en dos: ', L[::2])
+print('invertida: ', L[::-1])
+
+
+
