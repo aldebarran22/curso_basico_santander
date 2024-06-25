dedc5e138929de9bc43996c426c4a81c7cb00f92
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-25 14:26:52 +0200

    14.26

--- codigo_jun_24/histograma.py
+++ codigo_jun_24/histograma.py
@@ -4,6 +4,7 @@ Generar un histograma con una lista de valores aleatorios.
 
 import random
 
+def generarHisto()
 N = 1000
 
 # Generar una lista con N números aleatorios:
@@ -16,5 +17,9 @@ histograma = dict()
 for num in claves:
     histograma[num] = L.count(num)
 
-for k, v in histograma.items():
-    print(k,v)
+
+
+if __name__ == '__main__':
+    histograma = generarHisto(500, 1, 50)
+    for k, v in histograma.items():
+        print(k,v)
