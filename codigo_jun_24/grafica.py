5419a53284d244f94366f68837a33b8706f47e66
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-25 14:23:27 +0200

    14.23

--- codigo_jun_24/grafica.py
+++ codigo_jun_24/grafica.py
@@ -5,8 +5,12 @@ from random import randint
 x = list(range(10))
 
 y = []
+y2 = []
 for i in range(10):
     y.append(randint(5, 15))
+    y2.append(randint(5, 15))
 
-plt.plot(x, y)
+plt.plot(x, y, color="red", lw=5, ls="-.", label="ES")
+plt.plot(x, y2, color="green", lw=5, ls="-.", label="FR")
+plt.legend()
 plt.show()
