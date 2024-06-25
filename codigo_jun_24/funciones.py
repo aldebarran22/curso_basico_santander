9037a3734fd1e97a88b31327fe3633c570c310b0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-25 13:06:31 +0200

    13.06

--- codigo_jun_24/funciones.py
+++ codigo_jun_24/funciones.py
@@ -8,10 +8,16 @@ Tipos de llamada:
 """
 
 
-def sumar(a:str, b:str) -> str:
+def sumar(a: str, b: str) -> str:
     """Retorna la suma de los dos parámetros"""
     return a + b
 
-def restar(a,b):
+
+def restar(a, b):
     """Retorna la resta de los dos parámetros"""
-    return a-b
+    return a - b
+
+
+if __name__ == "__main__":
+    print("__name__ ", __name__)
+    print("suma:", sumar(12, 88))
