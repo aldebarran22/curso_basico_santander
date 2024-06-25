3ab09f15c7566df5c65f77a3050837890a3d20ac
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-25 11:30:13 +0200

    11.30

---
+++ codigo_jun_24/tuplas.py
@@ -0,0 +1,14 @@
+"""
+Tuplas en Python.
+- Inicializar múltiples variables
+- Intercambiar variables
+- Paso de parámetros a funciones
+- Para almacenar registros de una consulta a la BD.
+- select * from clientes where pais=? ("España",)
+- Operadores: + * in slicing, indices + y -
+- No se modificar una tupla
+"""
+
+t = 1,2,3,4
+t2 = (5,6,7,8)
+print(t, type(t))
