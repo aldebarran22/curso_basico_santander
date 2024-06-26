5343f113b18b96b484b654ad2397a32020bba982
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-26 09:14:23 +0200

    9.14

---
+++ codigo_jun_24/mutable_vs_inmutable.py
@@ -0,0 +1,25 @@
+"""
+Paso de parámetros mutables (list, set, dict) e 
+inmutables (int / float, str, tuple)
+"""
+
+
+def mutables(lista, conjunto, diccionario):
+    lista.append(100)
+    conjunto.add(100)
+    diccionario["x"] = 100
+
+
+def inmutables(numero, tupla, cadena):
+    numero += 100
+    tupla += (100, 200)
+    cadena += "adios"
+
+
+if __name__ == "__main__":
+    L = [1, 2, 3]
+    c = {1, 2, 3}
+    d = {"a": 1, "b": 2, "c": 3}
+
+    mutables(L, c, d)
+    print(L, c, d)
