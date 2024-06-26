d03f7cd2acc9c880a0acf696af0f97d5e4fb204d
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-26 12:34:05 +0200

    12.34

--- codigo_jun_24/conjuntos_practicas.py
+++ codigo_jun_24/conjuntos_practicas.py
@@ -24,13 +24,21 @@ def ordenar(fila):
     if aux.isnumeric():
         return int(aux)
     else:
-        return 0
+        return -1
     
-
+def ordenarNombre(fila):
+    nombre = fila.split(";")[1]
+    if nombre == "nombre":
+        return "0"
+    else:
+        return nombre
+    
 
 c2 = set(txt2.split("\n"))
 c3 = set(txt3.split("\n"))
 todo = c2 | c3
-L = sorted(todo, key=ordenar)
-csv = "\n".join(L)
+L = [fila for fila in todo if "nombre" not in fila]
+R = sorted(L, key=ordenar)
+R.insert(0, "id;nombre;cargo")
+csv = "\n".join(R)
 print(csv)
