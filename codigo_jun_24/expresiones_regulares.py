b0af23c8d56c06c98174cfb9c02a7c152fa99d96
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-27 14:06:17 +0200

    14.06

--- codigo_jun_24/expresiones_regulares.py
+++ codigo_jun_24/expresiones_regulares.py
@@ -4,14 +4,20 @@ Expresiones regulares en Python
 
 import re
 
+
 def validar(patron, L):
     print("patron:", patron)
     for i in L:
         print(i, True if re.match(patron, i) else False)
-    print('-' * 20)
+    print("-" * 20)
+
+
+# Ejemplo con el .
+patron = ".ython"
+L = ["python", "jython", "2ython", "*ython", "cithon"]
+validar(patron, L)
 
 # Validar DNIs:
 patron = ""
 L = ["51000456Y", "51000456y", "51000456YZ", "567890988"]
 validar(patron, L)
-
