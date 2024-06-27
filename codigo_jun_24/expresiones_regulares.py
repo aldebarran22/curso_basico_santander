f1d6e068f1927f56eaa38c17f5e8548ee0782168
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-27 14:32:40 +0200

    14.32

--- codigo_jun_24/expresiones_regulares.py
+++ codigo_jun_24/expresiones_regulares.py
@@ -24,11 +24,19 @@ validar(patron, L)
 
 # Cadenas que empiezan por 2 digitos, seguidas de un
 # tab y terminan en dos letras
-patron = ""
-L = []
+patron = r"\d{2}\t[A-Z]{2}$"
+L = ["12\tRR", "12 44", "1244", "12     55"]
 validar(patron, L)
 
 # Validar DNIs:
-patron = ""
-L = ["51000456Y", "51000456y", "51000456YZ", "567890988"]
+patron = "\d{8}[A-Z]$"
+L = ["51000456Y", " 51000456y", "51000456YZ", "567890988"]
 validar(patron, L)
+
+"""
+Comprobar la validez del siguiente código:
+Palabra COD o S/N, un guión bajo, 3 vocales mayúsculas, 
+guión bajo y 6 números que no pueden empezar por 0.
+COD_AEE_800959
+S/N_UOO_958474
+"""
