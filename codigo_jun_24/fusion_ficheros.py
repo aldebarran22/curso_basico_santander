0d63f735e231621e4cdd23e4b2b437b8dfba8fec
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-27 10:30:47 +0200

    10.30

--- codigo_jun_24/fusion_ficheros.py
+++ codigo_jun_24/fusion_ficheros.py
@@ -12,6 +12,20 @@ def ordenarNombre(fila):
         return "0"
     else:
         return nombre
+    
+def cargaFichero(path):
+    """Leer el fichero y devuelve un conjunto con las filas"""
+    fich = None
+    try:
+        fich = open(path, "r")
+        txt = fich.read()
+       
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+    finally:
+        if fich:
+            fich.close()
 
 
 c2 = set(txt2.split("\n"))
