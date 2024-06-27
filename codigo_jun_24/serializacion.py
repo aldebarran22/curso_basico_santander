d4b8176bcdfe0f32de3338a1a74d7da9d6411e81
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-27 13:08:57 +0200

    13.08

--- codigo_jun_24/serializacion.py
+++ codigo_jun_24/serializacion.py
@@ -9,7 +9,17 @@ from objetos import Candidato
 
 
 def serializarPickle(objeto, path):
-    pass
+    fich = None
+    try:
+        fich = open(path, "wb")
+        p.dump(objeto, fich)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if fich:
+            fich.close()
 
 
 def deserializarPickle(path):
@@ -18,6 +28,10 @@ def deserializarPickle(path):
 
 def testPickle():
     c1 = Candidato("Pedro", 10, 6, True)
+    print(c1)
+    serializarPickle(c1, "../ficheros/candidato.bin")
+    c2 = deserializarPickle("../ficheros/candidato.bin")
+    print(c2)
 
 
 if __name__ == "__main__":
