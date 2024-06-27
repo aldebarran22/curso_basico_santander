3baba4a1d19f19efe26268ca58a28c6b889cff0f
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-27 12:45:09 +0200

    12.45

---
+++ codigo_jun_24/directorios.py
@@ -0,0 +1,21 @@
+"""Explorar directorios"""
+
+from os import walk
+
+
+def ls(ruta="."):
+    # Devuelve una tupla de 3 elementos.
+    dir, subdirs, archivos = next(walk(ruta))
+    # print("Actual: ", dir)
+    # print("Subdirectorios: ", subdirs)
+    # print("Archivos: ", archivos[:3])  # Devuelve la lista de
+
+    for subdir in subdirs:
+        # print("*" * 30)
+        print(dir + "/" + subdir)
+        #print("Archivos: ", archivos[:3])
+        ls(dir + "/" + subdir)
+
+
+if __name__ == "__main__":
+    ls("..")
