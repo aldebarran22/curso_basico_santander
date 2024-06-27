6b0b10b94e3c8867550d6802b862f7f13f5a2619
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-27 09:33:17 +0200

    9.33

--- codigo_jun_24/excepciones.py
+++ codigo_jun_24/excepciones.py
@@ -5,17 +5,21 @@ finally: liberar recursos
 raise: lanzar excepciones
 """
 
+
 def test1():
     """Capturar una excepcion"""
     try:
-        L = [1,2,3,4,5]
+        L = [1, 2, 3, 4, 5]
         print(L[10])
+        print("La suma es:", sum(L))
+
     except IndexError as e:
         print(e.__class__.__name__, e)
 
 
 def test2():
     pass
+
 
 if __name__ == "__main__":
     test1()
