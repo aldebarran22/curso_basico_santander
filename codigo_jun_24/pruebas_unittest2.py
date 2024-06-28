7dfa31ac31fe002be41d895938cd9ba81c42f3b0
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-28 15:00:53 +0200

    fin

--- codigo_jun_24/pruebas_unittest2.py
+++ codigo_jun_24/pruebas_unittest2.py
@@ -5,11 +5,12 @@ Pruebas con la librería unittest
 import unittest
 from base_datos_CRUD import Empleado, EmpleadoCRUD
 
+
 class Ejemplo(unittest.TestCase):
 
     def testFile(self):
-        self.assertRaises(FileNotFoundError, EmpleadoCRUD, "empresa3.db")        
-   
+        self.assertRaises(FileNotFoundError, EmpleadoCRUD, "../bd/empresa3.db")
+
 
 if __name__ == "__main__":
     unittest.main()
