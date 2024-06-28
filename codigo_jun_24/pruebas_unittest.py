9f3c683bbe8642e9bb09a3a3eb8ad0798a2da560
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-28 14:50:39 +0200

    14.50

--- codigo_jun_24/pruebas_unittest.py
+++ codigo_jun_24/pruebas_unittest.py
@@ -4,17 +4,23 @@ Pruebas con la librería unittest
 
 import unittest
 
+
 class Ejemplo(unittest.TestCase):
+
+    def test2(self):
+        self.assertAlmostEqual(
+            3.456, 3.412, places=3, msg="No coinciden en los 3 primero decimales"
+        )
 
     def testOk(self):
         pass
 
     def testException(self):
         raise ValueError("error de prueba")
-    
+
     def testFail(self):
         raise AssertionError("Error assert")
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
