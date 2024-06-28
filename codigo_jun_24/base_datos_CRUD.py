04567ee4c2edd9b7189bcd3f3520ae0cfd755430
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-28 10:18:43 +0200

    10.18

--- codigo_jun_24/base_datos_CRUD.py
+++ codigo_jun_24/base_datos_CRUD.py
@@ -12,6 +12,43 @@ import sqlite3 as db
 from os.path import isfile
 
 
+class Empleado:
+    def __init__(self, id=0, nombre="", cargo=""):
+        self.__id = id
+        self.__nombre = nombre
+        self.__cargo = cargo
+
+    def getId(self):
+        return self.__id
+
+    def setId(self, id):
+        self.__id = id
+
+    def getNombre(self):
+        return self.__nombre
+
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+
+    def getCargo(self):
+        return self.__cargo
+
+    def setCargo(self, cargo):
+        self.__cargo = cargo
+
+    def getTupla(self):
+        return (self.__nombre, self.__cargo)
+
+    def getTupla2(self):
+        return (self.__nombre, self.__cargo, self.__id)
+
+    def __str__(self):
+        return str(self.__id) + " " + self.__nombre + " " + self.__cargo
+
+    def __repr__(self):
+        return str(self)
+
+
 class ProductoCRUD:
     """Operaciones CRUD para un producto"""
 
@@ -21,12 +58,12 @@ class ProductoCRUD:
         else:
             self.con = db.connect(path)
 
-    def __del__(self):       
+    def __del__(self):
         if hasattr(self, "con"):
             print("cerrando conexión...")
             self.con.close()
         else:
-            print('no existe el att con')
+            print("no existe el att con")
 
 
 if __name__ == "__main__":
