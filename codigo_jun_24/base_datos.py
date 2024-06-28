e39708a854c9052b44c53419cddcccb0f7276845
aldebarran22 <antonioespinherranz@hotmail.com>
2024-06-28 09:38:08 +0200

    9.38

--- codigo_jun_24/base_datos.py
+++ codigo_jun_24/base_datos.py
@@ -25,5 +25,31 @@ def testConexion(path):
             con.close()
 
 
+def listar(path, tabla):
+    con = None
+    cur = None
+    try:
+        if not isfile(path):
+            raise FileNotFoundError(f"No se ha encontrado el fichero: {path}")
+        else:
+            con = db.connect(path)
+            sql = f"select * from {tabla}"
+            cur = con.cursor()
+            cur.execute(sql)
+
+            print(cur.description)
+
+
+
+    except Exception as e:
+        print(e.__class__.__name__, e)
+
+    finally:
+        if cur:
+            cur.close()
+        if con:
+            con.close()
+
+
 if __name__ == "__main__":
-    testConexion("no existe.dat")
+    testConexion("../../bd/empresa3.db")
